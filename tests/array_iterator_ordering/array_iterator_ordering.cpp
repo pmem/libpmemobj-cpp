237aaceef9881485857cafc580342cae8e4820d5
Igor Chorazewicz <igor.chorazewicz@intel.com>
2018-07-19 10:29:57 +0200

    tests: add test for pmem::obj::array

---
+++ tests/array_iterator_ordering/array_iterator_ordering.cpp
@@ -0,0 +1,149 @@
+/*
+ * Copyright 2018, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "unittest.hpp"
+
+#include <iterator>
+
+#include <libpmemobj++/experimental/array.hpp>
+
+namespace pmemobj_exp = pmem::obj::experimental;
+
+int
+main(int argc, char *argv[])
+{
+	{
+		typedef pmemobj_exp::array<int, 5> C;
+		C c;
+		C::iterator i;
+		i = c.begin();
+		C::const_iterator j;
+		j = c.cbegin();
+		UT_ASSERT(i == j);
+	}
+
+	{
+		typedef pmemobj_exp::array<int, 0> C;
+		C c;
+		C::iterator i;
+		i = c.begin();
+		C::const_iterator j;
+		j = c.cbegin();
+		UT_ASSERT(i == j);
+	}
+
+#if __cplusplus >= 201402L
+	{
+		typedef pmemobj_exp::array<int, 5> C;
+		C::iterator ii1{}, ii2{};
+		C::iterator ii4 = ii1;
+		C::const_iterator cii{};
+		UT_ASSERT(ii1 == ii2);
+		UT_ASSERT(ii1 == ii4);
+		UT_ASSERT(ii1 == cii);
+
+		UT_ASSERT(!(ii1 != ii2));
+		UT_ASSERT(!(ii1 != cii));
+
+		C c;
+		UT_ASSERT(c.begin() == std::begin(c));
+		UT_ASSERT(c.cbegin() == std::cbegin(c));
+		UT_ASSERT(c.rbegin() == std::rbegin(c));
+		UT_ASSERT(c.crbegin() == std::crbegin(c));
+		UT_ASSERT(c.end() == std::end(c));
+		UT_ASSERT(c.cend() == std::cend(c));
+		UT_ASSERT(c.rend() == std::rend(c));
+		UT_ASSERT(c.crend() == std::crend(c));
+
+		UT_ASSERT(std::begin(c) != std::end(c));
+		UT_ASSERT(std::rbegin(c) != std::rend(c));
+		UT_ASSERT(std::cbegin(c) != std::cend(c));
+		UT_ASSERT(std::crbegin(c) != std::crend(c));
+	}
+	{
+		typedef pmemobj_exp::array<int, 0> C;
+		C::iterator ii1{{}}, ii2{{}};
+		C::iterator ii4 = ii1;
+		C::const_iterator cii{{}};
+		UT_ASSERT(ii1 == ii2);
+		UT_ASSERT(ii1 == ii4);
+
+		UT_ASSERT(!(ii1 != ii2));
+
+		UT_ASSERT((ii1 == cii));
+		UT_ASSERT((cii == ii1));
+		UT_ASSERT(!(ii1 != cii));
+		UT_ASSERT(!(cii != ii1));
+		UT_ASSERT(!(ii1 < cii));
+		UT_ASSERT(!(cii < ii1));
+		UT_ASSERT((ii1 <= cii));
+		UT_ASSERT((cii <= ii1));
+		UT_ASSERT(!(ii1 > cii));
+		UT_ASSERT(!(cii > ii1));
+		UT_ASSERT((ii1 >= cii));
+		UT_ASSERT((cii >= ii1));
+		UT_ASSERT(cii - ii1 == 0);
+		UT_ASSERT(ii1 - cii == 0);
+
+		C c;
+		UT_ASSERT(c.begin() == std::begin(c));
+		UT_ASSERT(c.cbegin() == std::cbegin(c));
+		UT_ASSERT(c.rbegin() == std::rbegin(c));
+		UT_ASSERT(c.crbegin() == std::crbegin(c));
+		UT_ASSERT(c.end() == std::end(c));
+		UT_ASSERT(c.cend() == std::cend(c));
+		UT_ASSERT(c.rend() == std::rend(c));
+		UT_ASSERT(c.crend() == std::crend(c));
+
+		UT_ASSERT(std::begin(c) == std::end(c));
+		UT_ASSERT(std::rbegin(c) == std::rend(c));
+		UT_ASSERT(std::cbegin(c) == std::cend(c));
+		UT_ASSERT(std::crbegin(c) == std::crend(c));
+	}
+
+	{
+		typedef pmemobj_exp::array<int, 5> C;
+		C c{{0, 1, 2, 3, 4}};
+
+		UT_ASSERT(c.begin() == std::begin(c));
+		UT_ASSERT(c.end() == std::end(c));
+		UT_ASSERT(c.rbegin() == std::rbegin(c));
+		UT_ASSERT(c.rend() == std::rend(c));
+		UT_ASSERT(std::begin(c) != std::end(c));
+		UT_ASSERT(std::rbegin(c) != std::rend(c));
+		UT_ASSERT(*c.begin() == 0);
+		UT_ASSERT(*c.rbegin() == 4);
+		UT_ASSERT(*std::begin(c) == 0);
+		UT_ASSERT(*std::rbegin(c) == 4);
+	}
+#endif
+}
---
+++ tests/array_slice/array_slice.cpp
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2018, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "unittest.hpp"
+
+#include <libpmemobj++/experimental/array.hpp>
+#include <libpmemobj++/experimental/slice.hpp>
+#include <libpmemobj++/make_persistent.hpp>
+#include <libpmemobj++/pool.hpp>
+#include <libpmemobj++/transaction.hpp>
+
+namespace pmemobj_exp = pmem::obj::experimental;
+
+struct test {
+	void
+	run()
+	{
+		auto slice = c.range(2, 2);
+
+		UT_ASSERT(slice.size() == 2);
+		UT_ASSERT(slice[0] == 3);
+		UT_ASSERT(slice[1] == 4);
+
+		UT_ASSERT(slice.begin() == c.begin() + 2);
+		UT_ASSERT(slice.end() == c.begin() + 4);
+
+		for (auto &it : slice) {
+			it = 0;
+		}
+
+		UT_ASSERT(c[2] == 0);
+		UT_ASSERT(c[3] == 0);
+
+		try {
+			/* Out of range */
+			slice.at(2) = 2;
+			UT_ASSERT(0);
+		} catch (...) {
+		}
+
+		try {
+			/* Out of range */
+			c.range(100, 2);
+			UT_ASSERT(0);
+		} catch (...) {
+		}
+	}
+
+	using C = pmemobj_exp::array<double, 6>;
+	C c = {{1, 2, 3, 4, 5, 6}};
+};
+
+struct root {
+	pmem::obj::persistent_ptr<test> ptr;
+};
+
+int
+main(int argc, char *argv[])
+{
+	if (argc < 2) {
+		std::cerr << "usage: " << argv[0] << " file-name" << std::endl;
+		return 1;
+	}
+
+	auto path = argv[1];
+	auto pop = pmem::obj::pool<root>::create(
+		path, "ArrayTest", PMEMOBJ_MIN_POOL, S_IWUSR | S_IRUSR);
+
+	auto r = pop.get_root();
+
+	try {
+		pmem::obj::transaction::exec_tx(pop, [&] {
+			r->ptr = pmem::obj::make_persistent<test>();
+
+			r->ptr->run();
+
+			pmem::obj::delete_persistent<test>(r->ptr);
+		});
+	} catch (...) {
+		UT_ASSERT(0);
+	}
+
+	pop.close();
+}

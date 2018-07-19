944de5072c9e9a15274086cb071f7e7f734231b1
Igor Chorazewicz <igor.chorazewicz@intel.com>
2018-07-19 11:32:53 +0200

    common: add aggregate construction support

---
+++ tests/aggregate_initialization/aggregate_initialization.cpp
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2018, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "unittest.hpp"
+
+#include <libpmemobj++/make_persistent.hpp>
+#include <libpmemobj++/p.hpp>
+#include <libpmemobj++/persistent_ptr.hpp>
+#include <libpmemobj++/pool.hpp>
+#include <libpmemobj++/transaction.hpp>
+
+#include <iostream>
+
+#define LAYOUT "cpp"
+
+namespace nvobj = pmem::obj;
+
+struct foo {
+	nvobj::p<int> a;
+	nvobj::p<int> b;
+};
+
+struct root {
+	nvobj::persistent_ptr<foo> pfoo;
+};
+
+void
+test_aggregate(nvobj::pool<root> &pop)
+{
+	nvobj::persistent_ptr<root> r = pop.get_root();
+
+#if !__cpp_lib_is_aggregate
+	/* make sure aggregate initialization is available */
+	UT_ASSERT(0);
+#endif
+
+	try {
+		nvobj::transaction::exec_tx(pop, [&] {
+			r->pfoo = nvobj::make_persistent<foo>(2, 3);
+
+			UT_ASSERTeq(r->pfoo->a, 2);
+			UT_ASSERTeq(r->pfoo->b, 3);
+
+			nvobj::delete_persistent<foo>(r->pfoo);
+		});
+	} catch (...) {
+		UT_ASSERT(0);
+	}
+}
+
+int
+main(int argc, char *argv[])
+{
+	if (argc != 2)
+		UT_FATAL("usage: %s file-name", argv[0]);
+
+	const char *path = argv[1];
+
+	nvobj::pool<root> pop;
+
+	try {
+		pop = nvobj::pool<root>::create(path, LAYOUT, PMEMOBJ_MIN_POOL,
+						S_IWUSR | S_IRUSR);
+	} catch (pmem::pool_error &pe) {
+		UT_FATAL("!pool::create: %s %s", pe.what(), path);
+	}
+
+	test_aggregate(pop);
+
+	pop.close();
+}

89d1ce4874d611337364a334e191295b7878a861
Sergei Vinogradov <sergey.vinogradov@intel.com>
2018-07-30 20:48:09 +0300

    Persistent concurrent hash map data structure.
    Implementation is based on concurrent_hash_map from Intel TBB library.
    
    Test for persistent_concurrent_hash_map is built and executed only if
    Intel TBB library found by CMake.
    
    Update README for Intel TBB related tests.
    
    Extend Docker images to support Intel TBB
    
    Add clang-format checks for experimental folder
    
    Fix CPack issue with deb package
    
    Add abstraction for RW mutex
    
    Add pmreorder test for concurrent_hash_map

---
+++ include/libpmemobj++/detail/persistent_pool_ptr.hpp
@@ -0,0 +1,542 @@
+/*
+ * Copyright 2018-2019, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef PMEMOBJ_PERSISTENT_POOL_PTR_HPP
+#define PMEMOBJ_PERSISTENT_POOL_PTR_HPP
+
+#include <cassert>
+#include <cstddef>
+#include <type_traits>
+
+#include <libpmemobj++/detail/specialization.hpp>
+#include <libpmemobj++/persistent_ptr.hpp>
+
+namespace pmem
+{
+namespace detail
+{
+
+using namespace pmem;
+using namespace pmem::obj;
+
+template <typename T>
+class persistent_pool_ptr {
+	template <typename Y>
+	friend class persistent_pool_ptr;
+
+	typedef persistent_pool_ptr<T> this_type;
+
+public:
+	/**
+	 * Type of an actual object with all qualifier removed,
+	 * used for easy underlying type access
+	 */
+	typedef typename pmem::detail::sp_element<T>::type element_type;
+
+	persistent_pool_ptr() : off(0)
+	{
+		verify_type();
+	}
+
+	/**
+	 *  Default null constructor, zeroes the off.
+	 */
+	persistent_pool_ptr(std::nullptr_t) noexcept : off(0)
+	{
+		verify_type();
+	}
+
+	/**
+	 * PMEMoid constructor.
+	 *
+	 * Provided for easy interoperability between C++ and C API's.
+	 *
+	 * @param oid C-style persistent pointer
+	 */
+	persistent_pool_ptr(PMEMoid oid) noexcept : off(oid.off)
+	{
+		verify_type();
+	}
+
+	/**
+	 * PMEMoid constructor.
+	 *
+	 * Provided for easy interoperability between C++ and C API's.
+	 *
+	 * @param off offset inside persistent memory pool
+	 */
+	persistent_pool_ptr(uint64_t _off) noexcept : off(_off)
+	{
+		verify_type();
+	}
+
+	/**
+	 * Copy constructor from a different persistent_pool_ptr<>.
+	 *
+	 * Available only for convertible types.
+	 *
+	 */
+	template <typename Y,
+		  typename = typename std::enable_if<
+			  std::is_convertible<Y *, T *>::value>::type>
+	persistent_pool_ptr(const persistent_pool_ptr<Y> &r) noexcept
+	    : off(r.off)
+	{
+		verify_type();
+	}
+
+	/**
+	 * Copy constructor from a different persistent_ptr<>.
+	 *
+	 * Available only for convertible types.
+	 *
+	 */
+	template <typename Y,
+		  typename = typename std::enable_if<
+			  std::is_convertible<Y *, T *>::value>::type>
+	persistent_pool_ptr(const persistent_ptr<Y> &r) noexcept
+	    : off(r.raw().off)
+	{
+		verify_type();
+	}
+
+	/*
+	 * Copy constructor.
+	 *
+	 * @param r Persistent pool pointer to the same type.
+	 */
+	persistent_pool_ptr(const persistent_pool_ptr &r) noexcept : off(r.off)
+	{
+		verify_type();
+	}
+
+	/*
+	 * Copy constructor from a persistent_ptr.
+	 *
+	 * @param r Persistent pointer to the same type.
+	 */
+	persistent_pool_ptr(const persistent_ptr<T> &r) noexcept
+	    : off(r.raw().off)
+	{
+		verify_type();
+	}
+
+	/**
+	 * Move constructor.
+	 */
+	persistent_pool_ptr(persistent_pool_ptr &&r) noexcept
+	    : off(std::move(r.off))
+	{
+		verify_type();
+	}
+
+	/**
+	 * Move assignment operator.
+	 */
+	persistent_pool_ptr &
+	operator=(persistent_pool_ptr &&r)
+	{
+		conditional_add_to_tx(this);
+		this->off = std::move(r.off);
+
+		return *this;
+	}
+
+	persistent_pool_ptr &operator=(std::nullptr_t)
+	{
+		conditional_add_to_tx(this);
+		this->off = 0;
+
+		return *this;
+	}
+
+	/**
+	 * Assignment operator.
+	 *
+	 * Persistent pool pointer assignment within a transaction
+	 * automatically registers this operation so that a rollback
+	 * is possible.
+	 *
+	 * @throw pmem::transaction_error when adding the object to the
+	 *	transaction failed.
+	 */
+	persistent_pool_ptr &
+	operator=(const persistent_pool_ptr &r)
+	{
+		conditional_add_to_tx(this);
+		this->off = r.off;
+
+		return *this;
+	}
+
+	/**
+	 * Assignment operator from a persistent_ptr.
+	 *
+	 * Persistent pool pointer assignment within a transaction
+	 * automatically registers this operation so that a rollback
+	 * is possible.
+	 *
+	 * @throw pmem::transaction_error when adding the object to the
+	 *	transaction failed.
+	 */
+	persistent_pool_ptr &
+	operator=(const persistent_ptr<T> &r)
+	{
+		conditional_add_to_tx(this);
+		this->off = r.raw().off;
+
+		return *this;
+	}
+
+	/**
+	 * Assignment operator from a PMEMoid.
+	 *
+	 * Persistent pool pointer assignment within a transaction
+	 * automatically registers this operation so that a rollback
+	 * is possible.
+	 *
+	 * @throw pmem::transaction_error when adding the object to the
+	 *	transaction failed.
+	 */
+	persistent_pool_ptr &
+	operator=(const PMEMoid &oid)
+	{
+		conditional_add_to_tx(this);
+		this->off = oid.off;
+		return *this;
+	}
+
+	/**
+	 * Converting assignment operator from a different
+	 * persistent_pool_ptr<>.
+	 *
+	 * Available only for convertible types.
+	 * Just like regular assignment, also automatically registers
+	 * itself in a transaction.
+	 *
+	 * @throw pmem::transaction_error when adding the object to the
+	 *	transaction failed.
+	 */
+	template <typename Y,
+		  typename = typename std::enable_if<
+			  std::is_convertible<Y *, T *>::value>::type>
+	persistent_pool_ptr &
+	operator=(const persistent_pool_ptr<Y> &r)
+	{
+		conditional_add_to_tx(this);
+		this->off = r.off;
+
+		return *this;
+	}
+
+	/**
+	 * Converting assignment operator from a different
+	 * persistent_ptr<>.
+	 *
+	 * Available only for convertible types.
+	 * Just like regular assignment, also automatically registers
+	 * itself in a transaction.
+	 *
+	 * @throw pmem::transaction_error when adding the object to the
+	 *	transaction failed.
+	 */
+	template <typename Y,
+		  typename = typename std::enable_if<
+			  std::is_convertible<Y *, T *>::value>::type>
+	persistent_pool_ptr &
+	operator=(const persistent_ptr<Y> &r)
+	{
+		conditional_add_to_tx(this);
+		this->off = r.raw().off;
+
+		return *this;
+	}
+
+	/**
+	 * Get a direct pointer.
+	 *
+	 * Performs a calculations on the underlying C-style pointer.
+	 *
+	 * @return a direct pointer to the object.
+	 */
+	element_type *
+	get(uint64_t pool_uuid) const noexcept
+	{
+		PMEMoid oid = {pool_uuid, this->off};
+		return static_cast<element_type *>(pmemobj_direct(oid));
+	}
+
+	element_type *
+	operator()(uint64_t pool_uuid) const noexcept
+	{
+		return get(pool_uuid);
+	}
+
+	/**
+	 * Get a persistent pointer.
+	 *
+	 * Performs a calculations on the underlying C-style pointer.
+	 *
+	 * @return a direct pointer to the object.
+	 */
+	persistent_ptr<T>
+	get_persistent_ptr(uint64_t pool_uuid) const noexcept
+	{
+		PMEMoid oid = {pool_uuid, this->off};
+		return persistent_ptr<T>(oid);
+	}
+
+	/**
+	 * Swaps two persistent_pool_ptr objects of the same type.
+	 */
+	void
+	swap(persistent_pool_ptr &other) noexcept
+	{
+		conditional_add_to_tx(this);
+		conditional_add_to_tx(&other);
+		std::swap(this->off, other.off);
+	}
+
+	/*
+	 * Bool conversion operator.
+	 */
+	explicit operator bool() const noexcept
+	{
+		return this->off != 0;
+	}
+
+	/**
+	 * Get PMEMoid encapsulated by this object.
+	 *
+	 * For C API compatibility.
+	 *
+	 * @return const reference to the PMEMoid
+	 */
+	PMEMoid
+	raw_oid(uint64_t pool_uuid) const noexcept
+	{
+		PMEMoid oid = {pool_uuid, this->off};
+		return oid;
+	}
+
+	const uint64_t &
+	raw() const noexcept
+	{
+		return this->off;
+	}
+
+	uint64_t &
+	raw()
+	{
+		conditional_add_to_tx(this);
+		return this->off;
+	}
+
+	/**
+	 * Prefix increment operator.
+	 */
+	inline persistent_pool_ptr<T> &
+	operator++()
+	{
+		conditional_add_to_tx(this);
+		this->off += sizeof(T);
+
+		return *this;
+	}
+
+	/**
+	 * Postfix increment operator.
+	 */
+	inline persistent_pool_ptr<T>
+	operator++(int)
+	{
+		persistent_pool_ptr<T> ret(*this);
+		++(*this);
+
+		return ret;
+	}
+
+	/**
+	 * Prefix decrement operator.
+	 */
+	inline persistent_pool_ptr<T> &
+	operator--()
+	{
+		conditional_add_to_tx(this);
+		this->off -= sizeof(T);
+
+		return *this;
+	}
+
+	/**
+	 * Postfix decrement operator.
+	 */
+	inline persistent_pool_ptr<T>
+	operator--(int)
+	{
+		persistent_pool_ptr<T> ret(*this);
+		--(*this);
+
+		return ret;
+	}
+
+	/**
+	 * Addition assignment operator.
+	 */
+	inline persistent_pool_ptr<T> &
+	operator+=(std::ptrdiff_t s)
+	{
+		conditional_add_to_tx(this);
+		this->off += s * sizeof(T);
+
+		return *this;
+	}
+
+	/**
+	 * Subtraction assignment operator.
+	 */
+	inline persistent_pool_ptr<T> &
+	operator-=(std::ptrdiff_t s)
+	{
+		conditional_add_to_tx(this);
+		this->off -= s * sizeof(T);
+
+		return *this;
+	}
+
+	inline persistent_pool_ptr<T>
+	operator+(std::ptrdiff_t s)
+	{
+		persistent_pool_ptr<T> ret(*this);
+		ret.off += s * sizeof(T);
+
+		return ret;
+	}
+
+	inline persistent_pool_ptr<T>
+	operator-(std::ptrdiff_t s)
+	{
+		persistent_pool_ptr<T> ret(*this);
+		ret.off -= s * sizeof(T);
+
+		return ret;
+	}
+
+private:
+	/* offset of persistent object in a persistent memory pool*/
+	uint64_t off;
+
+	void
+	verify_type()
+	{
+		static_assert(!std::is_polymorphic<element_type>::value,
+			      "Polymorphic types are not supported");
+	}
+};
+
+/**
+ * Equality operator.
+ *
+ * This checks if underlying PMEMoids are equal.
+ */
+template <typename T, typename Y>
+inline bool
+operator==(const persistent_pool_ptr<T> &lhs,
+	   const persistent_pool_ptr<Y> &rhs) noexcept
+{
+	return lhs.raw() == rhs.raw();
+}
+
+/**
+ * Inequality operator.
+ */
+template <typename T, typename Y>
+inline bool
+operator!=(const persistent_pool_ptr<T> &lhs,
+	   const persistent_pool_ptr<Y> &rhs) noexcept
+{
+	return !(lhs == rhs);
+}
+
+/**
+ * Inequality operator with nullptr.
+ */
+template <typename T>
+inline bool
+operator!=(const persistent_pool_ptr<T> &lhs, std::nullptr_t) noexcept
+{
+	return lhs.raw() != 0;
+}
+
+/**
+ * Inequality operator with nullptr.
+ */
+template <typename T>
+inline bool
+operator!=(std::nullptr_t, const persistent_pool_ptr<T> &lhs) noexcept
+{
+	return lhs.raw() != 0;
+}
+
+/**
+ * Equality operator with nullptr.
+ */
+template <typename T>
+inline bool
+operator==(const persistent_pool_ptr<T> &lhs, std::nullptr_t) noexcept
+{
+	return lhs.raw() == 0;
+}
+
+/**
+ * Equality operator with nullptr.
+ */
+template <typename T>
+inline bool
+operator==(std::nullptr_t, const persistent_pool_ptr<T> &lhs) noexcept
+{
+	return lhs.raw() == 0;
+}
+
+template <class T, class U>
+persistent_pool_ptr<T>
+static_persistent_pool_pointer_cast(const persistent_pool_ptr<U> &r)
+{
+	static_assert(std::is_convertible<T *, U *>::value,
+		      "Cannot cast persistent_pool_ptr");
+	return persistent_pool_ptr<T>(r.raw());
+}
+
+} // namespace detail
+} // namespace pmem
+
+#endif // PMEMOBJ_PERSISTENT_POOL_PTR_HPP
---
+++ tests/concurrent_hash_map_singlethread/concurrent_hash_map_singlethread.cpp
@@ -0,0 +1,430 @@
+/*
+ * Copyright 2018-2019, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * concurrent_hash_map.cpp -- pmem::obj::concurrent_hash_map test
+ *
+ */
+
+#include "unittest.hpp"
+
+#include <libpmemobj++/make_persistent.hpp>
+#include <libpmemobj++/p.hpp>
+#include <libpmemobj++/persistent_ptr.hpp>
+#include <libpmemobj++/pool.hpp>
+
+#include <iterator>
+#include <thread>
+#include <vector>
+
+#include <libpmemobj++/experimental/concurrent_hash_map.hpp>
+
+#define LAYOUT "concurrent_hash_map"
+
+namespace nvobj = pmem::obj;
+
+namespace
+{
+
+typedef nvobj::experimental::concurrent_hash_map<nvobj::p<int>, nvobj::p<int>>
+	persistent_map_type;
+
+typedef persistent_map_type::value_type value_type;
+
+struct move_element {
+	move_element(int val) noexcept : val(val)
+	{
+	}
+
+	move_element(const move_element &) = delete;
+
+	move_element &operator=(const move_element &) = delete;
+
+	move_element(move_element &&e) noexcept : val(e.val)
+	{
+	}
+
+	move_element &
+	operator=(move_element &&e)
+	{
+		val = e.val;
+		return *this;
+	}
+
+	nvobj::p<int> val;
+};
+
+typedef nvobj::experimental::concurrent_hash_map<nvobj::p<int>, move_element>
+	persistent_map_move_type;
+
+typedef persistent_map_move_type::value_type value_move_type;
+
+struct root {
+	nvobj::persistent_ptr<persistent_map_type> map1;
+	nvobj::persistent_ptr<persistent_map_type> map2;
+
+	nvobj::persistent_ptr<persistent_map_move_type> map_move;
+};
+
+void
+verify_elements(persistent_map_type &map, size_t elements)
+{
+	UT_ASSERT(map.size() == elements);
+
+	for (int i = 0; i < static_cast<int>(elements); i++) {
+		UT_ASSERT(map.count(i) == 1);
+	}
+}
+
+/**
+ * Wrapper around PMDK allocator
+ * @throw std::bad_alloc on allocation failure.
+ */
+template <typename T, typename U, typename... Args>
+void
+tx_alloc_wrapper(nvobj::pool_base &pop, nvobj::persistent_ptr<U> &ptr,
+		 Args &&... args)
+{
+	try {
+		nvobj::transaction::manual tx(pop);
+		ptr = nvobj::make_persistent<T>(std::forward<Args>(args)...);
+		nvobj::transaction::commit();
+	} catch (...) {
+		throw std::bad_alloc();
+	}
+}
+
+/*
+ * ctor_test -- (internal) test constrcutors
+ * pmem::obj::concurrent_hash_map<nvobj::p<int>, nvobj::p<int> >
+ */
+void
+ctor_test(nvobj::pool<root> &pop)
+{
+	auto &map1 = pop.root()->map1;
+	auto &map2 = pop.root()->map2;
+
+	tx_alloc_wrapper<persistent_map_type>(pop, map1, size_t(10));
+	UT_ASSERT(map1->bucket_count() >= 10);
+	UT_ASSERT(map1->empty());
+
+	for (int i = 0; i < 300; i++) {
+		UT_ASSERT(map1->insert(value_type(i, i)) == true);
+	}
+
+	tx_alloc_wrapper<persistent_map_type>(pop, map2, map1->begin(),
+					      map1->end());
+
+	UT_ASSERT(!map2->empty());
+	UT_ASSERT(map1->size() == map2->size());
+
+	verify_elements(*map2, 300);
+
+	pmem::detail::destroy<persistent_map_type>(*map2);
+	tx_alloc_wrapper<persistent_map_type>(pop, map2, *map1);
+
+	UT_ASSERT(map1->size() == map2->size());
+
+	verify_elements(*map2, 300);
+
+	pmem::detail::destroy<persistent_map_type>(*map2);
+	tx_alloc_wrapper<persistent_map_type>(pop, map2, std::move(*map1));
+
+	verify_elements(*map2, 300);
+
+	pmem::detail::destroy<persistent_map_type>(*map2);
+	tx_alloc_wrapper<persistent_map_type>(
+		pop, map2,
+		std::initializer_list<value_type>{value_type(0, 0),
+						  value_type(1, 1)});
+
+	verify_elements(*map2, 2);
+
+	pmem::detail::destroy<persistent_map_type>(*map1);
+	pmem::detail::destroy<persistent_map_type>(*map2);
+}
+
+/*
+ * assignment_test -- (internal) test assignment operators
+ * pmem::obj::concurrent_hash_map<nvobj::p<int>, nvobj::p<int> >
+ */
+void
+assignment_test(nvobj::pool<root> &pop)
+{
+	auto &map1 = pop.root()->map1;
+	auto &map2 = pop.root()->map2;
+
+	tx_alloc_wrapper<persistent_map_type>(pop, map1);
+	tx_alloc_wrapper<persistent_map_type>(pop, map2);
+
+	UT_ASSERT(map1->empty());
+
+	for (int i = 0; i < 50; i++) {
+		UT_ASSERT(map1->insert(value_type(i, i)) == true);
+	}
+
+	for (int i = 0; i < 300; i++) {
+		UT_ASSERT(map2->insert(value_type(i, i)) == true);
+	}
+
+	*map1 = *map2;
+
+	verify_elements(*map1, 300);
+
+	for (int i = 300; i < 350; i++) {
+		UT_ASSERT(map1->insert(value_type(i, i)) == true);
+	}
+
+	verify_elements(*map1, 350);
+	verify_elements(*map2, 300);
+
+	map2->clear();
+
+	*map1 = *map2;
+
+	UT_ASSERT(map1->size() == 0);
+	UT_ASSERT(std::distance(map1->begin(), map1->end()) == 0);
+	UT_ASSERT(map2->size() == 0);
+	UT_ASSERT(std::distance(map2->begin(), map2->end()) == 0);
+
+	for (int i = 0; i < 350; i++) {
+		UT_ASSERT(map1->count(i) == 0);
+		UT_ASSERT(map2->count(i) == 0);
+	}
+
+	for (int i = 0; i < 100; i++) {
+		UT_ASSERT(map1->insert(value_type(i, i)) == true);
+	}
+
+	verify_elements(*map1, 100);
+	pmem::detail::destroy<persistent_map_type>(*map1);
+	pmem::detail::destroy<persistent_map_type>(*map2);
+}
+
+/*
+ * swap_test -- (internal) test swap method
+ * pmem::obj::concurrent_hash_map<nvobj::p<int>, nvobj::p<int> >
+ */
+void
+swap_test(nvobj::pool<root> &pop)
+{
+	auto &map1 = pop.root()->map1;
+	auto &map2 = pop.root()->map2;
+
+	tx_alloc_wrapper<persistent_map_type>(pop, map1);
+	tx_alloc_wrapper<persistent_map_type>(pop, map2);
+
+	for (int i = 0; i < 50; i++) {
+		UT_ASSERT(map1->insert(value_type(i, i)) == true);
+	}
+
+	for (int i = 0; i < 300; i++) {
+		UT_ASSERT(map2->insert(value_type(i, i)) == true);
+	}
+
+	map1->swap(*map2);
+	verify_elements(*map1, 300);
+	verify_elements(*map2, 50);
+
+	pmem::detail::destroy<persistent_map_type>(*map1);
+	pmem::detail::destroy<persistent_map_type>(*map2);
+}
+
+/*
+ * access_test -- (internal) test access methods
+ * pmem::obj::concurrent_hash_map<nvobj::p<int>, nvobj::p<int> >
+ */
+void
+access_test(nvobj::pool<root> &pop)
+{
+	auto &map1 = pop.root()->map1;
+
+	tx_alloc_wrapper<persistent_map_type>(pop, map1);
+
+	for (int i = 0; i < 100; i++) {
+		UT_ASSERT(map1->insert(value_type(i, i)) == true);
+	}
+
+	UT_ASSERT(map1->begin() ==
+		  const_cast<const persistent_map_type *>(map1.get())->begin());
+	UT_ASSERT(map1->end() ==
+		  const_cast<const persistent_map_type *>(map1.get())->end());
+
+	int i = 0;
+	auto it = map1->begin();
+	auto const_it =
+		const_cast<const persistent_map_type *>(map1.get())->begin();
+	while (it != map1->end()) {
+		UT_ASSERT(it->first == i);
+		UT_ASSERT(it->second == i);
+
+		UT_ASSERT(const_it->first == i);
+		UT_ASSERT(const_it->second == i);
+
+		i++;
+		it++;
+		const_it++;
+	}
+
+	UT_ASSERT(static_cast<size_t>(i) == map1->size());
+
+	pmem::detail::destroy<persistent_map_type>(*map1);
+}
+
+/*
+ * insert_test -- (internal) test insert methods
+ * pmem::obj::concurrent_hash_map<nvobj::p<int>, nvobj::p<int> >
+ */
+void
+insert_test(nvobj::pool<root> &pop)
+{
+	auto &map1 = pop.root()->map1;
+	auto &map_move = pop.root()->map_move;
+
+	tx_alloc_wrapper<persistent_map_type>(pop, map1);
+
+	{
+		typename persistent_map_type::accessor accessor;
+		UT_ASSERTeq(map1->insert(accessor, value_type(1, 1)), true);
+
+		UT_ASSERTeq(accessor->first, 1);
+		UT_ASSERTeq(accessor->second, 1);
+	}
+
+	{
+		typename persistent_map_type::const_accessor accessor;
+		UT_ASSERTeq(map1->insert(accessor, value_type(2, 2)), true);
+
+		UT_ASSERTeq(accessor->first, 2);
+		UT_ASSERTeq(accessor->second, 2);
+	}
+
+	tx_alloc_wrapper<persistent_map_move_type>(pop, map_move);
+
+	{
+		typename persistent_map_move_type::accessor accessor;
+		value_move_type e(3, 3);
+
+		UT_ASSERTeq(map_move->insert(accessor, std::move(e)), true);
+
+		UT_ASSERTeq(accessor->first, 3);
+		UT_ASSERTeq(accessor->second.val, 3);
+	}
+
+	{
+		typename persistent_map_move_type::const_accessor accessor;
+		value_move_type e(4, 4);
+
+		UT_ASSERTeq(map_move->insert(accessor, std::move(e)), true);
+
+		UT_ASSERTeq(accessor->first, 4);
+		UT_ASSERTeq(accessor->second.val, 4);
+	}
+
+	{
+		value_move_type e(5, 5);
+
+		UT_ASSERTeq(map_move->insert(std::move(e)), true);
+
+		persistent_map_move_type::accessor accessor;
+		UT_ASSERT(map_move->find(accessor, 5) == true);
+
+		UT_ASSERTeq(accessor->first, 5);
+		UT_ASSERTeq(accessor->second.val, 5);
+	}
+
+	{
+		value_move_type e(6, 6);
+
+		UT_ASSERTeq(map_move->insert(std::move(e)), true);
+
+		persistent_map_move_type::const_accessor accessor;
+		UT_ASSERT(map_move->find(accessor, 6) == true);
+
+		UT_ASSERTeq(accessor->first, 6);
+		UT_ASSERTeq(accessor->second.val, 6);
+	}
+
+	{
+		std::vector<value_type> v = {value_type(11, 11),
+					     value_type(12, 12),
+					     value_type(13, 13)};
+
+		map1->insert(v.begin(), v.end());
+
+		for (auto &e : v)
+			UT_ASSERTeq(map1->count(e.first), 1);
+	}
+
+	{
+		map1->insert(std::initializer_list<value_type>{
+			value_type(21, 21), value_type(22, 22)});
+
+		UT_ASSERTeq(map1->count(21), 1);
+		UT_ASSERTeq(map1->count(22), 1);
+	}
+
+	pmem::detail::destroy<persistent_map_type>(*map1);
+	pmem::detail::destroy<persistent_map_move_type>(*map_move);
+}
+}
+
+int
+main(int argc, char *argv[])
+{
+	START();
+
+	if (argc < 2) {
+		UT_FATAL("usage: %s file-name", argv[0]);
+	}
+
+	const char *path = argv[1];
+
+	nvobj::pool<root> pop;
+
+	try {
+		pop = nvobj::pool<root>::create(
+			path, LAYOUT, PMEMOBJ_MIN_POOL * 20, S_IWUSR | S_IRUSR);
+	} catch (pmem::pool_error &pe) {
+		UT_FATAL("!pool::create: %s %s", pe.what(), path);
+	}
+
+	ctor_test(pop);
+	assignment_test(pop);
+	access_test(pop);
+	swap_test(pop);
+	insert_test(pop);
+
+	pop.close();
+
+	return 0;
+}

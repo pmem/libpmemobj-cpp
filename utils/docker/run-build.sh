fbe6d5fadfa2f4f3c71396397d15663c2b8b99ff
Sergei Vinogradov <sergey.vinogradov@intel.com>
2018-07-30 20:48:09 +0300

    Persistent concurrent hash map data structure.
    Implementation is based on concurrent_hash_map from Intel TBB library.
    
    Test for persistent_concurrent_hash_map is built and executed only if
    Intel TBB library found by CMake.
    
    Update README for Intel TBB related tests.
    
    Extend Docker images to support Intel TBB
    
    Add clang-format checks for experimental folder
    
    Fix CPack issue with deb package

--- utils/docker/run-build.sh
+++ utils/docker/run-build.sh
@@ -115,7 +115,9 @@ cmake .. -DDEVELOPER_MODE=1 \
 			-DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
 			-DTRACE_TESTS=1 \
 			-DCOVERAGE=$COVERAGE \
-			-DUSE_LLVM_LIBCPP=0
+			-DUSE_LLVM_LIBCPP=0 \
+			-DUSE_TBB=1 \
+			-DTBB_DIR=/opt/tbb/cmake
 
 make -j2
 test_command tests_clang_release llvm
@@ -134,7 +136,9 @@ cmake .. -DDEVELOPER_MODE=1 \
 			-DTRACE_TESTS=1 \
 			-DCOVERAGE=$COVERAGE \
 			-DUSE_LLVM_LIBCPP=0 \
-			-DCXX_STANDARD=17
+			-DCXX_STANDARD=17 \
+			-DUSE_TBB=1 \
+			-DTBB_DIR=/opt/tbb/cmake
 
 make -j2
 test_command tests_clang_debug_cpp17 llvm
@@ -152,7 +156,9 @@ cmake .. -DDEVELOPER_MODE=1 \
 			-DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
 			-DTRACE_TESTS=1 \
 			-DCOVERAGE=$COVERAGE \
-			-DUSE_LLVM_LIBCPP=0
+			-DUSE_LLVM_LIBCPP=0 \
+			-DUSE_TBB=1 \
+			-DTBB_DIR=/opt/tbb/cmake
 
 make -j2
 test_command tests_gcc_debug
@@ -170,7 +176,9 @@ cmake .. -DCMAKE_BUILD_TYPE=Release \
 			-DTRACE_TESTS=1 \
 			-DCOVERAGE=$COVERAGE \
 			-DUSE_LLVM_LIBCPP=0 \
-			-DCXX_STANDARD=17
+			-DCXX_STANDARD=17 \
+			-DUSE_TBB=1 \
+			-DTBB_DIR=/opt/tbb/cmake
 
 make -j2
 test_command tests_gcc_release_cpp17

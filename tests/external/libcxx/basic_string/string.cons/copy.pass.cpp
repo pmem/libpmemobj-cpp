7f22935c896715f2ca45e664e1ca8163c4571bce
Igor Chorazewicz <igor.chorazewicz@intel.com>
2018-09-03 09:38:06 +0200

    Add tests for pmem::obj::experimental::string.

--- tests/external/libcxx/basic_string/string.cons/copy.pass.cpp
+++ tests/external/libcxx/basic_string/string.cons/copy.pass.cpp
@@ -6,45 +6,124 @@
 // Source Licenses. See LICENSE.TXT for details.
 //
 //===----------------------------------------------------------------------===//
+//
+// Copyright 2019, Intel Corporation
+//
+// Modified to test pmem::obj containers
+//
 
-// <string>
+#include "unittest.hpp"
 
-// basic_string(const basic_string<charT,traits,Allocator>& str);
+#include <libpmemobj++/experimental/string.hpp>
+#include <libpmemobj++/make_persistent.hpp>
+#include <libpmemobj++/persistent_ptr.hpp>
+#include <libpmemobj++/pool.hpp>
+#include <libpmemobj++/transaction.hpp>
 
-#include <string>
-#include <cassert>
+namespace pmem_exp = pmem::obj::experimental;
+namespace nvobj = pmem::obj;
 
-#include "test_macros.h"
-#include "test_allocator.h"
-#include "min_allocator.h"
+struct root {
+	nvobj::persistent_ptr<pmem_exp::string> s;
+};
 
 template <class S>
 void
-test(S s1)
+test(S &s1, pmem::obj::pool<root> &pop)
 {
-    S s2 = s1;
-    LIBCPP_ASSERT(s2.__invariants());
-    assert(s2 == s1);
-    assert(s2.capacity() >= s2.size());
-    assert(s2.get_allocator() == s1.get_allocator());
+	using T = typename pmem_exp::string::traits_type;
+	auto r = pop.root();
+
+	nvobj::transaction::run(pop, [&] {
+		r->s = nvobj::make_persistent<pmem_exp::string>(s1);
+	});
+
+	auto &s2 = *r->s;
+
+	// XXX: enable operator==
+	// UT_ASSERT(s2 == s1);
+
+	UT_ASSERT(s1.size() == s2.size());
+	UT_ASSERT(T::compare(s2.c_str(), s1.c_str(), s1.size()) == 0);
+	UT_ASSERT(s2.capacity() >= s2.size());
+
+	nvobj::transaction::run(pop, [&] {
+		UT_ASSERT(s2.c_str() == s2.data());
+		UT_ASSERT(s2.c_str() == s2.cdata());
+		UT_ASSERT(s2.c_str() ==
+			  static_cast<const pmem_exp::string &>(s2).data());
+	});
+
+	nvobj::transaction::run(
+		pop, [&] { nvobj::delete_persistent<pmem_exp::string>(r->s); });
 }
 
-int main()
+void
+run(pmem::obj::pool<root> &pop)
 {
-    {
-    typedef test_allocator<char> A;
-    typedef std::basic_string<char, std::char_traits<char>, A> S;
-    test(S(A(3)));
-    test(S("1", A(5)));
-    test(S("1234567890123456789012345678901234567890123456789012345678901234567890", A(7)));
-    }
-#if TEST_STD_VER >= 11
-    {
-    typedef min_allocator<char> A;
-    typedef std::basic_string<char, std::char_traits<char>, A> S;
-    test(S(A{}));
-    test(S("1", A()));
-    test(S("1234567890123456789012345678901234567890123456789012345678901234567890", A()));
-    }
-#endif
+	try {
+		nvobj::persistent_ptr<pmem_exp::string> s1, s2, s3, s4;
+
+		nvobj::transaction::run(pop, [&] {
+			s1 = nvobj::make_persistent<pmem_exp::string>();
+		});
+
+		nvobj::transaction::run(pop, [&] {
+			s2 = nvobj::make_persistent<pmem_exp::string>("1");
+		});
+
+		nvobj::transaction::run(pop, [&] {
+			s3 = nvobj::make_persistent<pmem_exp::string>(
+				"1234567890");
+		});
+
+		nvobj::transaction::run(pop, [&] {
+			s4 = nvobj::make_persistent<pmem_exp::string>(
+				"1234567890123456789012345678901234567890"
+				"1234567890123456789012345678901234567890"
+				"1234567890123456789012345678901234567890"
+				"1234567890");
+		});
+
+		test(*s1, pop);
+		test(*s2, pop);
+		test(*s3, pop);
+		test(*s4, pop);
+
+		nvobj::transaction::run(pop, [&] {
+			nvobj::delete_persistent<pmem_exp::string>(s1);
+			nvobj::delete_persistent<pmem_exp::string>(s2);
+			nvobj::delete_persistent<pmem_exp::string>(s3);
+			nvobj::delete_persistent<pmem_exp::string>(s4);
+		});
+
+	} catch (std::exception &e) {
+		UT_FATALexc(e);
+	}
+}
+
+int
+main(int argc, char *argv[])
+{
+	START();
+
+	if (argc != 2)
+		UT_FATAL("usage: %s file-name", argv[0]);
+
+	const char *path = argv[1];
+
+	pmem::obj::pool<root> pop;
+
+	try {
+		pop = pmem::obj::pool<root>::create(
+			path, "move.pass", PMEMOBJ_MIN_POOL, S_IWUSR | S_IRUSR);
+	} catch (...) {
+		UT_FATAL("!pmemobj_create: %s", path);
+	}
+
+	run(pop);
+
+	pop.close();
+
+	return 0;
 }

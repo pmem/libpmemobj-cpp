dba0460dc03bc6cb3826c98a5eab48ade3bebe07
Igor Chorazewicz <igor.chorazewicz@intel.com>
2018-11-09 14:37:13 +0100

    Move finding some packages to test/CMakeLists.txt

--- tests/ctest_helpers.cmake
+++ tests/ctest_helpers.cmake
@@ -94,14 +94,46 @@ endif()
 set(CMAKE_REQUIRED_FLAGS ${SAVED_CMAKE_REQUIRED_FLAGS})
 set(CMAKE_REQUIRED_INCLUDES ${SAVED_CMAKE_REQUIRED_INCLUDES})
 
-if(PKG_CONFIG_FOUND)
-	pkg_check_modules(CURSES QUIET ncurses)
-else()
-	# Specifies that we want FindCurses to find ncurses and not just any
-	# curses library
-	set(CURSES_NEED_NCURSES TRUE)
-	find_package(Curses QUIET)
-endif()
+function(find_packages)
+	if(PKG_CONFIG_FOUND)
+		pkg_check_modules(CURSES QUIET ncurses)
+	else()
+		# Specifies that we want FindCurses to find ncurses and not just any
+		# curses library
+		set(CURSES_NEED_NCURSES TRUE)
+		find_package(Curses QUIET)
+	endif()
+
+	if(PKG_CONFIG_FOUND)
+		pkg_check_modules(VALGRIND QUIET valgrind)
+	else()
+		find_package(VALGRIND QUIET)
+	endif()
+
+	if(PKG_CONFIG_FOUND)
+		pkg_check_modules(LIBUNWIND QUIET libunwind)
+	else()
+		find_package(LIBUNWIND QUIET)
+	endif()
+	if(NOT LIBUNWIND_FOUND)
+		message(WARNING "libunwind not found. Stack traces from tests will not be reliable")
+	endif()
+
+	if(NOT WIN32)
+		if(VALGRIND_FOUND)
+			set(ENV{PATH} ${VALGRIND_PREFIX}/bin:$ENV{PATH})
+			execute_process(COMMAND valgrind --tool=pmemcheck --help
+					RESULT_VARIABLE VALGRIND_PMEMCHECK_NOT_FOUND
+					OUTPUT_QUIET
+					ERROR_QUIET)
+			if(VALGRIND_PMEMCHECK_NOT_FOUND)
+				message(WARNING "Valgrind pmemcheck NOT found. Pmemcheck tests will not be performed.")
+			endif()
+		else()
+			message(WARNING "Valgrind not found. Valgrind tests will not be performed.")
+		endif()
+	endif()
+endfunction()
 
 function(build_test name)
 	# skip posix tests

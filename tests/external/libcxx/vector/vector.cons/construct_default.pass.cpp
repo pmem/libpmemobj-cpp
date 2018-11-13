890e9b39c39d2058517a9e3cbc22967b0c6795d9
Szymon Romik <szymon.romik@intel.com>
2018-11-13 19:15:05 +0100

    test: add vector construct_default.pass test

--- tests/external/libcxx/vector/vector.cons/construct_default.pass.cpp
+++ tests/external/libcxx/vector/vector.cons/construct_default.pass.cpp
@@ -6,89 +6,78 @@
 // Source Licenses. See LICENSE.TXT for details.
 //
 //===----------------------------------------------------------------------===//
- // <vector>
- // vector();
-// vector(const Alloc&);
- #include <vector>
-#include <cassert>
- #include "test_macros.h"
-#include "test_allocator.h"
-#include "../../../NotConstructible.h"
-#include "test_allocator.h"
-#include "min_allocator.h"
-#include "asan_testing.h"
- template <class C>
+//
+// Copyright 2018, Intel Corporation
+//
+// Modified to test pmem::obj containers
+//
+
+#include <unittest.hpp>
+
+#include <libpmemobj++/detail/common.hpp>
+#include <libpmemobj++/detail/life.hpp>
+#include <libpmemobj++/experimental/vector.hpp>
+#include <libpmemobj++/make_persistent.hpp>
+#include <libpmemobj++/pool.hpp>
+#include <libpmemobj++/transaction.hpp>
+
+#include <cstring>
+
+namespace nvobj = pmem::obj;
+namespace pmem_exp = nvobj::experimental;
+using vector_type = pmem_exp::vector<int>;
+
+struct foo {
+	vector_type v_1;
+	vector_type v_2 = {};
+};
+
+struct root {
+	nvobj::persistent_ptr<vector_type> v_pptr;
+	nvobj::persistent_ptr<foo> foo_pptr;
+};
+
+/**
+ * Test pmem::obj::experimental::vector default constructor.
+ *
+ * First case: call default constructor in three different ways and check if
+ * new container is empty. Expect no exception was thrown.
+ */
 void
-test0()
+test_default_ctor(nvobj::pool<struct root> &pop)
 {
-#if TEST_STD_VER > 14
-    static_assert((noexcept(C{})), "" );
-#elif TEST_STD_VER >= 11
-    static_assert((noexcept(C()) == noexcept(typename C::allocator_type())), "" );
-#endif
-    C c;
-    LIBCPP_ASSERT(c.__invariants());
-    assert(c.empty());
-    assert(c.get_allocator() == typename C::allocator_type());
-    LIBCPP_ASSERT(is_contiguous_container_asan_correct(c));
-#if TEST_STD_VER >= 11
-    C c1 = {};
-    LIBCPP_ASSERT(c1.__invariants());
-    assert(c1.empty());
-    assert(c1.get_allocator() == typename C::allocator_type());
-    LIBCPP_ASSERT(is_contiguous_container_asan_correct(c1));
-#endif
+	auto r = pop.root();
+	try {
+		nvobj::transaction::run(pop, [&] {
+			r->v_pptr = nvobj::make_persistent<vector_type>();
+			r->foo_pptr = nvobj::make_persistent<foo>();
+		});
+		UT_ASSERT(r->v_pptr->empty() == 1);
+		UT_ASSERT(r->foo_pptr->v_1.empty() == 1);
+		UT_ASSERT(r->foo_pptr->v_2.empty() == 1);
+	} catch (std::exception &e) {
+		std::cerr << e.what() << std::endl
+			  << std::strerror(nvobj::transaction::error())
+			  << std::endl;
+		UT_ASSERT(0);
+	}
 }
- template <class C>
-void
-test1(const typename C::allocator_type& a)
+
+int
+main(int argc, char *argv[])
 {
-#if TEST_STD_VER > 14
-    static_assert((noexcept(C{typename C::allocator_type{}})), "" );
-#elif TEST_STD_VER >= 11
-    static_assert((noexcept(C(typename C::allocator_type())) == std::is_nothrow_copy_constructible<typename C::allocator_type>::value), "" );
-#endif
-    C c(a);
-    LIBCPP_ASSERT(c.__invariants());
-    assert(c.empty());
-    assert(c.get_allocator() == a);
-    LIBCPP_ASSERT(is_contiguous_container_asan_correct(c));
-}
- int main()
-{
-    {
-    test0<std::vector<int> >();
-    test0<std::vector<NotConstructible> >();
-    test1<std::vector<int, test_allocator<int> > >(test_allocator<int>(3));
-    test1<std::vector<NotConstructible, test_allocator<NotConstructible> > >
-        (test_allocator<NotConstructible>(5));
-    }
-    {
-        std::vector<int, limited_allocator<int, 10> > v;
-        assert(v.empty());
-    }
-#if TEST_STD_VER >= 11
-    {
-    test0<std::vector<int, min_allocator<int>> >();
-    test0<std::vector<NotConstructible, min_allocator<NotConstructible>> >();
-    test1<std::vector<int, min_allocator<int> > >(min_allocator<int>{});
-    test1<std::vector<NotConstructible, min_allocator<NotConstructible> > >
-        (min_allocator<NotConstructible>{});
-    }
-    {
-        std::vector<int, min_allocator<int> > v;
-        assert(v.empty());
-    }
-     {
-    test0<std::vector<int, explicit_allocator<int>> >();
-    test0<std::vector<NotConstructible, explicit_allocator<NotConstructible>> >();
-    test1<std::vector<int, explicit_allocator<int> > >(explicit_allocator<int>{});
-    test1<std::vector<NotConstructible, explicit_allocator<NotConstructible> > >
-        (explicit_allocator<NotConstructible>{});
-    }
-    {
-        std::vector<int, explicit_allocator<int> > v;
-        assert(v.empty());
-    }
-#endif
+	START();
+	if (argc < 2) {
+		std::cerr << "usage: " << argv[0] << " file-name" << std::endl;
+		return 1;
+	}
+	auto path = argv[1];
+	auto pop = nvobj::pool<root>::create(
+		path, "VectorTest: construct_default.pass", PMEMOBJ_MIN_POOL,
+		S_IWUSR | S_IRUSR);
+	test_default_ctor(pop);
+
+	pop.close();
+
+	return 0;
 }

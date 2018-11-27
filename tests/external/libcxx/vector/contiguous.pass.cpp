789a878b17ac9c8070c73efa32e3a47c3e6d09ef
Szymon Romik <szymon.romik@intel.com>
2018-11-27 15:51:52 +0100

    test: add vector contiguous.pass test

--- tests/external/libcxx/vector/contiguous.pass.cpp
+++ tests/external/libcxx/vector/contiguous.pass.cpp
@@ -6,47 +6,70 @@
 // Source Licenses. See LICENSE.TXT for details.
 //
 //===----------------------------------------------------------------------===//
+//
+// Copyright 2018, Intel Corporation
+//
+// Modified to test pmem::obj containers
+//
 
-// <vector>
+#include "unittest.hpp"
 
-// An vector is a contiguous container
+#include "libpmemobj++/experimental/vector.hpp"
+#include "libpmemobj++/make_persistent.hpp"
+#include "libpmemobj++/pool.hpp"
+#include "libpmemobj++/transaction.hpp"
 
-#include <vector>
-#include <cassert>
+namespace nvobj = pmem::obj;
+namespace pmem_exp = nvobj::experimental;
 
-#include "test_allocator.h"
-#include "min_allocator.h"
+struct root {
+	nvobj::persistent_ptr<pmem_exp::vector<int>> v1;
+	nvobj::persistent_ptr<pmem_exp::vector<int>> v2;
+};
 
-template <class C>
-void test_contiguous ( const C &c )
+void
+test_contiguous(const pmem_exp::vector<int> &c)
 {
-    for ( size_t i = 0; i < c.size(); ++i )
-        assert ( *(c.begin() + static_cast<typename C::difference_type>(i)) == *(std::addressof(*c.begin()) + i));
+	for (size_t i = 0; i < c.size(); ++i)
+		UT_ASSERT(*(c.begin() +
+			    static_cast<typename pmem_exp::vector<
+				    int>::difference_type>(i)) ==
+			  *(std::addressof(*c.begin()) + i));
 }
 
-int main()
+int
+main(int argc, char *argv[])
 {
-    {
-    typedef int T;
-    typedef std::vector<T> C;
-    test_contiguous(C());
-    test_contiguous(C(3, 5));
-    }
+	START();
 
-    {
-    typedef double T;
-    typedef test_allocator<T> A;
-    typedef std::vector<T, A> C;
-    test_contiguous(C(A(3)));
-    test_contiguous(C(7, 9.0, A(5)));
-    }
-#if TEST_STD_VER >= 11
-    {
-    typedef double T;
-    typedef min_allocator<T> A;
-    typedef std::vector<T, A> C;
-    test_contiguous(C(A{}));
-    test_contiguous(C(9, 11.0, A{}));
-    }
-#endif
+	if (argc < 2) {
+		std::cerr << "usage: " << argv[0] << " file-name" << std::endl;
+		return 1;
+	}
+
+	auto path = argv[1];
+	auto pop =
+		nvobj::pool<root>::create(path, "VectorTest: contiguous.pass",
+					  PMEMOBJ_MIN_POOL, S_IWUSR | S_IRUSR);
+
+	auto r = pop.root();
+
+	try {
+		nvobj::transaction::run(pop, [&] {
+			r->v1 = nvobj::make_persistent<pmem_exp::vector<int>>();
+			r->v2 = nvobj::make_persistent<pmem_exp::vector<int>>(
+				3U, 5);
+		});
+		test_contiguous(*r->v1);
+		test_contiguous(*r->v2);
+	} catch (std::exception &e) {
+		std::cerr << e.what() << std::endl
+			  << std::strerror(nvobj::transaction::error())
+			  << std::endl;
+		UT_ASSERT(0);
+	}
+
+	pop.close();
+
+	return 0;
 }

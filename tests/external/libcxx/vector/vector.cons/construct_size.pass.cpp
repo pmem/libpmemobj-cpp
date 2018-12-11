93ea236b6e2a4ad9e297eddbbd2808052bc30b9f
Krzysztof Kajrewicz <krzysztof.kajrewicz@intel.com>
2018-12-11 17:16:26 +0100

    test: add pmem:obj:vector ctors tests

--- tests/external/libcxx/vector/vector.cons/construct_size.pass.cpp
+++ tests/external/libcxx/vector/vector.cons/construct_size.pass.cpp
@@ -7,7 +7,7 @@
 //
 //===----------------------------------------------------------------------===//
 //
-// Copyright 2018, Intel Corporation
+// Copyright 2018-2019, Intel Corporation
 //
 // Modified to test pmem::obj containers
 //

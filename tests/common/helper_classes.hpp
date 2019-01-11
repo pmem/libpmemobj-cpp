748a2eb50a71bd804ae2d5f1fcdd8921416bb4ad
Krzysztof Kajrewicz <krzysztof.kajrewicz@intel.com>
2019-01-11 08:08:12 +0100

    Remove redundant includes and fix comment

--- tests/common/helper_classes.hpp
+++ tests/common/helper_classes.hpp
@@ -103,10 +103,8 @@ struct emplace_constructible_and_move_insertable {
 	    : value(other.value), moved(other.moved + 1)
 	{
 	}
-	/*
-	 * Since move constructor is declared explicitly, copy connstructor
-	 * won't be generated.
-	 */
+	/* Since move constructor is user-declared, copy constructor and copy
+	 * assignment operator are not implicitly declared by compiler */
 };
 
 #endif /* HELPER_CLASSES_HPP */

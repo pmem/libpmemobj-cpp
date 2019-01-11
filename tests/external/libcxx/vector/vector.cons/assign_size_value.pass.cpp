4a62dfd52d2010ec44ca0946c6e18fa79ea58575
Krzysztof Kajrewicz <krzysztof.kajrewicz@intel.com>
2019-01-11 14:13:29 +0100

    test: modify libcxx's vector assign methods and operators tests

--- tests/external/libcxx/vector/vector.cons/assign_size_value.pass.cpp
+++ tests/external/libcxx/vector/vector.cons/assign_size_value.pass.cpp
@@ -6,49 +6,93 @@
 // Source Licenses. See LICENSE.TXT for details.
 //
 //===----------------------------------------------------------------------===//
+//
+// Copyright 2019, Intel Corporation
+//
+// Modified to test pmem::obj containers
+//
 
-// <vector>
+#include "unittest.hpp"
 
-// void assign(size_type n, const_reference v);
+#include <libpmemobj++/experimental/vector.hpp>
+#include <libpmemobj++/make_persistent.hpp>
 
-#include <vector>
-#include <algorithm>
-#include <cassert>
-#include <iostream>
+namespace nvobj = pmem::obj;
+namespace pmem_exp = nvobj::experimental;
+using C = pmem_exp::vector<int>;
 
-#include "min_allocator.h"
-#include "asan_testing.h"
+struct root {
+	nvobj::persistent_ptr<C> v1;
+	nvobj::persistent_ptr<C> v2;
+	nvobj::persistent_ptr<C> v3;
+};
 
-bool is6(int x) { return x == 6; }
-
-template <typename Vec>
-void test ( Vec &v )
+bool
+is6(int x)
 {
-    v.assign(5, 6);
-    assert(v.size() == 5);
-    assert(is_contiguous_container_asan_correct(v));
-    assert(std::all_of(v.begin(), v.end(), is6));
+	return x == 6;
 }
 
-int main()
+/**
+ * Test pmem::obj::experimental::vector fill version of assign method
+ *
+ * Replaces vector's contents with count copies of given value
+ * Validates size and elements of the vector.
+ */
+template <typename Vec>
+void
+test(nvobj::pool<struct root> &pop, Vec &v)
 {
-    {
-    typedef std::vector<int> V;
-    V d1;
-    V d2;
-    d2.reserve(10);  // no reallocation during assign.
-    test(d1);
-    test(d2);
-    }
+	v.assign(5, 6);
+	UT_ASSERT(v.size() == 5);
 
-#if TEST_STD_VER >= 11
-    {
-    typedef std::vector<int, min_allocator<int>> V;
-    V d1;
-    V d2;
-    d2.reserve(10);  // no reallocation during assign.
-    test(d1);
-    test(d2);
-    }
-#endif
+	try {
+		nvobj::transaction::run(
+			pop, [&] { std::all_of(v.begin(), v.end(), is6); });
+	} catch (std::exception &e) {
+		UT_FATALexc(e);
+	}
+}
+
+int
+main(int argc, char *argv[])
+{
+	START();
+
+	if (argc < 2) {
+		std::cerr << "usage: " << argv[0] << " file-name" << std::endl;
+		return 1;
+	}
+	auto path = argv[1];
+	auto pop = nvobj::pool<root>::create(
+		path, "VectorTest: assign_size_value.pass", PMEMOBJ_MIN_POOL,
+		S_IWUSR | S_IRUSR);
+
+	auto r = pop.root();
+
+	try {
+		nvobj::transaction::run(pop, [&] {
+			r->v1 = nvobj::make_persistent<C>();
+			r->v2 = nvobj::make_persistent<C>();
+			r->v3 = nvobj::make_persistent<C>(10U, 1);
+		});
+
+		r->v2->reserve(10); // no reallocation during assign
+
+		test<C>(pop, *r->v1);
+		test<C>(pop, *r->v2);
+		test<C>(pop, *r->v3);
+
+		nvobj::transaction::run(pop, [&] {
+			nvobj::delete_persistent<C>(r->v1);
+			nvobj::delete_persistent<C>(r->v2);
+			nvobj::delete_persistent<C>(r->v3);
+		});
+	} catch (std::exception &e) {
+		UT_FATALexc(e);
+	}
+
+	pop.close();
+
+	return 0;
 }

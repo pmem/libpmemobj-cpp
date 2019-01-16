510ec5e9f141740ccc64bff4af58ea4321a061c2
Krzysztof Kajrewicz <krzysztof.kajrewicz@intel.com>
2019-01-16 13:04:22 +0100

    test: modify libcxx tests for pmem::obj::vector capacity methods

--- tests/external/libcxx/vector/vector.capacity/reserve.pass.cpp
+++ tests/external/libcxx/vector/vector.capacity/reserve.pass.cpp
@@ -6,65 +6,87 @@
 // Source Licenses. See LICENSE.TXT for details.
 //
 //===----------------------------------------------------------------------===//
+//
+// Copyright 2019, Intel Corporation
+//
+// Modified to test pmem::obj containers
+//
 
-// <vector>
+#include "unittest.hpp"
 
-// void reserve(size_type n);
+#include <libpmemobj++/experimental/vector.hpp>
+#include <libpmemobj++/make_persistent.hpp>
 
-#include <vector>
-#include <cassert>
-#include "test_allocator.h"
-#include "min_allocator.h"
-#include "asan_testing.h"
+namespace nvobj = pmem::obj;
+namespace pmem_exp = nvobj::experimental;
 
-int main()
+using C = pmem_exp::vector<int>;
+
+struct root {
+	nvobj::persistent_ptr<C> v;
+};
+
+int
+main(int argc, char *argv[])
 {
-    {
-        std::vector<int> v;
-        v.reserve(10);
-        assert(v.capacity() >= 10);
-        assert(is_contiguous_container_asan_correct(v));
-    }
-    {
-        std::vector<int> v(100);
-        assert(v.capacity() == 100);
-        v.reserve(50);
-        assert(v.size() == 100);
-        assert(v.capacity() == 100);
-        v.reserve(150);
-        assert(v.size() == 100);
-        assert(v.capacity() == 150);
-        assert(is_contiguous_container_asan_correct(v));
-    }
-    {
-        // Add 1 for implementations that dynamically allocate a container proxy.
-        std::vector<int, limited_allocator<int, 250 + 1> > v(100);
-        assert(v.capacity() == 100);
-        v.reserve(50);
-        assert(v.size() == 100);
-        assert(v.capacity() == 100);
-        v.reserve(150);
-        assert(v.size() == 100);
-        assert(v.capacity() == 150);
-        assert(is_contiguous_container_asan_correct(v));
-    }
-#if TEST_STD_VER >= 11
-    {
-        std::vector<int, min_allocator<int>> v;
-        v.reserve(10);
-        assert(v.capacity() >= 10);
-        assert(is_contiguous_container_asan_correct(v));
-    }
-    {
-        std::vector<int, min_allocator<int>> v(100);
-        assert(v.capacity() == 100);
-        v.reserve(50);
-        assert(v.size() == 100);
-        assert(v.capacity() == 100);
-        v.reserve(150);
-        assert(v.size() == 100);
-        assert(v.capacity() == 150);
-        assert(is_contiguous_container_asan_correct(v));
-    }
-#endif
+	START();
+
+	if (argc < 2) {
+		std::cerr << "usage: " << argv[0] << " file-name" << std::endl;
+		return 1;
+	}
+
+	auto path = argv[1];
+	auto pop =
+		nvobj::pool<root>::create(path, "VectorTest: reserve",
+					  PMEMOBJ_MIN_POOL, S_IWUSR | S_IRUSR);
+
+	auto r = pop.root();
+
+	{
+		try {
+			nvobj::transaction::run(pop, [&] {
+				r->v = nvobj::make_persistent<C>();
+			});
+
+			r->v->reserve(10);
+			UT_ASSERT(r->v->capacity() >= 10);
+
+			nvobj::transaction::run(pop, [&] {
+				nvobj::delete_persistent<C>(r->v);
+			});
+
+		} catch (std::exception &e) {
+			UT_FATALexc(e);
+		}
+	}
+
+	{
+		try {
+			nvobj::transaction::run(pop, [&] {
+				r->v = nvobj::make_persistent<C>(100U);
+			});
+
+			UT_ASSERT(r->v->capacity() == 100);
+
+			r->v->reserve(50);
+			UT_ASSERT(r->v->size() == 100);
+			UT_ASSERT(r->v->capacity() == 100);
+
+			r->v->reserve(150);
+			UT_ASSERT(r->v->size() == 100);
+			UT_ASSERT(r->v->capacity() == 150);
+
+			nvobj::transaction::run(pop, [&] {
+				nvobj::delete_persistent<C>(r->v);
+			});
+
+		} catch (std::exception &e) {
+			UT_FATALexc(e);
+		}
+	}
+
+	pop.close();
+
+	return 0;
 }

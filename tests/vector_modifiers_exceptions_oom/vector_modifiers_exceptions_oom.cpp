2c70b6dcb2f5769a57ddc78940c4dc11608470dd
Krzysztof Kajrewicz <krzysztof.kajrewicz@intel.com>
2019-01-21 08:16:30 +0100

    test: add tests for pmem::obj::vector capacity methods

---
+++ tests/vector_modifiers_exceptions_oom/vector_modifiers_exceptions_oom.cpp
@@ -0,0 +1,128 @@
+/*
+ * Copyright 2019, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "unittest.hpp"
+
+#include <libpmemobj++/experimental/vector.hpp>
+#include <libpmemobj++/make_persistent.hpp>
+
+namespace nvobj = pmem::obj;
+namespace pmem_exp = nvobj::experimental;
+using C = pmem_exp::vector<int>;
+
+struct root {
+	nvobj::persistent_ptr<C> v;
+};
+
+/**
+ * Test pmem::obj::experimental::vector modifiers
+ *
+ * Increase size of the vector to value greater than pool size
+ * Methods under test:
+ * - resize()
+ * - resize() with value
+ * Expect pmem::transaction_allor_error exception is thrown
+ */
+void
+test(nvobj::pool<struct root> &pop)
+{
+	auto r = pop.root();
+
+	UT_ASSERT(r->v->capacity() == 100);
+	UT_ASSERT(r->v->size() == 100);
+
+	bool exception_thrown = false;
+
+	auto size = r->v->max_size();
+
+	/* test resize() */
+	try {
+		r->v->resize(size);
+		UT_ASSERT(0);
+	} catch (pmem::transaction_alloc_error &) {
+		exception_thrown = true;
+	} catch (std::exception &e) {
+		UT_FATALexc(e);
+	}
+
+	UT_ASSERT(exception_thrown);
+	UT_ASSERT(r->v->capacity() == 100);
+	UT_ASSERT(r->v->size() == 100);
+
+	/* test resize() with value */
+	try {
+		r->v->resize(size, 0);
+		UT_ASSERT(0);
+	} catch (pmem::transaction_alloc_error &) {
+		exception_thrown = true;
+	} catch (std::exception &e) {
+		UT_FATALexc(e);
+	}
+
+	UT_ASSERT(exception_thrown);
+	UT_ASSERT(r->v->capacity() == 100);
+	UT_ASSERT(r->v->size() == 100);
+}
+
+int
+main(int argc, char *argv[])
+{
+	START();
+
+	if (argc < 2) {
+		std::cerr << "usage: " << argv[0] << " file-name" << std::endl;
+		return 1;
+	}
+
+	auto path = argv[1];
+	auto pop = nvobj::pool<root>::create(
+		path, "VectorTest: modifiers_exceptions_oom", PMEMOBJ_MIN_POOL,
+		S_IWUSR | S_IRUSR);
+
+	auto r = pop.root();
+
+	try {
+		nvobj::transaction::run(
+			pop, [&] { r->v = nvobj::make_persistent<C>(100U); });
+
+		test(pop);
+
+		nvobj::transaction::run(
+			pop, [&] { nvobj::delete_persistent<C>(r->v); });
+	} catch (std::exception &e) {
+		UT_FATALexc(e);
+	}
+
+	pop.close();
+
+	return 0;
+}

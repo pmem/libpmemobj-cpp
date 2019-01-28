2f8e4510176c559ebcd778d9d64c692770c2995e
Szymon Romik <szymon.romik@intel.com>
2019-01-28 18:24:19 +0100

    test: modify vector_private tests

--- tests/valgrind_internal.hpp
+++ tests/valgrind_internal.hpp
@@ -39,6 +39,48 @@ extern unsigned On_memcheck;
 extern unsigned On_helgrind;
 extern unsigned On_drd;
 
+#if LIBPMEMOBJ_CPP_VG_PMEMCHECK_ENABLED
+
+#define VALGRIND_ADD_TO_TX(addr, len)                                          \
+	do {                                                                   \
+		if (On_pmemcheck)                                              \
+			VALGRIND_PMC_ADD_TO_TX(addr, len);                     \
+	} while (0)
+
+#define VALGRIND_SET_CLEAN(addr, len)                                          \
+	do {                                                                   \
+		if (On_pmemcheck)                                              \
+			VALGRIND_PMC_SET_CLEAN(addr, len);                     \
+	} while (0)
+
+#define VALGRIND_REMOVE_FROM_TX(addr, len)                                     \
+	do {                                                                   \
+		if (On_pmemcheck)                                              \
+			VALGRIND_PMC_REMOVE_FROM_TX(addr, len);                \
+	} while (0)
+
+#else
+
+#define VALGRIND_ADD_TO_TX(addr, len)                                          \
+	do {                                                                   \
+		(void)(addr);                                                  \
+		(void)(len);                                                   \
+	} while (0)
+
+#define VALGRIND_SET_CLEAN(addr, len)                                          \
+	do {                                                                   \
+		(void)(addr);                                                  \
+		(void)(len);                                                   \
+	} while (0)
+
+#define VALGRIND_REMOVE_FROM_TX(addr, len)                                     \
+	do {                                                                   \
+		(void)(addr);                                                  \
+		(void)(len);                                                   \
+	} while (0)
+
+#endif /* LIBPMEMOBJ_CPP_VG_PMEMCHECK_ENABLED */
+
 void set_valgrind_internals(void);
 
 #endif /* VALGRIND_INTERNAL_HPP */

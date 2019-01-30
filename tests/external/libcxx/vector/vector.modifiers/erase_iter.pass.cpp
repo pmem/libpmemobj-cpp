b3e4b568e22488c0cbba0f4dccc2bab0f188b5f0
Krzysztof Kajrewicz <krzysztof.kajrewicz@intel.com>
2019-01-30 09:25:08 +0100

    test: modify libcxx's vector modifiers tests

--- tests/external/libcxx/vector/vector.modifiers/erase_iter.pass.cpp
+++ tests/external/libcxx/vector/vector.modifiers/erase_iter.pass.cpp
@@ -6,98 +6,136 @@
 // Source Licenses. See LICENSE.TXT for details.
 //
 //===----------------------------------------------------------------------===//
+//
+// Copyright 2019, Intel Corporation
+//
+// Modified to test pmem::obj containers
+//
 
-// <vector>
+#include "unittest.hpp"
 
-// iterator erase(const_iterator position);
+#include <libpmemobj++/experimental/vector.hpp>
+#include <libpmemobj++/make_persistent.hpp>
 
-#include <vector>
 #include <iterator>
-#include <cassert>
 
-#include "min_allocator.h"
-#include "asan_testing.h"
+namespace nvobj = pmem::obj;
+namespace pmem_exp = nvobj::experimental;
 
-#ifndef TEST_HAS_NO_EXCEPTIONS
+struct Throws;
+
+using C = pmem_exp::vector<int>;
+using C2 = pmem_exp::vector<Throws>;
+using std::distance;
+using std::next;
+
+struct root {
+	nvobj::persistent_ptr<C> l1;
+	nvobj::persistent_ptr<C2> v;
+};
+
 struct Throws {
-    Throws() : v_(0) {}
-    Throws(int v) : v_(v) {}
-    Throws(const Throws  &rhs) : v_(rhs.v_) { if (sThrows) throw 1; }
-    Throws(      Throws &&rhs) : v_(rhs.v_) { if (sThrows) throw 1; }
-    Throws& operator=(const Throws  &rhs) { v_ = rhs.v_; return *this; }
-    Throws& operator=(      Throws &&rhs) { v_ = rhs.v_; return *this; }
-    int v_;
-    static bool sThrows;
-    };
+	Throws() : v_(0)
+	{
+	}
+	Throws(int v) : v_(v)
+	{
+	}
+	Throws(const Throws &rhs) : v_(rhs.v_)
+	{
+		if (sThrows)
+			throw 1;
+	}
+	Throws(Throws &&rhs) : v_(rhs.v_)
+	{
+		if (sThrows)
+			throw 1;
+	}
+	Throws &
+	operator=(const Throws &rhs)
+	{
+		v_ = rhs.v_;
+		return *this;
+	}
+	Throws &
+	operator=(Throws &&rhs)
+	{
+		v_ = rhs.v_;
+		return *this;
+	}
+	int v_;
+	static bool sThrows;
+};
 
 bool Throws::sThrows = false;
-#endif
 
-int main()
+int
+main(int argc, char *argv[])
 {
-    {
-    int a1[] = {1, 2, 3};
-    std::vector<int> l1(a1, a1+3);
-    std::vector<int>::const_iterator i = l1.begin();
-    assert(is_contiguous_container_asan_correct(l1));
-    ++i;
-    std::vector<int>::iterator j = l1.erase(i);
-    assert(l1.size() == 2);
-    assert(distance(l1.begin(), l1.end()) == 2);
-    assert(*j == 3);
-    assert(*l1.begin() == 1);
-    assert(*next(l1.begin()) == 3);
-    assert(is_contiguous_container_asan_correct(l1));
-    j = l1.erase(j);
-    assert(j == l1.end());
-    assert(l1.size() == 1);
-    assert(distance(l1.begin(), l1.end()) == 1);
-    assert(*l1.begin() == 1);
-    assert(is_contiguous_container_asan_correct(l1));
-    j = l1.erase(l1.begin());
-    assert(j == l1.end());
-    assert(l1.size() == 0);
-    assert(distance(l1.begin(), l1.end()) == 0);
-    assert(is_contiguous_container_asan_correct(l1));
-    }
-#if TEST_STD_VER >= 11
-    {
-    int a1[] = {1, 2, 3};
-    std::vector<int, min_allocator<int>> l1(a1, a1+3);
-    std::vector<int, min_allocator<int>>::const_iterator i = l1.begin();
-    assert(is_contiguous_container_asan_correct(l1));
-    ++i;
-    std::vector<int, min_allocator<int>>::iterator j = l1.erase(i);
-    assert(l1.size() == 2);
-    assert(distance(l1.begin(), l1.end()) == 2);
-    assert(*j == 3);
-    assert(*l1.begin() == 1);
-    assert(*next(l1.begin()) == 3);
-    assert(is_contiguous_container_asan_correct(l1));
-    j = l1.erase(j);
-    assert(j == l1.end());
-    assert(l1.size() == 1);
-    assert(distance(l1.begin(), l1.end()) == 1);
-    assert(*l1.begin() == 1);
-    assert(is_contiguous_container_asan_correct(l1));
-    j = l1.erase(l1.begin());
-    assert(j == l1.end());
-    assert(l1.size() == 0);
-    assert(distance(l1.begin(), l1.end()) == 0);
-    assert(is_contiguous_container_asan_correct(l1));
-    }
-#endif
-#ifndef TEST_HAS_NO_EXCEPTIONS
-// Test for LWG2853:
-// Throws: Nothing unless an exception is thrown by the assignment operator or move assignment operator of T.
-    {
-    Throws arr[] = {1, 2, 3};
-    std::vector<Throws> v(arr, arr+3);
-    Throws::sThrows = true;
-    v.erase(v.begin());
-    v.erase(--v.end());
-    v.erase(v.begin());
-    assert(v.size() == 0);
-    }
-#endif
+	START();
+
+	if (argc < 2) {
+		std::cerr << "usage: " << argv[0] << " file-name" << std::endl;
+		return 1;
+	}
+
+	auto path = argv[1];
+	auto pop =
+		nvobj::pool<root>::create(path, "VectorTest: erase_iter",
+					  PMEMOBJ_MIN_POOL, S_IWUSR | S_IRUSR);
+
+	auto r = pop.root();
+	{
+		int a1[] = {1, 2, 3};
+		try {
+			nvobj::transaction::run(pop, [&] {
+				r->l1 = nvobj::make_persistent<C>(a1, a1 + 3);
+			});
+			C::const_iterator i = r->l1->begin();
+			++i;
+			C::iterator j = r->l1->erase(i);
+			UT_ASSERT(r->l1->size() == 2);
+			UT_ASSERT(distance(r->l1->begin(), r->l1->end()) == 2);
+			UT_ASSERT(*j == 3);
+			UT_ASSERT(*r->l1->begin() == 1);
+			UT_ASSERT(*std::next(r->l1->begin()) == 3);
+			j = r->l1->erase(j);
+			UT_ASSERT(j == r->l1->end());
+			UT_ASSERT(r->l1->size() == 1);
+			UT_ASSERT(distance(r->l1->begin(), r->l1->end()) == 1);
+			UT_ASSERT(*r->l1->begin() == 1);
+			j = r->l1->erase(r->l1->begin());
+			UT_ASSERT(j == r->l1->end());
+			UT_ASSERT(r->l1->size() == 0);
+			UT_ASSERT(distance(r->l1->begin(), r->l1->end()) == 0);
+
+			nvobj::transaction::run(pop, [&] {
+				nvobj::delete_persistent<C>(r->l1);
+			});
+		} catch (std::exception &e) {
+			UT_FATALexc(e);
+		}
+	}
+	{
+		Throws arr[] = {1, 2, 3};
+		try {
+			nvobj::transaction::run(pop, [&] {
+				r->v = nvobj::make_persistent<C2>(arr, arr + 3);
+			});
+			Throws::sThrows = true;
+			r->v->erase(r->v->begin());
+			r->v->erase(--r->v->end());
+			r->v->erase(r->v->begin());
+			UT_ASSERT(r->v->size() == 0);
+			nvobj::transaction::run(pop, [&] {
+				nvobj::delete_persistent<C2>(r->v);
+			});
+		} catch (std::exception &e) {
+			UT_FATALexc(e);
+		}
+	}
+
+	pop.close();
+
+	return 0;
 }

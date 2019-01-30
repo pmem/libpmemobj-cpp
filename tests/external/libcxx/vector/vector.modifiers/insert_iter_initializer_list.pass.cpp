a914d4d9a0a064dfb0d0edc112a10fe0e26cf966
Krzysztof Kajrewicz <krzysztof.kajrewicz@intel.com>
2019-01-30 09:25:08 +0100

    test: modify libcxx's vector modifiers tests

--- tests/external/libcxx/vector/vector.modifiers/insert_iter_initializer_list.pass.cpp
+++ tests/external/libcxx/vector/vector.modifiers/insert_iter_initializer_list.pass.cpp
@@ -6,61 +6,72 @@
 // Source Licenses. See LICENSE.TXT for details.
 //
 //===----------------------------------------------------------------------===//
+//
+// Copyright 2019, Intel Corporation
+//
+// Modified to test pmem::obj containers
+//
 
-// UNSUPPORTED: c++98, c++03
+#include "unittest.hpp"
 
-// <vector>
+#include <libpmemobj++/experimental/vector.hpp>
+#include <libpmemobj++/make_persistent.hpp>
 
-// iterator insert(const_iterator p, initializer_list<value_type> il);
+namespace nvobj = pmem::obj;
+namespace pmem_exp = nvobj::experimental;
 
-#include <vector>
-#include <cassert>
+struct Throws;
 
-#include "min_allocator.h"
-#include "asan_testing.h"
+using C = pmem_exp::vector<int>;
 
-int main()
+struct root {
+	nvobj::persistent_ptr<C> d;
+};
+
+int
+main(int argc, char *argv[])
 {
-    {
-    std::vector<int> d(10, 1);
-    std::vector<int>::iterator i = d.insert(d.cbegin() + 2, {3, 4, 5, 6});
-    assert(d.size() == 14);
-    assert(is_contiguous_container_asan_correct(d));
-    assert(i == d.begin() + 2);
-    assert(d[0] == 1);
-    assert(d[1] == 1);
-    assert(d[2] == 3);
-    assert(d[3] == 4);
-    assert(d[4] == 5);
-    assert(d[5] == 6);
-    assert(d[6] == 1);
-    assert(d[7] == 1);
-    assert(d[8] == 1);
-    assert(d[9] == 1);
-    assert(d[10] == 1);
-    assert(d[11] == 1);
-    assert(d[12] == 1);
-    assert(d[13] == 1);
-    }
-    {
-    std::vector<int, min_allocator<int>> d(10, 1);
-    std::vector<int, min_allocator<int>>::iterator i = d.insert(d.cbegin() + 2, {3, 4, 5, 6});
-    assert(d.size() == 14);
-    assert(is_contiguous_container_asan_correct(d));
-    assert(i == d.begin() + 2);
-    assert(d[0] == 1);
-    assert(d[1] == 1);
-    assert(d[2] == 3);
-    assert(d[3] == 4);
-    assert(d[4] == 5);
-    assert(d[5] == 6);
-    assert(d[6] == 1);
-    assert(d[7] == 1);
-    assert(d[8] == 1);
-    assert(d[9] == 1);
-    assert(d[10] == 1);
-    assert(d[11] == 1);
-    assert(d[12] == 1);
-    assert(d[13] == 1);
-    }
+	START();
+
+	if (argc < 2) {
+		std::cerr << "usage: " << argv[0] << " file-name" << std::endl;
+		return 1;
+	}
+
+	auto path = argv[1];
+	auto pop = nvobj::pool<root>::create(
+		path, "VectorTest: insert_iter_initializer_list",
+		PMEMOBJ_MIN_POOL, S_IWUSR | S_IRUSR);
+
+	auto r = pop.root();
+
+	try {
+		nvobj::transaction::run(
+			pop, [&] { r->d = nvobj::make_persistent<C>(10U, 1); });
+		C::iterator i = r->d->insert(r->d->cbegin() + 2, {3, 4, 5, 6});
+		UT_ASSERT(r->d->size() == 14);
+		UT_ASSERT(i == r->d->begin() + 2);
+		UT_ASSERT((*r->d)[0] == 1);
+		UT_ASSERT((*r->d)[1] == 1);
+		UT_ASSERT((*r->d)[2] == 3);
+		UT_ASSERT((*r->d)[3] == 4);
+		UT_ASSERT((*r->d)[4] == 5);
+		UT_ASSERT((*r->d)[5] == 6);
+		UT_ASSERT((*r->d)[6] == 1);
+		UT_ASSERT((*r->d)[7] == 1);
+		UT_ASSERT((*r->d)[8] == 1);
+		UT_ASSERT((*r->d)[9] == 1);
+		UT_ASSERT((*r->d)[10] == 1);
+		UT_ASSERT((*r->d)[11] == 1);
+		UT_ASSERT((*r->d)[12] == 1);
+		UT_ASSERT((*r->d)[13] == 1);
+		nvobj::transaction::run(
+			pop, [&] { nvobj::delete_persistent<C>(r->d); });
+	} catch (std::exception &e) {
+		UT_FATALexc(e);
+	}
+
+	pop.close();
+
+	return 0;
 }
--- tests/external/libcxx/vector/vector.modifiers/insert_iter_iter_iter.pass.cpp
+++ tests/external/libcxx/vector/vector.modifiers/insert_iter_iter_iter.pass.cpp
@@ -6,162 +6,161 @@
 // Source Licenses. See LICENSE.TXT for details.
 //
 //===----------------------------------------------------------------------===//
+//
+// Copyright 2019, Intel Corporation
+//
+// Modified to test pmem::obj containers
+//
 
-// <vector>
+#include "iterators_support.hpp"
+#include "unittest.hpp"
 
-// template <class Iter>
-//   iterator insert(const_iterator position, Iter first, Iter last);
+#include <libpmemobj++/experimental/vector.hpp>
+#include <libpmemobj++/make_persistent.hpp>
 
-#include <vector>
-#include <cassert>
-#include <cstddef>
+namespace nvobj = pmem::obj;
+namespace pmem_exp = nvobj::experimental;
 
-#include "test_macros.h"
-#include "test_allocator.h"
-#include "test_iterators.h"
-#include "min_allocator.h"
-#include "asan_testing.h"
+struct Throws;
 
-int main()
+using C = pmem_exp::vector<int>;
+
+struct root {
+	nvobj::persistent_ptr<C> v;
+};
+
+int
+main(int argc, char *argv[])
 {
-    {
-        std::vector<int> v(100);
-        int a[] = {1, 2, 3, 4, 5};
-        const int N = sizeof(a)/sizeof(a[0]);
-        std::vector<int>::iterator i = v.insert(v.cbegin() + 10, input_iterator<const int*>(a),
-                                        input_iterator<const int*>(a+N));
-        assert(v.size() == 100 + N);
-        assert(is_contiguous_container_asan_correct(v));
-        assert(i == v.begin() + 10);
-        int j;
-        for (j = 0; j < 10; ++j)
-            assert(v[j] == 0);
-        for (std::size_t k = 0; k < N; ++j, ++k)
-            assert(v[j] == a[k]);
-        for (; j < 105; ++j)
-            assert(v[j] == 0);
-    }
-    {
-        std::vector<int> v(100);
-        int a[] = {1, 2, 3, 4, 5};
-        const int N = sizeof(a)/sizeof(a[0]);
-        std::vector<int>::iterator i = v.insert(v.cbegin() + 10, forward_iterator<const int*>(a),
-                                        forward_iterator<const int*>(a+N));
-        assert(v.size() == 100 + N);
-        assert(is_contiguous_container_asan_correct(v));
-        assert(i == v.begin() + 10);
-        int j;
-        for (j = 0; j < 10; ++j)
-            assert(v[j] == 0);
-        for (std::size_t k = 0; k < N; ++j, ++k)
-            assert(v[j] == a[k]);
-        for (; j < 105; ++j)
-            assert(v[j] == 0);
-    }
-    {
-        std::vector<int> v(100);
-        while(v.size() < v.capacity()) v.push_back(0); // force reallocation
-        size_t sz = v.size();
-        int a[] = {1, 2, 3, 4, 5};
-        const unsigned N = sizeof(a)/sizeof(a[0]);
-        std::vector<int>::iterator i = v.insert(v.cbegin() + 10, forward_iterator<const int*>(a),
-                                        forward_iterator<const int*>(a+N));
-        assert(v.size() == sz + N);
-        assert(i == v.begin() + 10);
-        std::size_t j;
-        for (j = 0; j < 10; ++j)
-            assert(v[j] == 0);
-        for (std::size_t k = 0; k < N; ++j, ++k)
-            assert(v[j] == a[k]);
-        for (; j < v.size(); ++j)
-            assert(v[j] == 0);
-    }
-    {
-        std::vector<int> v(100);
-        v.reserve(128); // force no reallocation
-        size_t sz = v.size();
-        int a[] = {1, 2, 3, 4, 5};
-        const unsigned N = sizeof(a)/sizeof(a[0]);
-        std::vector<int>::iterator i = v.insert(v.cbegin() + 10, forward_iterator<const int*>(a),
-                                        forward_iterator<const int*>(a+N));
-        assert(v.size() == sz + N);
-        assert(i == v.begin() + 10);
-        std::size_t j;
-        for (j = 0; j < 10; ++j)
-            assert(v[j] == 0);
-        for (std::size_t k = 0; k < N; ++j, ++k)
-            assert(v[j] == a[k]);
-        for (; j < v.size(); ++j)
-            assert(v[j] == 0);
-    }
-    {
-        std::vector<int, limited_allocator<int, 308> > v(100);
-        int a[] = {1, 2, 3, 4, 5};
-        const int N = sizeof(a)/sizeof(a[0]);
-        std::vector<int>::iterator i = v.insert(v.cbegin() + 10, input_iterator<const int*>(a),
-                                        input_iterator<const int*>(a+N));
-        assert(v.size() == 100 + N);
-        assert(is_contiguous_container_asan_correct(v));
-        assert(i == v.begin() + 10);
-        int j;
-        for (j = 0; j < 10; ++j)
-            assert(v[j] == 0);
-        for (std::size_t k = 0; k < N; ++j, ++k)
-            assert(v[j] == a[k]);
-        for (; j < 105; ++j)
-            assert(v[j] == 0);
-    }
-    {
-        std::vector<int, limited_allocator<int, 300> > v(100);
-        int a[] = {1, 2, 3, 4, 5};
-        const int N = sizeof(a)/sizeof(a[0]);
-        std::vector<int>::iterator i = v.insert(v.cbegin() + 10, forward_iterator<const int*>(a),
-                                        forward_iterator<const int*>(a+N));
-        assert(v.size() == 100 + N);
-        assert(is_contiguous_container_asan_correct(v));
-        assert(i == v.begin() + 10);
-        int j;
-        for (j = 0; j < 10; ++j)
-            assert(v[j] == 0);
-        for (std::size_t k = 0; k < N; ++j, ++k)
-            assert(v[j] == a[k]);
-        for (; j < 105; ++j)
-            assert(v[j] == 0);
-    }
-#if TEST_STD_VER >= 11
-    {
-        std::vector<int, min_allocator<int>> v(100);
-        int a[] = {1, 2, 3, 4, 5};
-        const int N = sizeof(a)/sizeof(a[0]);
-        std::vector<int, min_allocator<int>>::iterator i = v.insert(v.cbegin() + 10, input_iterator<const int*>(a),
-                                        input_iterator<const int*>(a+N));
-        assert(v.size() == 100 + N);
-        assert(is_contiguous_container_asan_correct(v));
-        assert(i == v.begin() + 10);
-        int j;
-        for (j = 0; j < 10; ++j)
-            assert(v[j] == 0);
-        for (std::size_t k = 0; k < N; ++j, ++k)
-            assert(v[j] == a[k]);
-        for (; j < 105; ++j)
-            assert(v[j] == 0);
-    }
-    {
-        std::vector<int, min_allocator<int>> v(100);
-        int a[] = {1, 2, 3, 4, 5};
-        const int N = sizeof(a)/sizeof(a[0]);
-        std::vector<int, min_allocator<int>>::iterator i = v.insert(v.cbegin() + 10, forward_iterator<const int*>(a),
-                                        forward_iterator<const int*>(a+N));
-        assert(v.size() == 100 + N);
-        assert(is_contiguous_container_asan_correct(v));
-        assert(i == v.begin() + 10);
-        int j;
-        for (j = 0; j < 10; ++j)
-            assert(v[j] == 0);
-        for (std::size_t k = 0; k < N; ++j, ++k)
-            assert(v[j] == a[k]);
-        for (; j < 105; ++j)
-            assert(v[j] == 0);
-    }
-#endif
+	START();
+
+	if (argc < 2) {
+		std::cerr << "usage: " << argv[0] << " file-name" << std::endl;
+		return 1;
+	}
+
+	auto path = argv[1];
+	auto pop = nvobj::pool<root>::create(
+		path, "VectorTest: insert_iter_iter_iter", PMEMOBJ_MIN_POOL,
+		S_IWUSR | S_IRUSR);
+
+	auto r = pop.root();
+
+	{
+		try {
+			nvobj::transaction::run(pop, [&] {
+				r->v = nvobj::make_persistent<C>(100U);
+			});
+			int a[] = {1, 2, 3, 4, 5};
+			const int N = sizeof(a) / sizeof(a[0]);
+			C::iterator i = r->v->insert(
+				r->v->cbegin() + 10,
+				test_support::input_it<const int *>(a),
+				test_support::input_it<const int *>(a + N));
+			UT_ASSERT(r->v->size() == 100 + N);
+			UT_ASSERT(i == r->v->begin() + 10);
+			unsigned j;
+			for (j = 0; j < 10; ++j)
+				UT_ASSERT((*r->v)[j] == 0);
+			for (std::size_t k = 0; k < N; ++j, ++k)
+				UT_ASSERT((*r->v)[j] == a[k]);
+			for (; j < 105; ++j)
+				UT_ASSERT((*r->v)[j] == 0);
+			nvobj::transaction::run(pop, [&] {
+				nvobj::delete_persistent<C>(r->v);
+			});
+		} catch (std::exception &e) {
+			UT_FATALexc(e);
+		}
+	}
+	{
+		try {
+			nvobj::transaction::run(pop, [&] {
+				r->v = nvobj::make_persistent<C>(100U);
+			});
+			int a[] = {1, 2, 3, 4, 5};
+			const int N = sizeof(a) / sizeof(a[0]);
+			C::iterator i = r->v->insert(
+				r->v->cbegin() + 10,
+				test_support::forward_it<const int *>(a),
+				test_support::forward_it<const int *>(a + N));
+			UT_ASSERT(r->v->size() == 100 + N);
+			UT_ASSERT(i == r->v->begin() + 10);
+			unsigned j;
+			for (j = 0; j < 10; ++j)
+				UT_ASSERT((*r->v)[j] == 0);
+			for (std::size_t k = 0; k < N; ++j, ++k)
+				UT_ASSERT((*r->v)[j] == a[k]);
+			for (; j < 105; ++j)
+				UT_ASSERT((*r->v)[j] == 0);
+			nvobj::transaction::run(pop, [&] {
+				nvobj::delete_persistent<C>(r->v);
+			});
+		} catch (std::exception &e) {
+			UT_FATALexc(e);
+		}
+	}
+	{
+		try {
+			nvobj::transaction::run(pop, [&] {
+				r->v = nvobj::make_persistent<C>(100U);
+			});
+			while (r->v->size() < r->v->capacity())
+				r->v->push_back(0); // force reallocation
+			size_t sz = r->v->size();
+			int a[] = {1, 2, 3, 4, 5};
+			const int N = sizeof(a) / sizeof(a[0]);
+			C::iterator i = r->v->insert(
+				r->v->cbegin() + 10,
+				test_support::forward_it<const int *>(a),
+				test_support::forward_it<const int *>(a + N));
+			UT_ASSERT(r->v->size() == sz + N);
+			UT_ASSERT(i == r->v->begin() + 10);
+			std::size_t j;
+			for (j = 0; j < 10; ++j)
+				UT_ASSERT((*r->v)[j] == 0);
+			for (std::size_t k = 0; k < N; ++j, ++k)
+				UT_ASSERT((*r->v)[j] == a[k]);
+			for (; j < r->v->size(); ++j)
+				UT_ASSERT((*r->v)[j] == 0);
+			nvobj::transaction::run(pop, [&] {
+				nvobj::delete_persistent<C>(r->v);
+			});
+		} catch (std::exception &e) {
+			UT_FATALexc(e);
+		}
+	}
+	{
+		try {
+			nvobj::transaction::run(pop, [&] {
+				r->v = nvobj::make_persistent<C>(100U);
+			});
+			r->v->reserve(128); // force no reallocation
+			size_t sz = r->v->size();
+			int a[] = {1, 2, 3, 4, 5};
+			const int N = sizeof(a) / sizeof(a[0]);
+			C::iterator i = r->v->insert(
+				r->v->cbegin() + 10,
+				test_support::forward_it<const int *>(a),
+				test_support::forward_it<const int *>(a + N));
+			UT_ASSERT(r->v->size() == sz + N);
+			UT_ASSERT(i == r->v->begin() + 10);
+			std::size_t j;
+			for (j = 0; j < 10; ++j)
+				UT_ASSERT((*r->v)[j] == 0);
+			for (std::size_t k = 0; k < N; ++j, ++k)
+				UT_ASSERT((*r->v)[j] == a[k]);
+			for (; j < r->v->size(); ++j)
+				UT_ASSERT((*r->v)[j] == 0);
+			nvobj::transaction::run(pop, [&] {
+				nvobj::delete_persistent<C>(r->v);
+			});
+		} catch (std::exception &e) {
+			UT_FATALexc(e);
+		}
+	}
+
+	pop.close();
+
+	return 0;
 }

7f6baf4216c562077a9405cb105c9c5df6e4eaa9
Szymon Romik <szymon.romik@intel.com>
2019-01-30 16:32:01 +0100

    add tracer detection
    add support for situation, if we don't want to enable valgrind
    tool A instrumentation, when test is ran with Valgrind tool B

--- tests/valgrind_internal.cpp
+++ tests/valgrind_internal.cpp
@@ -34,12 +34,26 @@
 #include <libpmemobj++/detail/common.hpp>
 
 unsigned On_valgrind = 0;
+unsigned On_pmemcheck = 0;
+unsigned On_memcheck = 0;
+unsigned On_helgrind = 0;
+unsigned On_drd = 0;
 
 void
 set_valgrind_internals(void)
 {
 #if LIBPMEMOBJ_CPP_ANY_VG_TOOL_ENABLED
         On_valgrind = RUNNING_ON_VALGRIND;
+
+        if (getenv("LIBPMEMOBJ_CPP_TRACER_PMEMCHECK"))
+                On_pmemcheck = 1;
+        else if (getenv("LIBPMEMOBJ_CPP_TRACER_MEMCHECK"))
+                On_memcheck = 1;
+        else if (getenv("LIBPMEMOBJ_CPP_TRACER_HELGRIND"))
+                On_helgrind = 1;
+        else if (getenv("LIBPMEMOBJ_CPP_TRACER_DRD"))
+                On_drd = 1;
+
 #endif /* LIBPMEMOBJ_CPP_ANY_VG_TOOL_ENABLED */
 }
 

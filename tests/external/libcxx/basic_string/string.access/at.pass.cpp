aafadbed73e1e69ac600b7e1f788d29a4e4806df
Krzysztof Kajrewicz <krzysztof.kajrewicz@intel.com>
2019-02-11 09:05:26 +0100

    test: modify libcxx's string access tests

--- tests/external/libcxx/basic_string/string.access/at.pass.cpp
+++ tests/external/libcxx/basic_string/string.access/at.pass.cpp
@@ -6,73 +6,98 @@
 // Source Licenses. See LICENSE.TXT for details.
 //
 //===----------------------------------------------------------------------===//
+//
+// Copyright 2019, Intel Corporation
+//
+// Modified to test pmem::obj containers
+//
 
-// <string>
+#include "unittest.hpp"
 
-// const_reference at(size_type pos) const;
-//       reference at(size_type pos);
+#include <libpmemobj++/experimental/string.hpp>
+#include <libpmemobj++/make_persistent.hpp>
 
-#include <string>
-#include <stdexcept>
-#include <cassert>
+namespace nvobj = pmem::obj;
+namespace pmem_exp = nvobj::experimental;
+using C = pmem_exp::string;
 
-#include "min_allocator.h"
-
-#include "test_macros.h"
+struct root {
+	nvobj::persistent_ptr<C> s1;
+	nvobj::persistent_ptr<C> s2;
+	nvobj::persistent_ptr<C> s3;
+};
 
 template <class S>
 void
-test(S s, typename S::size_type pos)
+test(S &s, typename S::size_type pos)
 {
-    const S& cs = s;
-    if (pos < s.size())
-    {
-        assert(s.at(pos) == s[pos]);
-        assert(cs.at(pos) == cs[pos]);
-    }
-#ifndef TEST_HAS_NO_EXCEPTIONS
-    else
-    {
-        try
-        {
-            TEST_IGNORE_NODISCARD s.at(pos);
-            assert(false);
-        }
-        catch (std::out_of_range&)
-        {
-            assert(pos >= s.size());
-        }
-        try
-        {
-            TEST_IGNORE_NODISCARD cs.at(pos);
-            assert(false);
-        }
-        catch (std::out_of_range&)
-        {
-            assert(pos >= s.size());
-        }
-    }
-#endif
+	const S &cs = s;
+
+	if (pos < s.size()) {
+		UT_ASSERT(s.at(pos) == s[pos]);
+		UT_ASSERT(cs.at(pos) == cs[pos]);
+	} else {
+		try {
+			s.at(pos);
+			UT_ASSERT(false);
+		} catch (std::out_of_range &) {
+			UT_ASSERT(pos >= s.size());
+		}
+		try {
+			cs.at(pos);
+			UT_ASSERT(false);
+		} catch (std::out_of_range &) {
+			UT_ASSERT(pos >= s.size());
+		}
+	}
 }
 
-int main()
+int
+main(int argc, char *argv[])
 {
-    {
-    typedef std::string S;
-    test(S(), 0);
-    test(S("123"), 0);
-    test(S("123"), 1);
-    test(S("123"), 2);
-    test(S("123"), 3);
-    }
-#if TEST_STD_VER >= 11
-    {
-    typedef std::basic_string<char, std::char_traits<char>, min_allocator<char>> S;
-    test(S(), 0);
-    test(S("123"), 0);
-    test(S("123"), 1);
-    test(S("123"), 2);
-    test(S("123"), 3);
-    }
-#endif
+	START();
+
+	if (argc < 2) {
+		std::cerr << "usage: " << argv[0] << " file-name" << std::endl;
+		return 1;
+	}
+
+	auto path = argv[1];
+	auto pop = nvobj::pool<root>::create(
+		path, "StringTest: at", PMEMOBJ_MIN_POOL, S_IWUSR | S_IRUSR);
+
+	auto r = pop.root();
+
+	try {
+		nvobj::transaction::run(pop, [&] {
+			r->s1 = nvobj::make_persistent<C>();
+			r->s2 = nvobj::make_persistent<C>("123");
+			r->s3 = nvobj::make_persistent<C>(
+				"1234567890123456789012345678901234567890"
+				"1234567890123456789012345678901234567890"
+				"1234567890123456789012345678901234567890"
+				"1234567890");
+		});
+
+		test(*r->s1, 0);
+		test(*r->s2, 0);
+		test(*r->s2, 1);
+		test(*r->s2, 2);
+		test(*r->s2, 3);
+		test(*r->s3, 0);
+		test(*r->s3, 64);
+		test(*r->s3, r->s3->size());
+
+		nvobj::transaction::run(pop, [&] {
+			nvobj::delete_persistent<C>(r->s1);
+			nvobj::delete_persistent<C>(r->s2);
+			nvobj::delete_persistent<C>(r->s3);
+		});
+	} catch (std::exception &e) {
+		UT_FATALexc(e);
+	}
+
+	pop.close();
+
+	return 0;
 }

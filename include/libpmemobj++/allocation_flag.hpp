8637c8bedd69166af5520293c15b45054b085e83
Szymon Romik <szymon.romik@intel.com>
2019-02-13 16:47:29 +0100

    fix allocation_flag issues

--- include/libpmemobj++/allocation_flag.hpp
+++ include/libpmemobj++/allocation_flag.hpp
@@ -61,12 +61,19 @@ struct allocation_flag {
 	allocation_flag() = delete;
 
 	/**
+	 * Emplace constructor.
+	 */
+	allocation_flag(uint64_t val) : value(val)
+	{
+	}
+
+	/**
 	 * Allocate the object from the allocation class with id equal to id.
 	 */
 	static allocation_flag
 	class_id(uint64_t id)
 	{
-		return {POBJ_CLASS_ID(id)};
+		return allocation_flag(POBJ_CLASS_ID(id));
 	}
 
 	/**
@@ -75,7 +82,7 @@ struct allocation_flag {
 	static allocation_flag
 	no_flush()
 	{
-		return {POBJ_XALLOC_NO_FLUSH};
+		return allocation_flag(POBJ_XALLOC_NO_FLUSH);
 	}
 
 	/**
@@ -84,7 +91,7 @@ struct allocation_flag {
 	static allocation_flag
 	none()
 	{
-		return {0};
+		return allocation_flag(0);
 	}
 
 	/**
@@ -99,7 +106,7 @@ struct allocation_flag {
 	allocation_flag
 	operator|(const allocation_flag &rhs)
 	{
-		return {value | rhs.value};
+		return allocation_flag(value | rhs.value);
 	}
 
 	uint64_t value;
@@ -119,12 +126,19 @@ struct allocation_flag_atomic {
 	allocation_flag_atomic() = delete;
 
 	/**
+	 * Emplace constructor.
+	 */
+	allocation_flag_atomic(uint64_t val) : value(val)
+	{
+	}
+
+	/**
 	 * Allocate the object from the allocation class with id equal to id.
 	 */
 	static allocation_flag_atomic
 	class_id(uint64_t id)
 	{
-		return {POBJ_CLASS_ID(id)};
+		return allocation_flag_atomic(POBJ_CLASS_ID(id));
 	}
 
 	/**
@@ -133,7 +147,7 @@ struct allocation_flag_atomic {
 	static allocation_flag_atomic
 	none()
 	{
-		return {0};
+		return allocation_flag_atomic(0);
 	}
 
 	/**
@@ -148,7 +162,7 @@ struct allocation_flag_atomic {
 	allocation_flag_atomic
 	operator|(const allocation_flag_atomic &rhs)
 	{
-		return {value | rhs.value};
+		return allocation_flag_atomic(value | rhs.value);
 	}
 
 	uint64_t value;

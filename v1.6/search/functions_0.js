83236e8e92c6194439fd018eeba82303f2841700
Krzysztof Kajrewicz <krzysztof.kajrewicz@intel.com>
2019-03-14 12:56:50 +0100

    doc: generate documentation for stable-1.6

---
+++ v1.6/search/functions_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['_5fget_5fdata',['_get_data',['../structpmem_1_1obj_1_1experimental_1_1array.html#acd4a8b75a55671257b31544cbfcbc7d4',1,'pmem::obj::experimental::array::_get_data()'],['../structpmem_1_1obj_1_1experimental_1_1array.html#a0ed361e699dd981960e24140d2e726f9',1,'pmem::obj::experimental::array::_get_data() const'],['../structpmem_1_1obj_1_1experimental_1_1array.html#a79a636fb783adf2bdae0036c9402bf5d',1,'pmem::obj::experimental::array::_get_data()'],['../structpmem_1_1obj_1_1experimental_1_1array.html#a9566ccf6e8d3ae15ed5c2cdc68b8476d',1,'pmem::obj::experimental::array::_get_data() const']]],
+  ['_5fget_5fpool',['_get_pool',['../structpmem_1_1obj_1_1experimental_1_1array.html#adb629f6f4cae47a3547f7f3be600d113',1,'pmem::obj::experimental::array']]]
+];

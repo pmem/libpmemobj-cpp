1cadd78a49a76d44ac057dac678b529e52a069ed
Sergei Vinogradov <sergey.vinogradov@intel.com>
2019-08-08 15:28:06 +0300

    Concurrent map data structure
    Implementation is based on concurrent skip list.
    Ref:
    https://www.cs.tau.ac.il/~shanir/nir-pubs-web/Papers/OPODIS2006-BA.pdf

---
+++ tests/concurrent_map_pmreorder_simple/concurrent_map_pmreorder_simple.cpp
@@ -0,0 +1,131 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2020, Intel Corporation */
+
+/*
+ * concurrent_map_reorder_simple.cpp -- pmem::obj::experimental::concurrent_map
+ * test
+ *
+ */
+
+#include "unittest.hpp"
+
+#include <libpmemobj++/make_persistent.hpp>
+#include <libpmemobj++/p.hpp>
+#include <libpmemobj++/persistent_ptr.hpp>
+#include <libpmemobj++/pool.hpp>
+
+#include <future>
+#include <iostream>
+
+#include <libpmemobj++/experimental/concurrent_map.hpp>
+
+#define LAYOUT "persistent_concurrent_map"
+
+namespace nvobj = pmem::obj;
+
+namespace
+{
+
+typedef nvobj::experimental::concurrent_map<nvobj::p<int>, nvobj::p<int>>
+	persistent_map_type;
+
+typedef persistent_map_type::value_type value_type;
+
+struct root {
+	nvobj::persistent_ptr<persistent_map_type> cons;
+};
+
+static constexpr int elements[] = {1, 2, 257, 513};
+
+/*
+ * test_map -- (internal) test
+ * pmem::obj::experimental::concurrent_map<nvobj::p<int>, nvobj::p<int> >
+ */
+void
+test_insert(nvobj::pool<root> &pop)
+{
+	auto persistent_map = pop.root()->cons;
+
+	persistent_map->runtime_initialize();
+
+	persistent_map->insert(value_type(elements[1], elements[1]));
+	persistent_map->insert(value_type(elements[2], elements[2]));
+	persistent_map->insert(value_type(elements[3], elements[3]));
+
+	{
+		UT_ASSERTeq(persistent_map->contains(elements[2]), true);
+	}
+}
+
+void
+check_consistency(nvobj::pool<root> &pop)
+{
+	auto persistent_map = pop.root()->cons;
+
+	persistent_map->runtime_initialize();
+
+	auto size = static_cast<typename persistent_map_type::difference_type>(
+		persistent_map->size());
+
+	UT_ASSERTeq(
+		std::distance(persistent_map->begin(), persistent_map->end()),
+		size);
+
+	for (int i = 0; i < size; i++) {
+		UT_ASSERTeq(persistent_map->count(elements[i]), 1);
+
+		auto it = persistent_map->find(elements[i]);
+		UT_ASSERTeq(it != persistent_map->end(), true);
+
+		UT_ASSERTeq(it->first, elements[i]);
+		UT_ASSERTeq(it->second, elements[i]);
+	}
+
+	for (int i = size;
+	     i < static_cast<int>(sizeof(elements) / sizeof(elements[0]));
+	     i++) {
+		UT_ASSERTeq(persistent_map->count(elements[i]), 0);
+	}
+}
+}
+
+int
+main(int argc, char *argv[])
+{
+	if (argc != 3 || strchr("coi", argv[1][0]) == nullptr)
+		UT_FATAL("usage: %s <c|o|i> file-name", argv[0]);
+
+	const char *path = argv[2];
+
+	nvobj::pool<root> pop;
+
+	try {
+		if (argv[1][0] == 'o') {
+			pop = nvobj::pool<root>::open(path, LAYOUT);
+
+			check_consistency(pop);
+		} else if (argv[1][0] == 'c') {
+			pop = nvobj::pool<root>::create(path, LAYOUT,
+							PMEMOBJ_MIN_POOL * 20,
+							S_IWUSR | S_IRUSR);
+
+			pmem::obj::transaction::run(pop, [&] {
+				pop.root()->cons = nvobj::make_persistent<
+					persistent_map_type>();
+			});
+
+			pop.root()->cons->insert(
+				value_type(elements[0], elements[0]));
+		} else if (argv[1][0] == 'i') {
+			pop = nvobj::pool<root>::open(path, LAYOUT);
+
+			test_insert(pop);
+		}
+	} catch (pmem::pool_error &pe) {
+		UT_FATAL("!pool::create: %s %s", pe.what(), path);
+	}
+
+	pop.close();
+
+	return 0;
+}

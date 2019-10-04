164ffc919330d8bb7dc33e7d4da81beb0f33086f
Igor Chorazewicz <igor.chorazewicz@intel.com>
2019-10-04 13:57:53 +0200

    Add test with concurrent_hash_map and pmem::obj::string

--- tests/concurrent_hash_map_rehash/concurrent_hash_map_rehash.cpp
+++ tests/concurrent_hash_map_rehash/concurrent_hash_map_rehash.cpp
@@ -47,6 +47,7 @@
 #include <vector>
 
 #include <libpmemobj++/container/concurrent_hash_map.hpp>
+#include <libpmemobj++/container/string.hpp>
 
 #define LAYOUT "concurrent_hash_map"
 
@@ -55,11 +56,51 @@ namespace nvobj = pmem::obj;
 namespace
 {
 
+class key_equal {
+public:
+	template <typename M, typename U>
+	bool
+	operator()(const M &lhs, const U &rhs) const
+	{
+		return lhs == rhs;
+	}
+};
+
+class string_hasher {
+	/* hash multiplier used by fibonacci hashing */
+	static const size_t hash_multiplier = 11400714819323198485ULL;
+
+public:
+	using transparent_key_equal = key_equal;
+
+	size_t
+	operator()(const pmem::obj::string &str) const
+	{
+		return hash(str.c_str(), str.size());
+	}
+
+private:
+	size_t
+	hash(const char *str, size_t size) const
+	{
+		size_t h = 0;
+		for (size_t i = 0; i < size; ++i) {
+			h = static_cast<size_t>(str[i]) ^ (h * hash_multiplier);
+		}
+		return h;
+	}
+};
+
 typedef nvobj::concurrent_hash_map<nvobj::p<int>, nvobj::p<int>>
 	persistent_map_type;
 
+typedef nvobj::concurrent_hash_map<pmem::obj::string, pmem::obj::string,
+				   string_hasher>
+	persistent_map_type_str;
+
 struct root {
 	nvobj::persistent_ptr<persistent_map_type> cons;
+	nvobj::persistent_ptr<persistent_map_type_str> cons_str;
 };
 
 /*
@@ -127,6 +168,87 @@ insert_erase_lookup_test(nvobj::pool<root> &pop)
 		UT_ASSERT(e.first <= e.second);
 	}
 }
+
+/*
+ * insert_and_erase_test_str -- (internal) test insert and erase operations
+ * pmem::obj::concurrent_hash_map<pmem::obj::string, pmem::obj::string>
+ */
+void
+insert_erase_lookup_test_str(nvobj::pool<root> &pop)
+{
+	const size_t NUMBER_ITEMS_INSERT = 500;
+
+	// Adding more concurrency will increase DRD test time
+	const size_t concurrency = 6;
+
+	auto map = pop.root()->cons_str;
+
+	UT_ASSERT(map != nullptr);
+
+	map->runtime_initialize();
+
+	std::vector<std::thread> threads;
+	threads.reserve(concurrency);
+
+	pmem::obj::persistent_ptr<persistent_map_type_str::value_type>
+		ptr[NUMBER_ITEMS_INSERT];
+
+	pmem::obj::transaction::run(pop, [&] {
+		for (int i = 0; i < static_cast<int>(NUMBER_ITEMS_INSERT);
+		     i++) {
+			ptr[i] = pmem::obj::make_persistent<
+				persistent_map_type_str::value_type>("1234",
+								     "1234");
+		}
+	});
+
+	for (size_t i = 0; i < concurrency; ++i) {
+		threads.emplace_back([&]() {
+			for (int i = 0;
+			     i < static_cast<int>(NUMBER_ITEMS_INSERT); ++i) {
+				map->insert(*(ptr[i]));
+			}
+		});
+	}
+
+	for (size_t i = 0; i < concurrency; ++i) {
+		threads.emplace_back([&]() {
+			for (int i = 0;
+			     i < static_cast<int>(NUMBER_ITEMS_INSERT); ++i) {
+				map->erase(ptr[i]->first);
+			}
+		});
+	}
+
+	for (size_t i = 0; i < concurrency; ++i) {
+		threads.emplace_back([&]() {
+			for (int i = 0;
+			     i < static_cast<int>(NUMBER_ITEMS_INSERT); ++i) {
+				persistent_map_type_str::accessor acc;
+				bool res = map->find(acc, ptr[i]->first);
+
+				if (res) {
+					UT_ASSERT(acc->first ==
+						  (ptr[i])->first);
+					acc->second.assign(
+						"012345678901234567890");
+				}
+			}
+		});
+	}
+
+	for (auto &t : threads) {
+		t.join();
+	}
+
+	pmem::obj::transaction::run(pop, [&] {
+		for (int i = 0; i < static_cast<int>(NUMBER_ITEMS_INSERT);
+		     i++) {
+			pmem::obj::delete_persistent<
+				persistent_map_type_str::value_type>(ptr[i]);
+		}
+	});
+}
 }
 
 int
@@ -148,12 +270,16 @@ main(int argc, char *argv[])
 		pmem::obj::transaction::run(pop, [&] {
 			pop.root()->cons =
 				nvobj::make_persistent<persistent_map_type>();
+
+			pop.root()->cons_str = nvobj::make_persistent<
+				persistent_map_type_str>();
 		});
 	} catch (pmem::pool_error &pe) {
 		UT_FATAL("!pool::create: %s %s", pe.what(), path);
 	}
 
 	insert_erase_lookup_test(pop);
+	insert_erase_lookup_test_str(pop);
 
 	pop.close();
 

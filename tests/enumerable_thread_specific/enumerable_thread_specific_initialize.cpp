ff3c8bf86a817db46ceba357c61c072a5b8f7c1e
Valeev Arslan <arslan.valeev@intel.com>
2019-10-17 13:07:30 +0300

    Implementation of enumerable_thread_specific for persistent memory
    Interface of the data structure is similar to tbb::enumerable_thread_specific

---
+++ tests/enumerable_thread_specific/enumerable_thread_specific_initialize.cpp
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2019, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "unittest.hpp"
+
+#include <libpmemobj++/experimental/enumerable_thread_specific.hpp>
+#include <libpmemobj++/make_persistent.hpp>
+
+#include <set>
+
+namespace nvobj = pmem::obj;
+
+using test_t = size_t;
+using container_type = nvobj::experimental::enumerable_thread_specific<test_t>;
+
+struct root {
+	nvobj::persistent_ptr<container_type> pptr;
+};
+
+template <typename Function>
+void
+parallel_exec(size_t concurrency, Function f)
+{
+	std::vector<std::thread> threads;
+	threads.reserve(concurrency);
+
+	for (size_t i = 0; i < concurrency; ++i) {
+		threads.emplace_back(f, i);
+	}
+
+	for (auto &t : threads) {
+		t.join();
+	}
+}
+
+void
+create_and_fill(nvobj::pool<struct root> &pop, size_t concurrency)
+{
+	auto &tls = pop.root()->pptr;
+
+	UT_ASSERT(tls == nullptr);
+
+	nvobj::transaction::run(
+		pop, [&] { tls = nvobj::make_persistent<container_type>(); });
+	parallel_exec(concurrency, [&](size_t thread_index) {
+		tls->local() = thread_index;
+	});
+	UT_ASSERT(tls->size() == concurrency);
+}
+
+void
+check_and_delete(nvobj::pool<struct root> &pop, size_t concurrency)
+{
+	auto &tls = pop.root()->pptr;
+
+	std::set<size_t> checker;
+	tls->initialize([&checker](test_t &e) {
+		UT_ASSERT(checker.emplace(e).second);
+	});
+	UT_ASSERT(checker.size() == concurrency);
+	UT_ASSERT(tls->empty());
+
+	nvobj::transaction::run(
+		pop, [&] { nvobj::delete_persistent<container_type>(tls); });
+}
+
+int
+main(int argc, char *argv[])
+{
+	START();
+
+	if (argc < 2) {
+		std::cerr << "usage: " << argv[0] << " file-name" << std::endl;
+		return 1;
+	}
+
+	auto path = argv[1];
+	auto layout = "TLSTest: enumerable_thread_specific_initialize";
+	auto pop = nvobj::pool<root>::create(path, layout, PMEMOBJ_MIN_POOL,
+					     S_IWUSR | S_IRUSR);
+	// Adding more concurrency will increase DRD test time
+	size_t concurrency = 16;
+
+	create_and_fill(pop, concurrency);
+
+	pop.close();
+	pop = nvobj::pool<root>::open(path, layout);
+
+	check_and_delete(pop, concurrency);
+
+	pop.close();
+	return 0;
+}

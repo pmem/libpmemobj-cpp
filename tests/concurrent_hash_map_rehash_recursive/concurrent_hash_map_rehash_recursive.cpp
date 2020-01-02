69b537b73f4e2724bcec0dc548daf0b6f5507a84
Lukasz Dorau <lukasz.dorau@intel.com>
2020-01-02 14:31:03 +0100

    test: add recursive rehashing tests for defrag()

---
+++ tests/concurrent_hash_map_rehash_recursive/concurrent_hash_map_rehash_recursive.cpp
@@ -0,0 +1,183 @@
+/*
+ * Copyright 2019-2020, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * concurrent_hash_map_rehash_recursive.cpp -- pmem::obj::concurrent_hash_map
+ * test
+ *
+ */
+
+#include "unittest.hpp"
+
+#include <libpmemobj++/make_persistent.hpp>
+#include <libpmemobj++/p.hpp>
+#include <libpmemobj++/persistent_ptr.hpp>
+#include <libpmemobj++/pool.hpp>
+
+#include <iterator>
+#include <thread>
+#include <vector>
+
+#include <libpmemobj++/container/concurrent_hash_map.hpp>
+#include <libpmemobj++/container/string.hpp>
+
+#define LAYOUT "concurrent_hash_map"
+
+namespace nvobj = pmem::obj;
+
+namespace
+{
+
+class key_equal {
+public:
+	template <typename M, typename U>
+	bool
+	operator()(const M &lhs, const U &rhs) const
+	{
+		return lhs == rhs;
+	}
+};
+
+class string_hasher {
+	/* hash multiplier used by fibonacci hashing */
+	static const size_t hash_multiplier = 11400714819323198485ULL;
+
+public:
+	using transparent_key_equal = key_equal;
+
+	size_t
+	operator()(const pmem::obj::string &str) const
+	{
+		return hash(str.c_str(), str.size());
+	}
+
+private:
+	size_t
+	hash(const char *str, size_t size) const
+	{
+		size_t h = 0;
+		for (size_t i = 0; i < size; ++i) {
+			h = static_cast<size_t>(str[i]) ^ (h * hash_multiplier);
+		}
+		return h;
+	}
+};
+
+typedef nvobj::concurrent_hash_map<nvobj::p<int>, nvobj::p<int>>
+	persistent_map_type;
+
+typedef nvobj::concurrent_hash_map<pmem::obj::string, pmem::obj::string,
+				   string_hasher>
+	persistent_map_type_str;
+
+struct root {
+	nvobj::persistent_ptr<persistent_map_type> cons;
+};
+
+/*
+ * recursive_rehashing_test -- (internal) test recursive rehashing in
+ * pmem::obj::concurrent_hash_map<nvobj::p<int>, nvobj::p<int> >
+ */
+void
+recursive_rehashing_test(nvobj::pool<root> &pop, size_t concurrency = 4)
+{
+
+	PRINT_TEST_PARAMS;
+
+	auto map = pop.root()->cons;
+
+	UT_ASSERT(map != nullptr);
+
+	map->runtime_initialize();
+
+	int skip = 0;
+	for (int i = 4095; i >= 2048; i--) {
+		int hb = i & 255;
+		if (hb == 128)
+			skip = 1;
+		if (skip && hb >= 128)
+			continue;
+		map->insert(persistent_map_type::value_type(i, i));
+	}
+
+	std::vector<std::thread> threads;
+	threads.reserve(concurrency);
+
+	threads.emplace_back([&]() {
+		for (int i = 4095; i >= 2048; i--) {
+			persistent_map_type::accessor acc;
+			map->find(acc, i);
+		}
+	});
+
+	threads.emplace_back([&]() {
+		for (int i = 4095; i >= 4070; i--) {
+			map->defrag();
+		}
+	});
+
+	for (auto &t : threads) {
+		t.join();
+	}
+}
+}
+
+int
+main(int argc, char *argv[])
+{
+	START();
+
+	if (argc < 2) {
+		UT_FATAL("usage: %s file-name", argv[0]);
+	}
+
+	const char *path = argv[1];
+
+	nvobj::pool<root> pop;
+
+	try {
+		pop = nvobj::pool<root>::create(
+			path, LAYOUT, PMEMOBJ_MIN_POOL * 20, S_IWUSR | S_IRUSR);
+		pmem::obj::transaction::run(pop, [&] {
+			pop.root()->cons =
+				nvobj::make_persistent<persistent_map_type>();
+		});
+	} catch (pmem::pool_error &pe) {
+		UT_FATAL("!pool::create: %s %s", pe.what(), path);
+	}
+
+	recursive_rehashing_test(pop);
+
+	pop.close();
+
+	return 0;
+}

35fd6f1451e598a398d4be647c9d746081356955
Igor Chorazewicz <igor.chorazewicz@intel.com>
2020-01-03 12:47:57 +0100

    concurrent_hash_map: deprecate runtime_initialize(bool)
    The bool parameter is not needed anymore, size is always restored from
    'consistent_size'.

--- include/libpmemobj++/container/concurrent_hash_map.hpp
+++ include/libpmemobj++/container/concurrent_hash_map.hpp
@@ -2077,7 +2077,7 @@ public:
 	 */
 	concurrent_hash_map() : hash_map_base()
 	{
-		runtime_initialize(true);
+		runtime_initialize();
 	}
 
 	/**
@@ -2086,7 +2086,7 @@ public:
 	 */
 	concurrent_hash_map(size_type n) : hash_map_base()
 	{
-		runtime_initialize(true);
+		runtime_initialize();
 
 		reserve(n);
 	}
@@ -2096,7 +2096,7 @@ public:
 	 */
 	concurrent_hash_map(const concurrent_hash_map &table) : hash_map_base()
 	{
-		runtime_initialize(true);
+		runtime_initialize();
 
 		reserve(table.size());
 
@@ -2108,7 +2108,7 @@ public:
 	 */
 	concurrent_hash_map(concurrent_hash_map &&table) : hash_map_base()
 	{
-		runtime_initialize(true);
+		runtime_initialize();
 
 		swap(table);
 	}
@@ -2119,7 +2119,7 @@ public:
 	template <typename I>
 	concurrent_hash_map(I first, I last)
 	{
-		runtime_initialize(true);
+		runtime_initialize();
 
 		reserve(static_cast<size_type>(std::distance(first, last)));
 
@@ -2131,7 +2131,7 @@ public:
 	 */
 	concurrent_hash_map(std::initializer_list<value_type> il)
 	{
-		runtime_initialize(true);
+		runtime_initialize();
 
 		reserve(il.size());
 
@@ -2147,7 +2147,7 @@ public:
 	 * version of libpmemobj-cpp
 	 */
 	void
-	runtime_initialize(bool graceful_shutdown = false)
+	runtime_initialize()
 	{
 		check_incompat_features();
 
@@ -2169,6 +2169,13 @@ public:
 
 		assert(this->size() ==
 		       size_type(std::distance(this->begin(), this->end())));
+	}
+
+	[[deprecated(
+		"runtime_initialize(bool) is now deprecated, use runtime_initialize(void)")]] void
+	runtime_initialize(bool)
+	{
+		runtime_initialize();
 	}
 
 	/**

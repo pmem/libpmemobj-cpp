c5c5a0cd71cff371fdb7c15711cfb38af02c7195
Igor Chorazewicz <igor.chorazewicz@intel.com>
2020-01-17 12:13:54 +0100

    tests: implement parallel_exec_with_sync
    This is needed for enumerable_thread_specific tests
    in which we need guarantee on number of threads executing
    simultaneously.

--- tests/common/thread_helpers.hpp
+++ tests/common/thread_helpers.hpp
@@ -32,7 +32,8 @@
 #ifndef THREAD_HELPERS_COMMON_HPP
 #define THREAD_HELPERS_COMMON_HPP
 
-#include <atomic>
+#include <condition_variable>
+#include <mutex>
 #include <thread>
 #include <vector>
 
@@ -50,6 +51,37 @@ parallel_exec(size_t concurrency, Function f)
 	for (auto &t : threads) {
 		t.join();
 	}
+}
+
+/*
+ * This function executes 'concurrency' threads and wait for all of them to
+ * finish executing f before calling join().
+ */
+template <typename Function>
+void
+parallel_exec_with_sync(size_t concurrency, Function f)
+{
+	std::condition_variable cv;
+	std::mutex m;
+	size_t counter = 0;
+
+	parallel_exec(concurrency, [&](size_t tid) {
+		f(tid);
+
+		{
+			std::unique_lock<std::mutex> lock(m);
+			counter++;
+			if (counter < concurrency)
+				cv.wait(lock);
+			else
+				/*
+				 * notify_call could be called outside of a lock
+				 * (it would perform better) but drd complains
+				 * in that case
+				 */
+				cv.notify_all();
+		}
+	});
 }
 
 #endif /* THREAD_HELPERS_COMMON_HPP */

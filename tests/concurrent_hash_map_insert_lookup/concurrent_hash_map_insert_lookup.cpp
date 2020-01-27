58c155bafe9d26d29646bc70913f65a2b6b7ddb1
Lukasz Dorau <lukasz.dorau@intel.com>
2020-01-27 10:20:26 +0100

    test: add mocks for pmemobj_defrag() function

--- tests/concurrent_hash_map_insert_lookup/concurrent_hash_map_insert_lookup.cpp
+++ tests/concurrent_hash_map_insert_lookup/concurrent_hash_map_insert_lookup.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2019, Intel Corporation
+ * Copyright 2018-2020, Intel Corporation
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -36,6 +36,7 @@
  */
 
 #include "../concurrent_hash_map/concurrent_hash_map_test.hpp"
+#include "tests/wrap_pmemobj_defrag.h"
 #include "unittest.hpp"
 
 int

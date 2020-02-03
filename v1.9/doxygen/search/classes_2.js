5c4fa8b4be3486a86b9e28a69c60a1b769532893
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2020-02-03 14:48:43 +0100

    docs: add gh-pages content for v1.9

---
+++ v1.9/doxygen/search/classes_2.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['concurrent_5fhash_5fmap',['concurrent_hash_map',['../classpmem_1_1obj_1_1concurrent__hash__map.html',1,'pmem::obj']]],
+  ['condition_5fvariable',['condition_variable',['../classpmem_1_1obj_1_1condition__variable.html',1,'pmem::obj']]],
+  ['const_5faccessor',['const_accessor',['../classpmem_1_1obj_1_1concurrent__hash__map_1_1const__accessor.html',1,'pmem::obj::concurrent_hash_map']]],
+  ['contiguous_5fiterator',['contiguous_iterator',['../structpmem_1_1detail_1_1contiguous__iterator.html',1,'pmem::detail']]],
+  ['contiguous_5fiterator_3c_20basic_5fcontiguous_5fiterator_3c_20t_20_3e_2c_20t_20_26_2c_20t_20_2a_20_3e',['contiguous_iterator&lt; basic_contiguous_iterator&lt; T &gt;, T &amp;, T * &gt;',['../structpmem_1_1detail_1_1contiguous__iterator.html',1,'pmem::detail']]],
+  ['contiguous_5fiterator_3c_20range_5fsnapshotting_5fiterator_3c_20t_20_3e_2c_20t_20_26_2c_20t_20_2a_20_3e',['contiguous_iterator&lt; range_snapshotting_iterator&lt; T &gt;, T &amp;, T * &gt;',['../structpmem_1_1detail_1_1contiguous__iterator.html',1,'pmem::detail']]],
+  ['ctl_5ferror',['ctl_error',['../classpmem_1_1ctl__error.html',1,'pmem']]]
+];
---
+++ v1.9/doxygen/search/files_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['basic_5fstring_2ehpp',['basic_string.hpp',['../basic__string_8hpp.html',1,'']]]
+];

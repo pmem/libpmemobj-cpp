7fcc7bf94699ee73317779edc9c1b8dbdbb908fb
Igor Chorążewicz <igor.chorazewicz@intel.com>
2020-03-26 13:47:56 +0100

    contigous_iterator: make operator[] take signed integral instead of unsigned.
    The reason is that Windows cannot decide for a call like this:
    
    `it[0] = 10`
    
    whether to use:
    * operator[](unsigned) -- one conversion from signed to unsigned
    * operator T*() and then build in operator[](signed) -- one conversion

--- include/libpmemobj++/experimental/slice.hpp
+++ include/libpmemobj++/experimental/slice.hpp
@@ -136,7 +136,8 @@ public:
 		if (idx >= size())
 			throw std::out_of_range("pmem::obj::slice");
 
-		return it_begin[idx];
+		return it_begin[static_cast<typename std::iterator_traits<
+			Iterator>::difference_type>(idx)];
 	}
 
 	/**
@@ -145,7 +146,8 @@ public:
 	 */
 	reference operator[](size_type idx)
 	{
-		return it_begin[idx];
+		return it_begin[static_cast<typename std::iterator_traits<
+			Iterator>::difference_type>(idx)];
 	}
 
 	size_type

e3ff1fac859c69996d157492c1df835265f0aed1
Krzysztof Filipek <krzysztof.filipek@intel.com>
2020-06-09 13:54:03 +0200

    libcxx tests: concurrent_map cons tests
    Port cons tests taken from libcxx master branch

--- tests/external/libcxx/map/map.cons/compare.pass.cpp
+++ tests/external/libcxx/map/map.cons/compare.pass.cpp
@@ -5,6 +5,11 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
+//
+// Copyright 2020, Intel Corporation
+//
+// Modified to test pmem::obj containers
+//
 
 // <map>
 
@@ -14,31 +19,78 @@
 
 // key_compare key_comp() const;
 
-#include <map>
-#include <cassert>
+#include "unittest.hpp"
 
-#include "test_macros.h"
-#include "../../../test_compare.h"
-#include "min_allocator.h"
+#include <libpmemobj++/experimental/concurrent_map.hpp>
+#include <libpmemobj++/make_persistent.hpp>
+#include <libpmemobj++/persistent_ptr.hpp>
+#include <libpmemobj++/pool.hpp>
+#include <libpmemobj++/transaction.hpp>
 
-int main(int, char**)
+namespace nvobj = pmem::obj;
+namespace nvobjex = pmem::obj::experimental;
+
+using CM = nvobjex::concurrent_map<int, double>;
+
+struct root {
+	nvobj::persistent_ptr<CM> s;
+};
+
+int
+run(pmem::obj::pool<root> &pop)
 {
-    {
-    typedef test_compare<std::less<int> > C;
-    const std::map<int, double, C> m(C(3));
-    assert(m.empty());
-    assert(m.begin() == m.end());
-    assert(m.key_comp() == C(3));
-    }
+	auto robj = pop.root();
+	(void)robj;
+	{
+#ifdef XXX // Implement test_compare class
+		typedef test_compare<std::less<int>> C;
+		const std::map<int, double, C> m(C(3));
+		assert(m.empty());
+		assert(m.begin() == m.end());
+		assert(m.key_comp() == C(3));
+#endif
+	}
 #if TEST_STD_VER >= 11
-    {
-    typedef test_compare<std::less<int> > C;
-    const std::map<int, double, C, min_allocator<std::pair<const int, double>>> m(C(3));
-    assert(m.empty());
-    assert(m.begin() == m.end());
-    assert(m.key_comp() == C(3));
-    }
+	{
+		typedef test_compare<std::less<int>> C;
+		const std::map<int, double, C,
+			       min_allocator<std::pair<const int, double>>>
+			m(C(3));
+		assert(m.empty());
+		assert(m.begin() == m.end());
+		assert(m.key_comp() == C(3));
+	}
 #endif
 
-  return 0;
+	return 0;
+}
+
+static void
+test(int argc, char *argv[])
+{
+	if (argc != 2)
+		UT_FATAL("usage: %s file-name", argv[0]);
+
+	const char *path = argv[1];
+
+	pmem::obj::pool<root> pop;
+	try {
+		pop = pmem::obj::pool<root>::create(path, "compare.pass",
+						    PMEMOBJ_MIN_POOL,
+						    S_IWUSR | S_IRUSR);
+	} catch (...) {
+		UT_FATAL("!pmemobj_create: %s", path);
+	}
+	try {
+		run(pop);
+		pop.close();
+	} catch (std::exception &e) {
+		UT_FATALexc(e);
+	}
+}
+
+int
+main(int argc, char *argv[])
+{
+	return run_test([&] { test(argc, argv); });
 }

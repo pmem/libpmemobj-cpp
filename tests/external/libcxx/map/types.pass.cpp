674309c03d23e302f1647cfa2d823c3154d6a918
Krzysztof Filipek <krzysztof.filipek@intel.com>
2020-08-04 19:35:49 +0200

    Enable unclassified map tests
    Enable unclassified tests ported from LIBCXX

--- tests/external/libcxx/map/types.pass.cpp
+++ tests/external/libcxx/map/types.pass.cpp
@@ -5,6 +5,11 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
+//
+// Copyright 2020, Intel Corporation
+//
+// Modified to test pmem::obj containers
+//
 
 // <map>
 
@@ -28,45 +33,152 @@
 //     ...
 // };
 
-#include <map>
-#include <type_traits>
+#include "map_wrapper.hpp"
+#include "unittest.hpp"
 
-#include "test_macros.h"
-#include "min_allocator.h"
+#include <libpmemobj++/make_persistent.hpp>
+#include <libpmemobj++/persistent_ptr.hpp>
+#include <libpmemobj++/pool.hpp>
+#include <libpmemobj++/transaction.hpp>
 
-int main(int, char**)
+namespace nvobj = pmem::obj;
+namespace nvobjex = pmem::obj::experimental;
+
+using container = container_t<int, double>;
+
+struct root {
+	nvobj::persistent_ptr<container> s;
+};
+
+int
+run(nvobj::pool<root> &pop)
 {
-    {
-    typedef std::map<int, double> C;
-    static_assert((std::is_same<C::key_type, int>::value), "");
-    static_assert((std::is_same<C::mapped_type, double>::value), "");
-    static_assert((std::is_same<C::value_type, std::pair<const int, double> >::value), "");
-    static_assert((std::is_same<C::key_compare, std::less<int> >::value), "");
-    static_assert((std::is_same<C::allocator_type, std::allocator<std::pair<const int, double> > >::value), "");
-    static_assert((std::is_same<C::reference, std::pair<const int, double>&>::value), "");
-    static_assert((std::is_same<C::const_reference, const std::pair<const int, double>&>::value), "");
-    static_assert((std::is_same<C::pointer, std::pair<const int, double>*>::value), "");
-    static_assert((std::is_same<C::const_pointer, const std::pair<const int, double>*>::value), "");
-    static_assert((std::is_same<C::size_type, std::size_t>::value), "");
-    static_assert((std::is_same<C::difference_type, std::ptrdiff_t>::value), "");
-    }
-#if TEST_STD_VER >= 11
-    {
-    typedef std::map<int, double, std::less<int>, min_allocator<std::pair<const int, double>>> C;
-    static_assert((std::is_same<C::key_type, int>::value), "");
-    static_assert((std::is_same<C::mapped_type, double>::value), "");
-    static_assert((std::is_same<C::value_type, std::pair<const int, double> >::value), "");
-    static_assert((std::is_same<C::key_compare, std::less<int> >::value), "");
-    static_assert((std::is_same<C::allocator_type, min_allocator<std::pair<const int, double> > >::value), "");
-    static_assert((std::is_same<C::reference, std::pair<const int, double>&>::value), "");
-    static_assert((std::is_same<C::const_reference, const std::pair<const int, double>&>::value), "");
-    static_assert((std::is_same<C::pointer, min_pointer<std::pair<const int, double>>>::value), "");
-    static_assert((std::is_same<C::const_pointer, min_pointer<const std::pair<const int, double>>>::value), "");
-//  min_allocator doesn't have a size_type, so one gets synthesized
-    static_assert((std::is_same<C::size_type, std::make_unsigned<C::difference_type>::type>::value), "");
-    static_assert((std::is_same<C::difference_type, std::ptrdiff_t>::value), "");
-    }
+	{
+		typedef container C;
+		static_assert((std::is_same<C::key_type, int>::value), "");
+		static_assert((std::is_same<C::mapped_type, double>::value),
+			      "");
+		static_assert(
+			(std::is_same<
+				C::value_type,
+				pmem::detail::pair<const int, double>>::value),
+			"");
+		static_assert(
+			(std::is_same<C::key_compare, std::less<int>>::value),
+			"");
+		static_assert(
+			(std::is_same<C::allocator_type,
+				      pmem::obj::allocator<pmem::detail::pair<
+					      const int, double>>>::value),
+			"");
+		static_assert(
+			(std::is_same<C::reference,
+				      pmem::detail::pair<const int, double>
+					      &>::value),
+			"");
+		static_assert(
+			(std::is_same<C::const_reference,
+				      const pmem::detail::pair<
+					      const int, double> &>::value),
+			"");
+		static_assert(
+			(std::is_same<C::pointer,
+				      nvobj::persistent_ptr<pmem::detail::pair<
+					      const int, double>>>::value),
+			"");
+		static_assert(
+			(std::is_same<
+				C::const_pointer,
+				nvobj::persistent_ptr<const pmem::detail::pair<
+					const int, double>>>::value),
+			"");
+		static_assert((std::is_same<C::size_type, std::size_t>::value),
+			      "");
+		static_assert((std::is_same<C::difference_type,
+					    std::ptrdiff_t>::value),
+			      "");
+	}
+#ifdef XXX // XXX: Implement min_allocator
+	{
+		typedef std::map<int, double, std::less<int>,
+				 min_allocator<std::pair<const int, double>>>
+			C;
+		static_assert((std::is_same<C::key_type, int>::value), "");
+		static_assert((std::is_same<C::mapped_type, double>::value),
+			      "");
+		static_assert(
+			(std::is_same<C::value_type,
+				      std::pair<const int, double>>::value),
+			"");
+		static_assert(
+			(std::is_same<C::key_compare, std::less<int>>::value),
+			"");
+		static_assert(
+			(std::is_same<C::allocator_type,
+				      min_allocator<std::pair<const int,
+							      double>>>::value),
+			"");
+		static_assert(
+			(std::is_same<C::reference,
+				      std::pair<const int, double> &>::value),
+			"");
+		static_assert(
+			(std::is_same<
+				C::const_reference,
+				const std::pair<const int, double> &>::value),
+			"");
+		static_assert(
+			(std::is_same<C::pointer,
+				      min_pointer<std::pair<const int,
+							    double>>>::value),
+			"");
+		static_assert(
+			(std::is_same<C::const_pointer,
+				      min_pointer<const std::pair<
+					      const int, double>>>::value),
+			"");
+		//  min_allocator doesn't have a size_type, so one gets
+		//  synthesized
+		static_assert(
+			(std::is_same<C::size_type,
+				      std::make_unsigned<C::difference_type>::
+					      type>::value),
+			"");
+		static_assert((std::is_same<C::difference_type,
+					    std::ptrdiff_t>::value),
+			      "");
+	}
 #endif
 
-  return 0;
+	return 0;
+}
+
+static void
+test(int argc, char *argv[])
+{
+	if (argc != 2)
+		UT_FATAL("usage: %s file-name", argv[0]);
+
+	const char *path = argv[1];
+
+	pmem::obj::pool<root> pop;
+	try {
+		pop = pmem::obj::pool<root>::create(path, "types.pass",
+						    PMEMOBJ_MIN_POOL,
+						    S_IWUSR | S_IRUSR);
+	} catch (...) {
+		UT_FATAL("!pmemobj_create: %s", path);
+	}
+	try {
+		run(pop);
+		pop.close();
+	} catch (std::exception &e) {
+		UT_FATALexc(e);
+	}
+}
+
+int
+main(int argc, char *argv[])
+{
+	return run_test([&] { test(argc, argv); });
 }

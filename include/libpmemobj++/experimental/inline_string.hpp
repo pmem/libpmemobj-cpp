925e8e1dccc459429c5335627258a9f8b42e1fa3
Igor Chorążewicz <igor.chorazewicz@intel.com>
2020-08-19 16:08:44 +0200

    inline_string: add capacity field
    After changing radix_tree iterators API to return refernce to the value
    instead of a wrapper user should be able to check the capacity of an
    inline string.
    
    The storage cost of one additional field is not very high and it also
    allows for checks inside assign() methods of inline_string.

--- include/libpmemobj++/experimental/inline_string.hpp
+++ include/libpmemobj++/experimental/inline_string.hpp
@@ -55,7 +55,7 @@ public:
 	 *
 	 * @throw pool_error if inline_string doesn't reside on pmem.
 	 */
-	inline_string(string_view v) : size_(v.size())
+	inline_string(string_view v) : size_(v.size()), capacity_(v.size())
 	{
 		if (nullptr == pmemobj_pool_by_ptr(this))
 			throw pmem::pool_error("Invalid pool handle.");
@@ -71,7 +71,8 @@ public:
 	 *
 	 * @throw pool_error if inline_string doesn't reside on pmem.
 	 */
-	inline_string(const inline_string &rhs) : size_(rhs.size())
+	inline_string(const inline_string &rhs)
+	    : size_(rhs.size()), capacity_(rhs.size())
 	{
 		if (nullptr == pmemobj_pool_by_ptr(this))
 			throw pmem::pool_error("Invalid pool handle.");
@@ -91,9 +92,12 @@ public:
 		if (this == &rhs)
 			return *this;
 
-		return assign(string_view(rhs));
+		return assign(rhs);
 	}
 
+	/**
+	 * Assignment operator from string_view.
+	 */
 	inline_string &
 	operator=(string_view rhs)
 	{
@@ -114,6 +118,18 @@ public:
 	size() const noexcept
 	{
 		return size_;
+	}
+
+	/**
+	 * @return number of characters that can be held in inline_string.
+	 *
+	 * The space actually occupied by inline_string is equal to
+	 * sizeof(inline_string) + capacity() + sizeof('\0')
+	 */
+	size_type
+	capacity() const noexcept
+	{
+		return capacity_;
 	}
 
 	/** @return pointer to the data (equal to (this + 1)) */
@@ -141,6 +157,10 @@ public:
 	{
 		auto pop = obj::pool_base(pmemobj_pool_by_ptr(this));
 
+		if (rhs.size() > capacity())
+			throw std::out_of_range(
+				"inline_string capacity exceeded.");
+
 		auto initialized_mem =
 			(std::min)(rhs.size(), size()) + 1 /* sizeof('\0') */;
 
@@ -167,6 +187,7 @@ public:
 
 private:
 	obj::p<uint64_t> size_;
+	obj::p<uint64_t> capacity_;
 };
 
 /**

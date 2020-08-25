0c6217e427d19cc8ab8925a7dd79cb6a9254baa3
Jan Dorniak <jan.dorniak@intel.com>
2020-08-25 12:16:29 +0200

    generic tests for radix_tree and concurrent_map

---
+++ tests/container_generic/ctor_and_assignment.hpp
@@ -0,0 +1,153 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2020, Intel Corporation */
+
+#ifndef LIBPMEMOBJ_CPP_TESTS_CTOR_AND_ASSIGNMENT
+#define LIBPMEMOBJ_CPP_TESTS_CTOR_AND_ASSIGNMENT
+
+#include "../common/unittest.hpp"
+
+namespace nvobj = pmem::obj;
+
+/**
+ * Wrapper around PMDK allocator
+ * @throw std::bad_alloc on allocation failure.
+ */
+template <typename T, typename U, typename... Args>
+void
+tx_alloc_wrapper(nvobj::pool_base &pop, nvobj::persistent_ptr<U> &ptr,
+		 Args &&... args)
+{
+	try {
+		nvobj::transaction::manual tx(pop);
+		ptr = nvobj::make_persistent<T>(std::forward<Args>(args)...);
+		nvobj::transaction::commit();
+	} catch (...) {
+		throw std::bad_alloc();
+	}
+}
+
+template <typename T>
+void
+verify_elements(T &container, size_t elements)
+{
+	UT_ASSERT(container.size() == elements);
+
+	for (int i = 0; i < static_cast<int>(elements); i++) {
+		UT_ASSERT(container.count(i) == 1);
+	}
+}
+
+template <typename T>
+void
+ctor_test(nvobj::pool_base &pop, nvobj::persistent_ptr<T> &container1,
+	  nvobj::persistent_ptr<T> &container2)
+{
+	using V = typename T::value_type;
+
+	tx_alloc_wrapper<T>(pop, container1);
+	UT_ASSERT(container1->empty());
+	UT_ASSERT(container1->size() == size_t(0));
+
+	for (int i = 0; i < 300; i++) {
+		auto ret = container1->insert(V(i, i));
+		UT_ASSERT(ret.MAP_VALUE == true);
+		UT_ASSERT(ret.MAP_KEY->MAP_KEY == i);
+		UT_ASSERT(ret.MAP_KEY->MAP_VALUE == i);
+	}
+
+	tx_alloc_wrapper<T>(pop, container2, container1->begin(),
+			    container1->end());
+
+	UT_ASSERT(!container2->empty());
+	UT_ASSERT(container1->size() == container2->size());
+
+	verify_elements<T>(*container2, 300);
+
+	pmem::detail::destroy<T>(*container2);
+	tx_alloc_wrapper<T>(pop, container2, *container1);
+
+	UT_ASSERT(container1->size() == container2->size());
+
+	verify_elements<T>(*container2, 300);
+
+	pmem::detail::destroy<T>(*container2);
+	tx_alloc_wrapper<T>(pop, container2, std::move(*container1));
+
+	verify_elements<T>(*container2, 300);
+
+	pmem::detail::destroy<T>(*container2);
+	tx_alloc_wrapper<T>(pop, container2,
+			    std::initializer_list<V>{V(0, 0), V(1, 1)});
+
+	verify_elements<T>(*container2, 2);
+
+	pmem::detail::destroy<T>(*container1);
+	pmem::detail::destroy<T>(*container2);
+}
+
+template <typename T>
+void
+assignment_test(nvobj::pool_base &pop, nvobj::persistent_ptr<T> &container1,
+		nvobj::persistent_ptr<T> &container2)
+{
+	using V = typename T::value_type;
+
+	tx_alloc_wrapper<T>(pop, container1);
+	tx_alloc_wrapper<T>(pop, container2);
+
+	UT_ASSERT(container1->empty());
+
+	for (int i = 0; i < 50; i++) {
+		auto ret = container1->insert(V(i, i));
+		UT_ASSERT(ret.MAP_VALUE == true);
+	}
+
+	verify_elements<T>(*container1, 50);
+
+	for (int i = 0; i < 300; i++) {
+		auto ret = container2->insert(V(i, i));
+		UT_ASSERT(ret.MAP_VALUE == true);
+	}
+
+	*container1 = *container2;
+
+	verify_elements<T>(*container1, 300);
+
+	for (int i = 300; i < 350; i++) {
+		auto ret = container1->insert(V(i, i));
+		UT_ASSERT(ret.MAP_VALUE == true);
+	}
+
+	verify_elements<T>(*container1, 350);
+	verify_elements<T>(*container2, 300);
+
+	container2->clear();
+
+	*container1 = *container2;
+
+	UT_ASSERT(container1->size() == 0);
+	UT_ASSERT(std::distance(container1->begin(), container1->end()) == 0);
+	UT_ASSERT(container2->size() == 0);
+	UT_ASSERT(std::distance(container2->begin(), container2->end()) == 0);
+
+	for (int i = 0; i < 350; i++) {
+		UT_ASSERT(container1->count(i) == 0);
+		UT_ASSERT(container2->count(i) == 0);
+	}
+
+	for (int i = 0; i < 100; i++) {
+		auto ret = container1->insert(V(i, i));
+		UT_ASSERT(ret.MAP_VALUE == true);
+	}
+
+	verify_elements<T>(*container1, 100);
+
+	*container2 = std::move(*container1);
+
+	verify_elements<T>(*container2, 100);
+
+	pmem::detail::destroy<T>(*container1);
+	pmem::detail::destroy<T>(*container2);
+}
+
+#endif
---
+++ tests/map/map_txabort.cpp
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2020, Intel Corporation */
+
+#include "../common/map_wrapper.hpp"
+#include "../common/unittest.hpp"
+#include "../container_generic/container_txabort.hpp"
+
+#define LAYOUT "map_txabort"
+
+namespace nvobj = pmem::obj;
+
+using map_type = container_t<int, nvobj::p<int>>;
+
+struct root {
+	nvobj::persistent_ptr<map_type> pptr1;
+	nvobj::persistent_ptr<map_type> pptr2;
+};
+
+static void
+test(int argc, char *argv[])
+{
+	if (argc < 2) {
+		UT_FATAL("usage: %s file-name", argv[0]);
+	}
+
+	const char *path = argv[1];
+
+	nvobj::pool<root> pop;
+
+	try {
+		pop = nvobj::pool<root>::create(
+			path, LAYOUT, PMEMOBJ_MIN_POOL * 20, S_IWUSR | S_IRUSR);
+	} catch (pmem::pool_error &pe) {
+		UT_FATAL("!pool::create: %s %s", pe.what(), path);
+	}
+
+/* XXX: until insert implemented in radix */
+#if !defined(LIBPMEMOBJ_CPP_TESTS_RADIX)
+	auto r = pop.root();
+
+	test_tx_singlethread<map_type>(pop, r->pptr1, r->pptr2);
+#endif
+
+	pop.close();
+}
+
+int
+main(int argc, char *argv[])
+{
+	return run_test([&] { test(argc, argv); });
+}

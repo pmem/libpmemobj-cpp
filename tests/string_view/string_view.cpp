c9908f556ae334b4ca1cf71ca1c021282e9bef5b
Krzysztof Filipek <krzysztof.filipek@intel.com>
2020-08-25 17:08:37 +0200

    Implementation of basic_string_view class
    Parametrized version of original string_view

--- tests/string_view/string_view.cpp
+++ tests/string_view/string_view.cpp
@@ -5,26 +5,64 @@
 
 #include <iostream>
 #include <libpmemobj++/string_view.hpp>
+#include <string>
 
+template <typename T>
 void
 test_string_view()
 {
 	std::string s1("abc");
 	std::string s2("xyz");
+	std::string sLonger("01234567890123456789"
+			    "01234567890123456789"
+			    "01234567890123456789"
+			    "01234567890123456789"
+			    "01234567890123456789");
+	std::string sLonger2("xyz34567890123456789"
+			     "01234567890123456789"
+			     "01234567890123456789"
+			     "01234567890123456789"
+			     "01234567890123456789");
 
-	pmem::obj::string_view v1(s1);
-	pmem::obj::string_view v2(s2);
+	std::basic_string<T> ts1(s1.begin(), s1.end());
+	std::basic_string<T> ts2(s2.begin(), s2.end());
+	std::basic_string<T> tsLonger(sLonger.begin(), sLonger.end());
+	std::basic_string<T> tsLonger2(sLonger2.begin(), sLonger2.end());
 
-	UT_ASSERT(s1.data() == v1.data());
-	UT_ASSERT(s1.size() == v1.size());
+	pmem::obj::basic_string_view<T> vEmpty;
+	pmem::obj::basic_string_view<T> v1(ts1.data(), ts1.length());
+	pmem::obj::basic_string_view<T> v2(ts2.data(), ts2.length());
+	pmem::obj::basic_string_view<T> vLonger(tsLonger.data(),
+						tsLonger.length());
+	pmem::obj::basic_string_view<T> vLonger2(tsLonger2.data(),
+						 tsLonger2.length());
+
+	UT_ASSERT(ts1.data() == v1.data());
+	UT_ASSERT(ts1.size() == v1.size());
 
 	UT_ASSERT(v1.compare(v2) < 0);
 	UT_ASSERT(v2.compare(v1) > 0);
 	UT_ASSERT(v1.compare(v1) == 0);
+
+	UT_ASSERT(v1.compare(vLonger) != 0);
+
+	UT_ASSERT(v2.compare(vLonger2) < 0);
+	UT_ASSERT(vLonger2.compare(v2) > 0);
+
+	UT_ASSERT(vEmpty.data() == nullptr);
+	UT_ASSERT(vEmpty.size() == 0);
+}
+
+void
+run_test()
+{
+	test_string_view<char>();
+	test_string_view<wchar_t>();
+	test_string_view<uint8_t>();
 }
 
 int
 main(int argc, char *argv[])
 {
-	return run_test([&] { test_string_view(); });
+	return run_test([&] { run_test(); });
 }

cab2deda9c14a3ea48c9bad7882b0e3df3a16911
Krzysztof Filipek <krzysztof.filipek@intel.com>
2020-09-03 16:08:57 +0200

    inline_string: turn implementation into template class
    - change implementation to template
    - move functions definitions outside class

--- tests/inline_string/inline_string.cpp
+++ tests/inline_string/inline_string.cpp
@@ -21,16 +21,6 @@ namespace nvobjex = pmem::obj::experimental;
 namespace
 {
 
-template <typename T>
-class Basic_String_Container {
-public:
-	static std::map<std::string, std::basic_string<T>> string_map;
-};
-
-template <typename T>
-typename std::map<std::string, std::basic_string<T>>
-	Basic_String_Container<T>::string_map;
-
 template <typename CharT>
 struct Object {
 	template <typename... Args>
@@ -52,55 +42,46 @@ struct root {
 };
 
 template <typename T>
-typename nvobj::basic_string_view<T>
-convert_to_basic_string_view(std::string str)
-{
-	Basic_String_Container<T>::string_map[str] =
-		std::basic_string<T>(str.begin(), str.end());
-	nvobj::basic_string_view<T> bsv(
-		Basic_String_Container<T>::string_map[str].data(),
-		Basic_String_Container<T>::string_map[str].length());
-	return bsv;
-}
-
-template <typename T>
 void
 test_inline_string(nvobj::pool<struct root<T>> &pop)
 {
 	auto r = pop.root();
 
+	const nvobjex::inline_string::size_type req_capacity = 100;
 	std::string test_string1("abcd");
-	std::string test_string2("xxxxxxx");
+	std::basic_string<T> bs2(7, static_cast<T>('x'));
 	std::string test_string3("abcdefgh");
 
-	nvobj::basic_string_view<T> bsv_test_string1(
-		convert_to_basic_string_view<T>(test_string1));
-	nvobj::basic_string_view<T> bsv_test_string2(
-		convert_to_basic_string_view<T>(test_string2));
-	nvobj::basic_string_view<T> test_view(
-		convert_to_basic_string_view<T>(test_string3));
+	auto bs1 =
+		std::basic_string<T>(test_string1.begin(), test_string1.end());
 
-	const nvobjex::inline_string::size_type req_capacity = 100;
+	auto bs3 =
+		std::basic_string<T>(test_string3.begin(), test_string3.end());
+
+	nvobj::basic_string_view<T> bsv_test_string1(bs1.data(), bs1.length());
+	nvobj::basic_string_view<T> bsv_test_string2(bs2.data(), bs2.length());
+	nvobj::basic_string_view<T> test_view(bs3.data(), bs3.length());
+
+	std::basic_string<T> max_capacity_bs(req_capacity, static_cast<T>('a'));
+	nvobj::basic_string_view<T> max_capacity_view(max_capacity_bs.data(),
+						      max_capacity_bs.length());
 
 	try {
 		nvobj::transaction::run(pop, [&] {
 			nvobj::standard_alloc_policy<void> allocator;
 			r->o1 = static_cast<nvobj::persistent_ptr<Object<T>>>(
 				allocator.allocate(sizeof(Object<T>) +
-						   req_capacity));
+						   req_capacity * sizeof(T)));
 			r->o2 = static_cast<nvobj::persistent_ptr<Object<T>>>(
 				allocator.allocate(sizeof(Object<T>) +
-						   req_capacity));
+						   req_capacity * sizeof(T)));
 			r->o3 = static_cast<nvobj::persistent_ptr<Object<T>>>(
 				allocator.allocate(sizeof(Object<T>) +
-						   req_capacity));
+						   req_capacity * sizeof(T)));
 
 			new (r->o1.get()) Object<T>(1, req_capacity);
 			new (r->o2.get()) Object<T>(2, req_capacity);
-			new (r->o3.get()) Object<T>(
-				3,
-				convert_to_basic_string_view<T>(
-					std::string(req_capacity, 'a')));
+			new (r->o3.get()) Object<T>(3, max_capacity_view);
 
 			r->o1->s.assign(bsv_test_string1);
 			r->o2->s.assign(bsv_test_string2);
@@ -125,8 +106,7 @@ test_inline_string(nvobj::pool<struct root<T>> &pop)
 
 	UT_ASSERT(r->o3->s.capacity() == r->o3->s.size());
 	UT_ASSERT(nvobj::basic_string_view<T>(r->o3->s).compare(
-			  convert_to_basic_string_view<T>(
-				  std::string(req_capacity, 'a'))) == 0);
+			  max_capacity_view) == 0);
 
 	try {
 		nvobj::transaction::run(pop, [&] {
@@ -162,26 +142,34 @@ test_inline_string(nvobj::pool<struct root<T>> &pop)
 
 	try {
 		std::string str(r->o1->s.capacity() + 5, 'x');
+		std::basic_string<T> bs(str.begin(), str.end());
+		nvobj::basic_string_view<T> v1(bs.data(), bs.length());
 
-		nvobj::basic_string_view<T> v1 =
-			convert_to_basic_string_view<T>(str);
 		nvobj::transaction::run(pop, [&] { r->o1->s = v1; });
 	} catch (std::out_of_range &) {
 	} catch (...) {
 		UT_ASSERT(0);
 	}
 
-	r->o1->s.assign(convert_to_basic_string_view<T>(""));
-
+	{
+		std::string empty("");
+		std::basic_string<T> bs(empty.begin(), empty.end());
+		r->o1->s.assign(
+			nvobj::basic_string_view<T>(bs.data(), bs.length()));
+	}
 	try {
 		nvobj::transaction::run(pop, [&] {
-			r->o1->s.assign(
-				convert_to_basic_string_view<T>("aaaa"));
+			std::string test_string("aaaa");
+			std::basic_string<T> bs(test_string.begin(),
+						test_string.end());
+			r->o1->s.assign(nvobj::basic_string_view<T>(
+				bs.data(), bs.length()));
 
 			r->o1->s = r->o1->s;
 			UT_ASSERT(nvobj::basic_string_view<T>(r->o1->s).compare(
-					  convert_to_basic_string_view<T>(
-						  "aaaa")) == 0);
+					  nvobj::basic_string_view<T>(
+						  bs.data(), bs.length())) ==
+				  0);
 
 			nvobj::transaction::abort(0);
 		});
@@ -190,13 +178,18 @@ test_inline_string(nvobj::pool<struct root<T>> &pop)
 		UT_ASSERT(0);
 	}
 
-	UT_ASSERT(nvobj::basic_string_view<T>(r->o1->s).compare(
-			  convert_to_basic_string_view<T>("")) == 0);
+	{
+		std::basic_string<T> bs;
+		UT_ASSERT(nvobj::basic_string_view<T>(r->o1->s).compare(
+				  nvobj::basic_string_view<T>(
+					  bs.data(), bs.length())) == 0);
+	}
 	UT_ASSERT(nvobj::basic_string_view<T>(r->o1->s).data()[0] == '\0');
 
 	nvobj::transaction::run(pop, [&] {
 		nvobj::delete_persistent<Object<T>>(r->o1);
 		nvobj::delete_persistent<Object<T>>(r->o2);
+		nvobj::delete_persistent<Object<T>>(r->o3);
 	});
 }
 
@@ -204,8 +197,14 @@ template <typename T>
 void
 test_ctor_exception_nopmem(nvobj::pool<struct root<T>> &pop)
 {
+	auto bs1 = std::basic_string<T>(4, static_cast<T>('a'));
+	nvobj::basic_string_view<T> bsv_test_string1(bs1.data(), bs1.length());
+
 	try {
-		Object<T> o(1, convert_to_basic_string_view<T>("example"));
+		std::string example_str("example");
+		std::basic_string<T> bs(example_str.begin(), example_str.end());
+		Object<T> o(
+			1, nvobj::basic_string_view<T>(bs.data(), bs.length()));
 	} catch (pmem::pool_error &) {
 	} catch (...) {
 		UT_ASSERT(0);
@@ -218,10 +217,10 @@ test_ctor_exception_nopmem(nvobj::pool<struct root<T>> &pop)
 	nvobj::transaction::run(pop, [&] {
 		nvobj::standard_alloc_policy<void> allocator;
 		r->o1 = static_cast<nvobj::persistent_ptr<Object<T>>>(
-			allocator.allocate(sizeof(Object<T>) + req_capacity));
+			allocator.allocate(sizeof(Object<T>) +
+					   req_capacity * sizeof(T)));
 
-		new (r->o1.get())
-			Object<T>(1, convert_to_basic_string_view<T>("abcd"));
+		new (r->o1.get()) Object<T>(1, bsv_test_string1);
 
 		try {
 			Object<T> o(*r->o1);

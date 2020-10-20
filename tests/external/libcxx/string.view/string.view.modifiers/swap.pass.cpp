2e432e6ec1e4486e3afd81ebf92a2f775562a652
Krzysztof Filipek <krzysztof.filipek@intel.com>
2020-10-20 15:00:28 +0200

    string_view: port modifiers libcxx tests
    - enable tests for remove_suffix/prefix and swap methods

--- tests/external/libcxx/string.view/string.view.modifiers/swap.pass.cpp
+++ tests/external/libcxx/string.view/string.view.modifiers/swap.pass.cpp
@@ -5,50 +5,52 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
+//
+// Copyright 2020, Intel Corporation
+//
+// Modified to test pmem::obj containers
+//
 
 // <string_view>
 
 // void swap(basic_string_view& _other) noexcept
 
-#include <cassert>
-#include <string_view>
+#include "unittest.hpp"
 
-#include "test_macros.h"
+#include <libpmemobj++/string_view.hpp>
 
 template <typename CharT>
 void
 test(const CharT *s, size_t len)
 {
-	typedef std::basic_string_view<CharT> SV;
+	typedef pmem::obj::basic_string_view<CharT> SV;
 	{
 		SV sv1(s);
 		SV sv2;
 
-		assert(sv1.size() == len);
-		assert(sv1.data() == s);
-		assert(sv2.size() == 0);
+		UT_ASSERT(sv1.size() == len);
+		UT_ASSERT(sv1.data() == s);
+		UT_ASSERT(sv2.size() == 0);
 
 		sv1.swap(sv2);
-		assert(sv1.size() == 0);
-		assert(sv2.size() == len);
-		assert(sv2.data() == s);
+		UT_ASSERT(sv1.size() == 0);
+		UT_ASSERT(sv2.size() == len);
+		UT_ASSERT(sv2.data() == s);
 	}
 }
 
-#if TEST_STD_VER > 11
 constexpr size_t
 test_ce(size_t n, size_t k)
 {
-	typedef std::basic_string_view<char> SV;
+	typedef pmem::obj::basic_string_view<char> SV;
 	SV sv1{"ABCDEFGHIJKL", n};
 	SV sv2{sv1.data(), k};
 	sv1.swap(sv2);
 	return sv1.size();
 }
-#endif
 
-int
-main(int, char **)
+static void
+run()
 {
 	test("ABCDE", 5);
 	test("a", 1);
@@ -58,7 +60,6 @@ main(int, char **)
 	test(L"a", 1);
 	test(L"", 0);
 
-#if TEST_STD_VER >= 11
 	test(u"ABCDE", 5);
 	test(u"a", 1);
 	test(u"", 0);
@@ -66,15 +67,16 @@ main(int, char **)
 	test(U"ABCDE", 5);
 	test(U"a", 1);
 	test(U"", 0);
-#endif
 
-#if TEST_STD_VER > 11
 	{
 		static_assert(test_ce(2, 3) == 3, "");
 		static_assert(test_ce(5, 3) == 3, "");
 		static_assert(test_ce(0, 1) == 1, "");
 	}
-#endif
+}
 
-	return 0;
+int
+main(int argc, char *argv[])
+{
+	return run_test([&] { run(); });
 }

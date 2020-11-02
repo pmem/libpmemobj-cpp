7a449eb53607eac74f2674952cf4b87df9c92c9c
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2020-11-02 10:38:08 +0100

    utils: clean up run-coverity.sh, run it only on upstream repo
    and don't build docs.

--- utils/docker/run-coverity.sh
+++ utils/docker/run-coverity.sh
@@ -3,20 +3,24 @@
 # Copyright 2018-2020, Intel Corporation
 
 #
-# run-coverity.sh - runs the Coverity scan build
+# run-coverity.sh - runs the Coverity scan build and send report to coverity.com
 #
 
 set -e
 
 # Run coverity build only on upstream repository and check for req. vars
-if [[ "${CI_REPO_SLUG}" != "${GITHUB_REPO}" \
-   && ( "${COVERITY_SCAN_NOTIFICATION_EMAIL}" == "" \
-     || "${COVERITY_SCAN_TOKEN}" == "" ) ]]; then
+if [[ "${CI_REPO_SLUG}" != "${GITHUB_REPO}" ]]; then
+	echo
+	echo "Skipping Coverity build: it doesn't run on forked repos."
+	exit 0
+fi
+if [[ -z "${COVERITY_SCAN_NOTIFICATION_EMAIL}"
+     || -z "${COVERITY_SCAN_TOKEN}" ]]; then
 	echo
 	echo "Skipping Coverity build:"\
 		"COVERITY_SCAN_TOKEN=\"${COVERITY_SCAN_TOKEN}\" or"\
 		"COVERITY_SCAN_NOTIFICATION_EMAIL="\
-		"\"${COVERITY_SCAN_NOTIFICATION_EMAIL}\" is not set"
+		"\"${COVERITY_SCAN_NOTIFICATION_EMAIL}\" are not set."
 	exit 0
 fi
 
@@ -34,26 +38,20 @@ echo -n | openssl s_client -connect scan.coverity.com:443 | \
 
 sudo_password mv ${TEMP_CF} ${CERT_FILE}
 
-echo "Prepare build and run coverity scan"
+echo "Prepare CMake build"
 mkdir ${WORKDIR}/build
 cd ${WORKDIR}/build
-PKG_CONFIG_PATH=/opt/pmdk/lib/pkgconfig/ cmake .. -DCMAKE_BUILD_TYPE=Debug
-
-export COVERITY_SCAN_PROJECT_NAME="${CI_REPO_SLUG}"
-export COVERITY_SCAN_BRANCH_PATTERN="master"
-export COVERITY_SCAN_BUILD_COMMAND="make -j$(nproc)"
-
-#
-# Run the Coverity scan
-#
+PKG_CONFIG_PATH=/opt/pmdk/lib/pkgconfig/ cmake .. -DCMAKE_BUILD_TYPE=Debug -DBUILD_DOC=OFF
 
 # The 'travisci_build_coverity_scan.sh' script requires the following
 # environment variables to be set:
-# - TRAVIS_BRANCH - has to contain the name of the current branch
-# - TRAVIS_PULL_REQUEST - has to be set to 'true' in case of pull requests
-#
+export COVERITY_SCAN_PROJECT_NAME="${GITHUB_REPO}"
+export COVERITY_SCAN_BRANCH_PATTERN="master"
+export COVERITY_SCAN_BUILD_COMMAND="make -j$(nproc)"
 export TRAVIS_BRANCH=${CI_BRANCH}
 [ "${CI_EVENT_TYPE}" == "pull_request" ] && export TRAVIS_PULL_REQUEST="true"
+
+echo "Run the Coverity scan"
 
 # XXX: Patch the Coverity script.
 # Recently, this script regularly exits with an error, even though

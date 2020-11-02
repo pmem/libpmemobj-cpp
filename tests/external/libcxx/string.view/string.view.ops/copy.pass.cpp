099a78bd15c0630537183170c941355e141fecfc
Krzysztof Filipek <krzysztof.filipek@intel.com>
2020-11-02 19:37:48 +0100

    string_view: port libcxx ops tests to use pmem::obj::string_view
    - port libcxx tests

--- tests/external/libcxx/string.view/string.view.ops/copy.pass.cpp
+++ tests/external/libcxx/string.view/string.view.ops/copy.pass.cpp
@@ -5,6 +5,11 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
+//
+// Copyright 2020, Intel Corporation
+//
+// Modified to test pmem::obj containers
+//
 
 // <string_view>
 
@@ -16,89 +21,93 @@
 // Effects: Equivalent to std::copy_n(begin() + pos, rlen, s).
 // Returns: rlen.
 
+#include "unittest.hpp"
 
-#include <string_view>
 #include <algorithm>
-#include <cassert>
+#include <libpmemobj++/string_view.hpp>
 #include <stdexcept>
 
-#include "test_macros.h"
+template <typename CharT>
+void
+test1(pmem::obj::basic_string_view<CharT> sv, size_t n, size_t pos)
+{
+	const size_t rlen = (std::min)(n, sv.size() - pos);
 
-template<typename CharT>
-void test1 ( std::basic_string_view<CharT> sv, size_t n, size_t pos ) {
-    const size_t rlen = std::min ( n, sv.size() - pos );
+	CharT *dest1 = new CharT[rlen + 1];
+	dest1[rlen] = 0;
+	CharT *dest2 = new CharT[rlen + 1];
+	dest2[rlen] = 0;
 
-    CharT *dest1 = new CharT [rlen + 1];    dest1[rlen] = 0;
-    CharT *dest2 = new CharT [rlen + 1];    dest2[rlen] = 0;
-
-    if (pos > sv.size()) {
-#ifndef TEST_HAS_NO_EXCEPTIONS
-        try {
-            sv.copy(dest1, n, pos);
-            assert(false);
-        } catch (const std::out_of_range&) {
-        } catch (...) {
-            assert(false);
-        }
-#endif
-    } else {
-        sv.copy(dest1, n, pos);
-        std::copy_n(sv.begin() + pos, rlen, dest2);
-        for ( size_t i = 0; i <= rlen; ++i )
-            assert ( dest1[i] == dest2[i] );
-    }
-    delete [] dest1;
-    delete [] dest2;
+	if (pos > sv.size()) {
+		try {
+			sv.copy(dest1, n, pos);
+			UT_ASSERT(false);
+		} catch (const std::out_of_range &) {
+		} catch (...) {
+			UT_ASSERT(false);
+		}
+	} else {
+		sv.copy(dest1, n, pos);
+		std::copy_n(sv.begin() + pos, rlen, dest2);
+		for (size_t i = 0; i <= rlen; ++i)
+			UT_ASSERT(dest1[i] == dest2[i]);
+	}
+	delete[] dest1;
+	delete[] dest2;
 }
 
+template <typename CharT>
+void
+test(const CharT *s)
+{
+	typedef pmem::obj::basic_string_view<CharT> string_view_t;
 
-template<typename CharT>
-void test ( const CharT *s ) {
-    typedef std::basic_string_view<CharT> string_view_t;
+	string_view_t sv1(s);
 
-    string_view_t sv1 ( s );
+	test1(sv1, 0, 0);
+	test1(sv1, 1, 0);
+	test1(sv1, 20, 0);
+	test1(sv1, sv1.size(), 0);
+	test1(sv1, 20, string_view_t::npos);
 
-    test1(sv1,  0, 0);
-    test1(sv1,  1, 0);
-    test1(sv1, 20, 0);
-    test1(sv1, sv1.size(), 0);
-    test1(sv1, 20, string_view_t::npos);
+	test1(sv1, 0, 3);
+	test1(sv1, 2, 3);
+	test1(sv1, 100, 3);
+	test1(sv1, 100, string_view_t::npos);
 
-    test1(sv1,   0, 3);
-    test1(sv1,   2, 3);
-    test1(sv1, 100, 3);
-    test1(sv1, 100, string_view_t::npos);
+	test1(sv1, sv1.size(), string_view_t::npos);
 
-    test1(sv1, sv1.size(), string_view_t::npos);
-
-    test1(sv1, sv1.size() + 1, 0);
-    test1(sv1, sv1.size() + 1, 1);
-    test1(sv1, sv1.size() + 1, string_view_t::npos);
-
+	test1(sv1, sv1.size() + 1, 0);
+	test1(sv1, sv1.size() + 1, 1);
+	test1(sv1, sv1.size() + 1, string_view_t::npos);
 }
 
-int main(int, char**) {
-    test ( "ABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDE" );
-    test ( "ABCDE");
-    test ( "a" );
-    test ( "" );
+static void
+run()
+{
+	test("ABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDE");
+	test("ABCDE");
+	test("a");
+	test("");
 
-    test ( L"ABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDE" );
-    test ( L"ABCDE" );
-    test ( L"a" );
-    test ( L"" );
+	test(L"ABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDE");
+	test(L"ABCDE");
+	test(L"a");
+	test(L"");
 
-#if TEST_STD_VER >= 11
-    test ( u"ABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDE" );
-    test ( u"ABCDE" );
-    test ( u"a" );
-    test ( u"" );
+	test(u"ABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDE");
+	test(u"ABCDE");
+	test(u"a");
+	test(u"");
 
-    test ( U"ABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDE" );
-    test ( U"ABCDE" );
-    test ( U"a" );
-    test ( U"" );
-#endif
+	test(U"ABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDE");
+	test(U"ABCDE");
+	test(U"a");
+	test(U"");
+}
 
-  return 0;
+int
+main(int argc, char *argv[])
+{
+	return run_test([&] { run(); });
 }

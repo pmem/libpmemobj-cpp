400b284275aa411c794b2e3146cf1d106cc201e4
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2020-11-18 14:09:18 +0100

    cmake: update finding TBB package
    - print more concise messages,
    - make it required when TESTS_TBB is on.

--- cmake/tbb.cmake
+++ cmake/tbb.cmake
@@ -1,23 +1,24 @@
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2017-2019, Intel Corporation
+# Copyright 2017-2020, Intel Corporation
+
+message(STATUS "Checking for module 'tbb'")
 
 if(PKG_CONFIG_FOUND)
-	pkg_check_modules(TBB tbb)
+	pkg_check_modules(TBB QUIET tbb)
 endif()
 
+# Now, if needed, try to find it without pkg-config..
 if(NOT TBB_FOUND)
 	# find_package without unsetting this var is not working correctly
 	unset(TBB_FOUND CACHE)
+
 	find_package(TBB COMPONENTS tbb)
-
 	if(TBB_FOUND)
-		message(STATUS "TBB package found without pkg-config")
+		set(TBB_LIBRARIES ${TBB_IMPORTED_TARGETS})
+		message(STATUS "  Found in: ${TBB_DIR} using CMake's find_package (ver: ${TBB_VERSION})")
+	elseif(TESTS_TBB)
+		message(FATAL_ERROR "TBB tests are enabled by cmake TESTS_TBB option, but Intel TBB library was not found.")
 	endif()
-
-	set(TBB_LIBRARIES ${TBB_IMPORTED_TARGETS})
-endif()
-
-if(NOT TBB_FOUND)
-	message(WARNING "TBB not found. Please set TBB_DIR CMake variable if TBB \
-is installed in a non-standard directory, like: -DTBB_DIR=<path_to_tbb_cmake_dir>")
+else()
+	message(STATUS "  Found in: ${TBB_LIBDIR} using pkg-config (ver: ${TBB_VERSION})")
 endif()

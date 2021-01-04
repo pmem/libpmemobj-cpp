9146ea24f3d096a7093687fd9881ecc93ac0999e
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2021-01-04 14:04:35 +0100

    tests: extend compatibility checks with more releases

---
+++ tests/compatibility/version/version_1.10.cpp
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/* Copyright 2021, Intel Corporation */
+
+#include <libpmemobj++/version.hpp>
+
+int
+main()
+{
+	static_assert(LIBPMEMOBJ_CPP_VERSION_MAJOR == 1, "");
+	static_assert(LIBPMEMOBJ_CPP_VERSION_MINOR == 10, "");
+}

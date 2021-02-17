5a2bc2d3b4dbccaf91361e7f4ed12f9723bf1dc8
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2021-02-17 10:50:16 +0100

    tests: add assert's description in compatibility tests

--- tests/compatibility/version/version_1.12.cpp
+++ tests/compatibility/version/version_1.12.cpp
@@ -6,6 +6,7 @@
 int
 main()
 {
-	static_assert(LIBPMEMOBJ_CPP_VERSION_MAJOR == 1, "");
-	static_assert(LIBPMEMOBJ_CPP_VERSION_MINOR == 12, "");
+	static_assert(LIBPMEMOBJ_CPP_VERSION_MAJOR == 1, "Wrong major version");
+	static_assert(LIBPMEMOBJ_CPP_VERSION_MINOR == 12,
+		      "Wrong minor version");
 }

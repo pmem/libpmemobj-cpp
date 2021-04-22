c748a03e7b2213ae4eb2ed59600563d6bd746a0c
Pawel Karczewski <pawel.karczewski@intel.com>
2021-04-22 19:06:27 +0200

    Use mpsc_queue in multi_tier_map example

--- examples/radix_tree/multi_tier_map.cpp
+++ examples/radix_tree/multi_tier_map.cpp
@@ -15,7 +15,9 @@
 #include <map>
 #include <thread>
 
-#include <tbb/concurrent_queue.h>
+#include <libpmemobj++/container/mpsc_queue.hpp>
+#include <iostream>
+#include <string>
 
 struct data {
 	pmem::obj::p<size_t> data_1;
@@ -34,15 +36,16 @@ using kv_type = pmem::obj::experimental::radix_tree<size_t, data>;
 
 struct root {
 	pmem::obj::persistent_ptr<kv_type> kv;
-	pmem::obj::persistent_ptr<pmem::obj::vector<char>> log;
+	pmem::obj::persistent_ptr<char[]> log;
 };
 
-static constexpr size_t LOG_CAPACITY = (1ULL << 30);
+//static constexpr size_t LOG_CAPACITY = (1ULL << 30);
+static constexpr size_t LOG_CAPACITY = (1ULL << 12);
 
 struct heterogenous_map {
 	heterogenous_map(size_t dram_size, kv_type &kv,
-			 pmem::obj::vector<char> &log)
-	    : dram_size(dram_size), kv(kv), log(log.data()), log_size(0)
+			 pmem::obj::persistent_ptr<char[]> *log)
+	    : dram_size(dram_size), kv(kv), log_size(10000), log(log), queue(log, log_size, 1)
 	{
 		stopped.store(false);
 		bg_thread = std::thread([&] { bg_work(); });
@@ -66,19 +69,16 @@ struct heterogenous_map {
 			map.erase(map.begin());
 		}
 
-		auto log_pos = log + log_size;
+		//auto log_pos = log + log_size;
 		map.insert_or_assign(k, v);
 
 		log_data ld;
 		ld.key = k;
 		ld.value = v;
 
-		// XXX - replace with queue.acc.add(...);
-		pmemobj_memcpy(pop.handle(), log_pos, (char *)&ld, req_size, 0);
-
-		// XXX - replace with queue.produce();
-		log_size += req_size;
-		queue.push((log_data *)log_pos);
+		auto worker = queue.register_worker();
+		auto acc = worker.produce(req_size);
+		acc.add((char *)&ld, req_size);
 	}
 
 	void
@@ -112,11 +112,12 @@ private:
 	kv_type &kv;
 	std::map<size_t, data> map;
 
-	char *log;
+//	char *log;
 	size_t log_capacity;
 	size_t log_size;
+	pmem::obj::persistent_ptr<char[]> *log;
 
-	tbb::concurrent_queue<log_data *> queue;
+	pmem::obj::experimental::mpsc_queue queue;
 
 	std::atomic<bool> stopped;
 	std::thread bg_thread;
@@ -127,17 +128,20 @@ private:
 	bg_work()
 	{
 		while (!stopped.load()) {
-			log_data *ptr = nullptr;
-			if (queue.try_pop(ptr)) {
-				// XXX - make sure tx does not abort and use
-				// defer_free
-				if (ptr->value == tombstone()) {
-					kv.erase(ptr->key);
-				} else {
-					kv.insert_or_assign(ptr->key,
-							    ptr->value);
-				}
+			auto acc = queue.consume();
+			if(acc.len == 0)
+				continue;
+
+			log_data *ptr = (log_data *) acc.data;
+			// XXX - make sure tx does not abort and use
+			// defer_free
+			if (ptr->value == tombstone()) {
+				kv.erase(ptr->key);
+			} else {
+				kv.insert_or_assign(ptr->key,
+						    ptr->value);
 			}
+			
 		}
 	}
 
@@ -175,25 +179,23 @@ main(int argc, char *argv[])
 		if (r->kv == nullptr) {
 			pmem::obj::transaction::run(pop, [&] {
 				r->kv = pmem::obj::make_persistent<kv_type>();
-				r->log = pmem::obj::make_persistent<
-					pmem::obj::vector<char>>(LOG_CAPACITY,
-								 0);
+				r->log = pmem::obj::make_persistent<char[]>(LOG_CAPACITY);
 			});
 		}
 
-		heterogenous_map map(10, *r->kv, *r->log);
+		heterogenous_map map(10, *r->kv, &r->log);
 
 		map.insert(1, data{1, 2, 1});
 		map.insert(2, data{1, 2, 2});
 		map.insert(3, data{1, 2, 3});
 		map.insert(4, data{1, 2, 4});
-		map.insert(5, data{1, 2, 5});
-
-		assert(map.get(1).data_3 == 1);
-		assert(map.get(2).data_3 == 2);
-		assert(map.get(3).data_3 == 3);
-		assert(map.get(4).data_3 == 4);
-		assert(map.get(5).data_3 == 5);
+		map.insert(5, data{1, 2, 0xDEADBEEF});
+	
+		std::cout << std::to_string( map.get(1).data_3 ) << std::endl;
+		std::cout << std::to_string( map.get(2).data_3 ) << std::endl;
+		std::cout << std::to_string( map.get(3).data_3 ) << std::endl;
+		std::cout << std::to_string( map.get(4).data_3 ) << std::endl;
+		std::cout << std::to_string( map.get(5).data_3 ) << std::endl;
 
 	} catch (pmem::pool_error &e) {
 		std::cerr << e.what() << std::endl;

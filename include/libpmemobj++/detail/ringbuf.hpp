8c1e5cc86227b59271f3a21c29442b459e01f596
Pawel Karczewski <pawel.karczewski@intel.com>
2021-06-02 18:54:55 +0200

    Automatically manage workers' array using uniqe_ptr

--- include/libpmemobj++/detail/ringbuf.hpp
+++ include/libpmemobj++/detail/ringbuf.hpp
@@ -44,6 +44,7 @@
 #include <algorithm>
 #include <atomic>
 #include <cassert>
+#include <memory>
 
 #include <libpmemobj++/detail/atomic_backoff.hpp>
 
@@ -80,6 +81,12 @@ typedef uint64_t ringbuf_off_t;
 typedef struct ringbuf_worker {
 	std::atomic<ringbuf_off_t> seen_off;
 	std::atomic<int> registered;
+
+	ringbuf_worker()
+	{
+		seen_off.store(0);
+		registered.store(0);
+	}
 } ringbuf_worker_t;
 
 typedef struct ringbuf {
@@ -97,21 +104,19 @@ typedef struct ringbuf {
 	/* The following are updated by the consumer. */
 	std::atomic<ringbuf_off_t> written;
 	unsigned nworkers;
-	ringbuf_worker_t *workers;
+	std::unique_ptr<ringbuf_worker_t[]> workers;
 
 	ringbuf(size_t max_workers, size_t length)
+	    : workers(new ringbuf_worker[max_workers])
 	{
 		written.store(0);
-		workers = new ringbuf_worker[max_workers];
+		next.store(0);
+		end.store(0);
 		space = length;
 		end = RBUF_OFF_MAX;
 		nworkers = max_workers;
 	}
 
-	~ringbuf()
-	{
-		delete[] workers;
-	}
 } ringbuf_t;
 
 /*

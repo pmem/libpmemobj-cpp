f43e50936f46529bc24e73971d2de86cf7a1a4cc
Igor Chorążewicz <igor.chorazewicz@intel.com>
2021-08-03 14:20:47 +0200

    Make exception_with_errormsg take arbitrary num of params

--- include/libpmemobj++/pexceptions.hpp
+++ include/libpmemobj++/pexceptions.hpp
@@ -12,9 +12,12 @@
 #include <stdexcept>
 #include <string>
 #include <system_error>
+#include <tuple>
 
 #include <libpmemobj/atomic_base.h>
 #include <libpmemobj/base.h>
+
+#include <libpmemobj++/detail/integer_sequence.hpp>
 
 namespace pmem
 {
@@ -36,13 +39,38 @@ errormsg(void)
 }
 
 /**
- * Generic error message decorator for pmemobj-based exceptions.
+ * Helper for exception_with_errormsg.
+ *
+ * Accepts tuple with arguments and index_sequence which correspond
+ * to all but last argument. The last element in tuple is assumed to
+ * be an error message. Before passing it to ExcT constructor it is merged
+ * with detail::errormsg()
  */
-template <class ExcT, typename MsgT>
+template <typename ExcT, size_t... I, typename Tuple>
 ExcT
-exception_with_errormsg(const MsgT &msg)
+exception_with_errormsg_helper(index_sequence<I...>, Tuple &&args)
 {
-	return ExcT(msg + std::string(": ") + detail::errormsg());
+	static_assert(std::is_rvalue_reference<decltype(args)>::value,
+		      "args should be rvalue reference!");
+
+	auto msg = std::get<sizeof...(I)>(std::move(args)) + std::string(": ") +
+		detail::errormsg();
+	return ExcT(std::get<I>(std::move(args))..., std::move(msg));
+}
+
+/**
+ * Generic error message decorator for pmemobj-based exceptions.
+ *
+ * It accepts arbitrary number of parameters. The last parameter must be an
+ * error message.
+ */
+template <class ExcT, typename... Args>
+ExcT
+exception_with_errormsg(Args &&... args)
+{
+	return exception_with_errormsg_helper<ExcT>(
+		make_index_sequence<sizeof...(Args) - 1>{},
+		std::forward_as_tuple(std::forward<Args>(args)...));
 }
 } /* namespace detail */
 

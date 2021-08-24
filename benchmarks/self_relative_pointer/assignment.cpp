7690fe05f37793bf296eed2b072f2e5e629410a7
Yang Jun 01 <yangjun01@4paradigm.com>
2021-08-24 10:16:06 +0800

    Implement persistent-aware feature of self_relative_ptr as a templated parameter. Implement the single-writer-multi-reader skip-list based on the persistent-aware self_relative_ptr in existing concurrent_skip_list_impl as a templated parameter. Add some UTs.

--- benchmarks/self_relative_pointer/assignment.cpp
+++ benchmarks/self_relative_pointer/assignment.cpp
@@ -37,7 +37,8 @@ using size_type = std::ptrdiff_t;
 template <typename U>
 using persistent_ptr = pmem::obj::persistent_ptr<U>;
 template <typename U>
-using self_relative_ptr = pmem::obj::experimental::self_relative_ptr<U, std::false_type>;
+using self_relative_ptr =
+	pmem::obj::experimental::self_relative_ptr<U, std::false_type>;
 template <typename U>
 using vector = pmem::obj::vector<U>;
 

86d145784822741c33aa9560fab223194e858122
yjrobin <55862971+yjrobin@users.noreply.github.com>
2021-09-28 15:25:56 +0800

    1. Use atomic_backoff instead of immediate yield in read-optimized load 2. Implement exchange and compare_exchange_weak for discussion 3. Fix UT based on previous comments 3. Minor fixes

--- include/libpmemobj++/experimental/atomic_persistent_aware_ptr.hpp
+++ include/libpmemobj++/experimental/atomic_persistent_aware_ptr.hpp
@@ -1,14 +1,15 @@
 // SPDX-License-Identifier: BSD-3-Clause
-/* Copyright 2021-2022, Intel Corporation */
+/* Copyright 2021, Intel Corporation */
 
 /**
  * @file
- * Atomic specialization for persistent-ware self_relative_ptr.
+ * Atomic specialization for persistent-aware self_relative_ptr.
  */
 
 #ifndef LIBPMEMOBJ_CPP_ATOMIC_PERSISTENT_AWARE_PTR_HPP
 #define LIBPMEMOBJ_CPP_ATOMIC_PERSISTENT_AWARE_PTR_HPP
 
+#include <libpmemobj++/detail/atomic_backoff.hpp>
 #include <libpmemobj++/detail/common.hpp>
 #include <libpmemobj++/detail/self_relative_ptr_base_impl.hpp>
 #include <libpmemobj++/experimental/atomic_self_relative_ptr.hpp>
@@ -25,6 +26,15 @@ namespace obj
 namespace experimental
 {
 
+/**
+ * Atomic specialization of a persistent ptr (self_relative_ptr) that manages
+ * its persistence by itself.
+ *
+ * In a multi-threaded scenario, the persistence of this ptr is guaranteed when
+ * it is visible to (or read by) other threads. Performance-wise, two different
+ * options are provided: Read-optimized and Write-optimized. See corresponding
+ * store/load functions for details.
+ */
 template <typename T, typename ReadOptimized>
 struct atomic_persistent_aware_ptr {
 private:
@@ -40,11 +50,11 @@ public:
 	using value_type = pmem::obj::experimental::self_relative_ptr<T>;
 	using difference_type = typename value_type::difference_type;
 
-	/*
+	constexpr atomic_persistent_aware_ptr() noexcept = default;
+
+	/**
 	 * Constructors
 	 */
-
-	constexpr atomic_persistent_aware_ptr() noexcept = default;
 	atomic_persistent_aware_ptr(value_type value) : ptr()
 	{
 		store(value);
@@ -52,9 +62,12 @@ public:
 	atomic_persistent_aware_ptr(const atomic_persistent_aware_ptr &) =
 		delete;
 
-	/*
-	 * Read-optimized store do the flush in store function, and clear the
+	/**
+	 * Read-optimized store does the flush in store function, and clear the
 	 * dirty marker after flush.
+	 *
+	 * @param[in] desired the self_relative_ptr (no dirty flag) to be stored
+	 *
 	 */
 	template <typename OPT = ReadOptimized>
 	typename std::enable_if<std::is_same<OPT, std::true_type>::value>::type
@@ -68,9 +81,12 @@ public:
 					    order);
 	}
 
-	/*
+	/**
 	 * Write-optimized store updates the ptr with the dirty flag, relies on
 	 * consequent load to do the flush.
+	 *
+	 * @param[in] desired the self_relative_ptr (no dirty flag) to be stored
+	 *
 	 */
 	template <typename OPT = ReadOptimized>
 	typename std::enable_if<!std::is_same<OPT, std::true_type>::value>::type
@@ -80,9 +96,11 @@ public:
 		ptr.store(mark_dirty(desired), order);
 	}
 
-	/*
-	 * Read-optimized load retries upon dirty ptr, relies on tge store
+	/**
+	 * Read-optimized load retries upon dirty ptr, relies on the store
 	 * function to clear the dirty before continue.
+	 *
+	 * @return the self_relative_ptr (no dirty flag)
 	 */
 	template <typename OPT = ReadOptimized>
 	typename std::enable_if<std::is_same<OPT, std::true_type>::value,
@@ -90,15 +108,21 @@ public:
 	load(std::memory_order order = std::memory_order_seq_cst) noexcept
 	{
 		auto val = ptr.load(order);
-		while (is_dirty(val)) {
-			std::this_thread::yield();
-			val = ptr.load(order);
+		if (is_dirty(val)) {
+			for (detail::atomic_backoff backoff(true);;) {
+				val = ptr.load(order);
+				if (!is_dirty(val))
+					break;
+			}
 		}
 		return val;
 	}
-	/*
+
+	/**
 	 * Write-optimized load flushes the ptr with the dirty flag, clears the
 	 * flag using CAS after flush.
+	 *
+	 * @return the self_relative_ptr (no dirty flag)
 	 */
 	template <typename OPT = ReadOptimized>
 	typename std::enable_if<!std::is_same<OPT, std::true_type>::value,
@@ -115,20 +139,109 @@ public:
 		return val;
 	}
 
+	/**
+	 * Not commonly used.
+	 *
+	 * @param[in] desired the self_relative_ptr (no dirty flag) to be
+	 * exchanged in.
+	 *
+	 * @return the old self_relative_ptr (no dirty flag) before exchange.
+	 *
+	 */
 	value_type
 	exchange(value_type desired,
 		 std::memory_order order = std::memory_order_seq_cst) noexcept
 	{
-		return ptr.exchange(desired, order);
+		auto dirty_desired = mark_dirty(desired);
+		auto ret = ptr.exchange(dirty_desired, order);
+		pool_by_vptr(this).persist(&ptr, sizeof(ptr));
+		ptr.compare_exchange_strong(dirty_desired,
+					    clear_dirty(dirty_desired), order);
+		return clear_dirty(ret);
 	}
 
+	/**
+	 * Not commonly used.
+	 *
+	 * @param[out] expected the self_relative_ptr (no dirty flag) expected
+	 * to be stored, return the actual one if CAS failed.
+	 * @param[in] desired the self_relative_ptr (no dirty flag) to be
+	 * exchanged in.
+	 *
+	 * @return true when CAS succeeds, false otherwise.
+	 *
+	 */
 	bool
 	compare_exchange_weak(value_type &expected, value_type desired,
 			      std::memory_order success,
 			      std::memory_order failure) noexcept
 	{
-		return ptr.compare_exchange_weak(expected, desired, success,
-						 failure);
+		bool ret;
+		auto dirty_desired = mark_dirty(desired);
+		auto vptr = expected.get();
+		if ((ret = ptr.compare_exchange_weak(expected, dirty_desired,
+						     success, failure))) {
+			/**
+			 * if the first CAS is a success, we continue to flush
+			 * it and use another CAS to clear the dirty flag after
+			 * flush, just like what store() does.
+			 */
+			pool_by_vptr(this).persist(&ptr, sizeof(ptr));
+			if ((ret = ptr.compare_exchange_weak(
+				     dirty_desired, clear_dirty(dirty_desired),
+				     success, failure))) {
+				return ret;
+			}
+			expected = dirty_desired;
+			return ret;
+		}
+		if (clear_dirty(expected).get() == vptr) {
+			/**
+			 * if the first CAS failed, expected will have the
+			 * actual ptr stored in it. We then check if the failure
+			 * is due the dirty flag. If so, it should to not be
+			 * considered as a failure. So we CAS again with the
+			 * actual ptr (should be with dirty flag this time).
+			 *
+			 */
+			if ((ret = ptr.compare_exchange_weak(
+				     expected, dirty_desired, success,
+				     failure))) {
+				/**
+				 * If this CAS is success, we continue to flush
+				 * the new ptr (with dirty), and then use CAS to
+				 * clear the dirty flag.
+				 */
+				pool_by_vptr(this).persist(&ptr, sizeof(ptr));
+				if (!ptr.compare_exchange_strong(
+					    dirty_desired,
+					    clear_dirty(dirty_desired), success,
+					    failure)) {
+					/**
+					 * If this CAS failed, it means another
+					 * thread just changed the ptr right
+					 * after we flushed it. So this should
+					 * be considered a failure. We store the
+					 * new ptr in expected and return
+					 * failure.
+					 */
+					expected = clear_dirty(dirty_desired);
+					ret = false;
+				}
+				return ret;
+			}
+			/**
+			 * When reach here, it means another thread updates the
+			 * ptr after we checked that the failure is due to the
+			 * dirty flag. So we just return failure and expected
+			 * already has the actual ptr stored in it.
+			 */
+		}
+		/**
+		 * When reach here, expected has already the actual ptr stored
+		 * in it, return as a failed CAS.
+		 */
+		return false;
 	}
 
 	bool
@@ -179,7 +292,6 @@ public:
 	/*
 	 * Operators
 	 */
-
 	operator value_type() const noexcept
 	{
 		return load();

0b89c2510d2a8dd012f12449b2cbaacd1d2a4f9d
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2021-10-11 13:19:32 +0200

    tests: add negative pmreorder test for atomic self relative ptr

---
+++ tests/ptr/atomic_pmreorder_0.cmake
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2019-2021, Intel Corporation
+
+include(${SRC_DIR}/../helpers.cmake)
+
+setup()
+
+execute(${TEST_EXECUTABLE} c ${DIR}/testfile)
+pmreorder_create_store_log(${DIR}/testfile ${TEST_EXECUTABLE} m ${DIR}/testfile)
+# negative test, should fail, uses mock of atomic_self_relative_ptr
+pmreorder_execute(false NoReorderNoCheck PMREORDER_MARKER=ReorderReverseAccumulative ${TEST_EXECUTABLE} n)
+
+finish()

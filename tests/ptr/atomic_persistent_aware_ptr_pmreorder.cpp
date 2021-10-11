8a9d89a6cdd113445f457bebc421064ea4acd2c3
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2021-10-11 13:19:32 +0200

    tests: add negative pmreorder test for atomic self relative ptr

--- tests/ptr/atomic_persistent_aware_ptr_pmreorder.cpp
+++ tests/ptr/atomic_persistent_aware_ptr_pmreorder.cpp
@@ -15,9 +15,43 @@
 
 #define LAYOUT "pmreorder"
 
-/* data (for self_relative_ptr) has to set the least significant byte to 0 */
-int TEST_DATA_R = 0xABBA;
-int TEST_DATA_W = 0xACDC;
+/* data (for self_relative_ptr) has to be aligned to 2B */
+static int TEST_DATA = 0xACDC;
+static std::function<void(void)> sync_func;
+
+/* mock prepared to check negative case (simulate wrong implementation) */
+template <typename T>
+class mock_atomic_self_relative_ptr {
+public:
+	using value_type = pmem::obj::experimental::self_relative_ptr<T>;
+
+	void
+	store(value_type val)
+	{
+		ptr.store(val);
+		/* sync other thread to stat reading now */
+		sync_func();
+		/* wait for other thread to finish reading */
+		sync_func();
+		pmem::obj::pool_by_vptr(this).persist(&ptr, sizeof(ptr));
+	}
+
+	value_type
+	load()
+	{
+		return ptr.load();
+	}
+
+	mock_atomic_self_relative_ptr &
+	operator=(value_type const &r)
+	{
+		ptr = r;
+		return *this;
+	}
+
+private:
+	std::atomic<pmem::obj::experimental::self_relative_ptr<T>> ptr;
+};
 
 namespace nvobj = pmem::obj;
 template <typename T>
@@ -31,57 +65,73 @@ struct root {
 	atomic_ptr<int, std::true_type> ptr_r;
 	/* write-optimized */
 	atomic_ptr<int, std::false_type> ptr_w;
+	/* mock for negative case */
+	mock_atomic_self_relative_ptr<int> ptr_neg;
 
 	self_relative_ptr<int> read_r;
 	self_relative_ptr<int> read_w;
+	self_relative_ptr<int> read_neg;
 };
 
+template <typename PtrA, typename PtrR>
 void
-insert_and_read(nvobj::pool<root> &pop)
+insert_and_read(nvobj::pool<root> &pop, PtrA &ptr_atomic, PtrR &ptr_read)
 {
 	parallel_xexec(
 		2,
 		[&](size_t thread_id, std::function<void(void)> syncthreads) {
 			syncthreads();
-			auto r = pop.root();
 
 			if (thread_id == 0) {
 				VALGRIND_PMC_EMIT_LOG("PMREORDER_MARKER.BEGIN");
+			}
+			syncthreads();
+
+			if (thread_id == 0) {
+				/* save globalally for mock class to use */
+				sync_func = syncthreads;
+
 				/* insert test data into atomic ptr's */
-				r->ptr_r.store(
-					reinterpret_cast<int *>(TEST_DATA_R));
-				r->ptr_w.store(
-					reinterpret_cast<int *>(TEST_DATA_W));
-				VALGRIND_PMC_EMIT_LOG("PMREORDER_MARKER.END");
+				ptr_atomic.store(
+					reinterpret_cast<int *>(TEST_DATA));
 			} else {
 				/* read test data into self relative ptr's */
-				r->read_w = r->ptr_w.load();
-				pop.persist(r->read_w.to_persistent_ptr());
-				r->read_r = r->ptr_r.load();
-				pop.persist(r->read_r.to_persistent_ptr());
+				if (std::is_same<PtrA,
+						 mock_atomic_self_relative_ptr<
+							 int>>::value) {
+					/* negative case have to be sync'ed (see
+					 * comments in mock class) */
+					syncthreads();
+					ptr_read = ptr_atomic.load();
+					syncthreads();
+				} else {
+					ptr_read = ptr_atomic.load();
+				}
+				pop.persist(&ptr_read, sizeof(ptr_read));
+			}
+
+			syncthreads();
+			if (thread_id == 0) {
+				VALGRIND_PMC_EMIT_LOG("PMREORDER_MARKER.END");
 			}
 		});
 }
 
+template <typename PtrL, typename PtrR>
 void
-check_consistency(nvobj::pool<root> &pop)
+check_consistency(nvobj::pool<root> &pop, PtrL &ptr_load, PtrR &ptr_read)
 {
-	auto r = pop.root();
-
-	/* we use pmreorder ReorderAccumulative engine
-	 * This means we either don't have the "read_* set up or it's properly
-	 * set to loaded value */
-	UT_ASSERT(r->read_w == nullptr ||
-		  r->ptr_w.load().get() == r->read_w.get());
-	UT_ASSERT(r->read_r == nullptr ||
-		  r->ptr_r.load().get() == r->read_r.get());
+	/* no matter how it's reordered we either don't have the read_*
+	 * set up yet, or it's properly set to loaded value */
+	UT_ASSERT(ptr_read == nullptr ||
+		  ptr_load.load().get() == ptr_read.get());
 }
 
 static void
 test(int argc, char *argv[])
 {
-	if (argc != 3 || strchr("cio", argv[1][0]) == nullptr)
-		UT_FATAL("usage: %s <c|i|o> file-name", argv[0]);
+	if (argc != 3 || strchr("ciomn", argv[1][0]) == nullptr)
+		UT_FATAL("usage: %s <c|i|o|m|n> file-name", argv[0]);
 
 	const char *path = argv[2];
 
@@ -97,19 +147,37 @@ test(int argc, char *argv[])
 			pmem::obj::transaction::run(pop, [&] {
 				pop.root()->ptr_r = nullptr;
 				pop.root()->ptr_w = nullptr;
+				pop.root()->ptr_neg = nullptr;
 				pop.root()->read_r = nullptr;
 				pop.root()->read_w = nullptr;
+				pop.root()->read_neg = nullptr;
 			});
 
 		} else if (argv[1][0] == 'i') {
 			/* store and load in parallel */
 			pop = nvobj::pool<root>::open(path, LAYOUT);
-			insert_and_read(pop);
+			auto r = pop.root();
+			insert_and_read(pop, r->ptr_r, r->read_r);
+			insert_and_read(pop, r->ptr_w, r->read_w);
 
 		} else if (argv[1][0] == 'o') {
 			/* re-open at the end, for consistency check */
 			pop = nvobj::pool<root>::open(path, LAYOUT);
-			check_consistency(pop);
+			auto r = pop.root();
+			check_consistency(pop, r->ptr_r, r->read_r);
+			check_consistency(pop, r->ptr_w, r->read_w);
+
+		} else if (argv[1][0] == 'm') {
+			/* mock store and load in parallel */
+			pop = nvobj::pool<root>::open(path, LAYOUT);
+			auto r = pop.root();
+			insert_and_read(pop, r->ptr_neg, r->read_neg);
+
+		} else if (argv[1][0] == 'n') {
+			/* re-open at the end, for negative consistency check */
+			pop = nvobj::pool<root>::open(path, LAYOUT);
+			auto r = pop.root();
+			check_consistency(pop, r->ptr_neg, r->read_neg);
 		}
 	} catch (pmem::pool_error &pe) {
 		UT_FATAL("!pool::create: %s %s", pe.what(), path);
